44696196b47ccb09b1b0d1b48323db0b7914ab02
Scott Willson <scott@butlerpress.com>
2006-10-07 02:45:14 +0000

    moved ResultsController to Racing on Rails

---
+++ vendor/plugins/racing_on_rails/app/controllers/results_controller.rb
@@ -0,0 +1,30 @@
+class ResultsController < ApplicationController
+  model :result, :event, :standings, :race, :racer
+  session :off
+  
+  def index
+    # TODO Create helper method to return Range of first and last of year
+  	@year = @params['year'].to_i
+  	@year = Date.today.year if @year == 0
+  	first_of_year = Date.new(@year, 1, 1)
+  	first_of_next_year = Date.new(@year +1, 1, 1)
+    @road_events = SingleDayEvent.find(
+      :all,
+      :conditions => ['date between ? and ? and id in (select event_id from standings)', 
+                      first_of_year, first_of_next_year],
+      :order => 'date desc'
+    )
+  end
+  
+  def event
+    @event = Event.find(params[:id])
+  end
+  
+  def racer
+  	@racer = Racer.find(params[:id])
+  end
+  
+  def show
+  	@result = Result.find(params[:id])
+  end
+end
