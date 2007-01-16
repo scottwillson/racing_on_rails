5395161bb160118ba1572e21548226710e2019f2
Scott Willson <scott@butlerpress.com>
2007-01-16 04:50:17 +0000

    Added Candi Update

--- app/models/announcement_mailer.rb
+++ app/models/announcement_mailer.rb
@@ -1,7 +1,7 @@
 class AnnouncementMailer < ActionMailer::Base
   def announcement(recipient)
-    subject     'A Candi Vanilla for Candi'
-    recipients  recipient
+    subject     'Candi Vanilla Update'
+    recipients  [recipient]
     from       'Yann Blindert <nevens42@yahoo.com>'
   end
 end
