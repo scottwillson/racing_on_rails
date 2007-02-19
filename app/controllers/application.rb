49690bd3447ff1f4e3dcc89daa2b966aacc4482b
Scott Willson <scott@butlerpress.com>
2007-02-19 00:03:51 +0000

    Added custom error pages back

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,14 +7,33 @@ class ApplicationController < ActionController::Base
   
   # FIXME: Use conditional to look in local first, RAILS_ROOT second
   def rescue_action_in_public(exception)
+    logger.debug('custom rescue_action_in_public')
   	case exception
     when ActiveRecord::RecordNotFound, ::ActionController::RoutingError, ::ActionController::UnknownAction
-      render(:file => "#{RAILS_ROOT}/public/local/404.html", :status => "404 Not Found")
+      render(:file => local_or_default_file('404.html'), :status => "404 Not Found")
 	  	
 	  else
-	  	render(:file => "#{RAILS_ROOT}/public/local/500.html", :status => "500 Error")
+	  	render(:file => local_or_default_file('500.html'), :status => "500 Error")
 	  	SystemNotifier.deliver_exception_notification(self, request, exception)
 	  	
     end
   end
+  
+  def rescue_action_locally(exception)
+    logger.debug('custom rescue_action_locally')
+    self.rescue_action_in_public(exception)
+  end
+  
+  protected
+  def local_or_default_file(name)
+    local_path = File.join(RAILS_ROOT, 'local', 'public', "#{name}")
+    logger.debug("local_or_default_file #{local_path} exists? #{File.exist?(local_path)}")
+    if File.exist?(local_path)
+      local_path
+    else
+      logger.debug(File.join(RAILS_ROOT, 'public', "#{name}"))
+      File.join(RAILS_ROOT, 'public', "#{name}")
+    end
+  end
 end
+
