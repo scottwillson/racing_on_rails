235cd54215536f04f8c95de68eb1233208ef11fa
Scott Willson <scott@butlerpress.com>
2007-03-02 22:15:41 +0000

    Moved OBRA deployment out of Racing on Rails trunk

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,34 +3,26 @@
 # cap -f local/config/deploy.rb -a deploy
 require 'mongrel_cluster/recipes'
 
-set :application, "obra"
+set :application, "racing_on_rails"
 set :repository, "svn+ssh://butlerpress.com/var/repos/racing_on_rails/trunk"
 
-role :app, "app.obra.org"
-role :db, 'app.obra.org', :primary => true
+# role :app, "my.server.com"
+# role :db, 'my.server.com', :primary => true
 
-set :deploy_to, "/srv/www/rails/#{application}"
+set :deploy_to, "/var/www/rails/#{application}"
 
-desc "OBRA custom deployment"
+desc "Custom deployment"
 task :deploy do
   transaction do
     update_code
     symlink
   
-    # Pull in local OBRA code
-    run "svn co svn+ssh://butlerpress.com/var/repos/obra/branches/rel_0.5.0 /srv/www/rails/#{application}/current/local"
+    # Pull in your local code
+    run "svn co svn+ssh://my.svn_server.com/var/repos/local_application /var/www/rails/#{application}/current/local"
 
-    set_permissions
     migrate
     restart
   end
-end
-
-desc "Set file permissions for Rails app"
-task :set_permissions do
-  sudo "chown -R wwwrun:rails #{deploy_to}/current/public"
-  sudo "chown -R sw:rails #{deploy_to}/revisions.log"
-  sudo "chmod +x /srv/www/rails/obra/current/script/runner"
 end
 
 desc "Restart the web server"
