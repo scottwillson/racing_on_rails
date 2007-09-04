c367e8d0375c822269a70ad7c49c0ff08d9fe0c8
Scott Willson <scott@butlerpress.com>
2007-09-04 03:36:07 +0000

    fixed broken test with admin page

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -3,5 +3,16 @@ class Admin::HomeController < ApplicationController
   before_filter :login_required
 
   def index
+    @upcoming_events = UpcomingEvents.new
+    
+    cutoff = Date.today - 14
+    @recent_results = SingleDayEvent.find(
+      :all,
+      :conditions => ['date > ? and id in (select event_id from standings)', cutoff],
+      :order => 'date desc'
+    )
+    
+    @news = NewsItem.find(:all)
+    @home_page_photo = Image.find_by_name('home_page_photo')
   end
 end
--- test/functional/admin/home_controller_test.rb
+++ test/functional/admin/home_controller_test.rb
@@ -13,14 +13,17 @@ class Admin::HomeControllerTest < Test::Unit::TestCase
     @request.session[:user] = users(:candi)
   end
 
-  def test_show
+  def test_index
     opts = {:controller => "admin/home", :action => 'index'}
     assert_routing("/admin/home", opts)
     
-    # expose old bug
     get(:index)
     assert_response(:success)
     assert_template("admin/home/index")
+    assert_not_nil(assigns['upcoming_events'], 'Should assign upcoming_events')
+    assert_not_nil(assigns['recent_results'], 'Should assign recent_results')
+    assert_not_nil(assigns['news'], 'Should assign news')
+    assert_not_nil(assigns['home_page_photo'], 'Should assign home_page_photo')
   end
   
   def test_security
