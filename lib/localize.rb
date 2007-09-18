490e62eefab4dff01b53f53f845630adff022c84
Scott Willson <scott@butlerpress.com>
2007-09-18 19:31:03 +0000

    Fiddled with configurable server roots to make them work in dev and hopefully in staging, too

--- lib/localize.rb
+++ lib/localize.rb
@@ -1,5 +1,6 @@
 # Local config customization
 load("#{RAILS_ROOT}/local/config/environment.rb") if File.exist?("#{RAILS_ROOT}/local/config/environment.rb")
+load("#{RAILS_ROOT}/local/config/environments/#{RAILS_ENV}.rb") if File.exist?("#{RAILS_ROOT}/local/config/environments/#{RAILS_ENV}.rb")
 
 # Prefer local templates, partials etc. if they exist.  Otherwise, use the base
 # application's generic files.
