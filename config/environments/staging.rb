996163c434c6c238185f626b250a9368dab84290
Scott Willson <scott@butlerpress.com>
2007-09-18 19:34:30 +0000

    Added APP_SERVER_ROOT fir staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,4 +19,4 @@ config.action_view.debug_rjs                         = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :test
 
-APP_SERVER_ROOT = "http://"
+APP_SERVER_ROOT = "http://app.obra.butlerpress.com"
