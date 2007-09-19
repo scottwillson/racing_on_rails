6012fd803fc110f607fad950eecaa8c13bff2d2e
Scott Willson <scott@butlerpress.com>
2007-09-19 19:30:36 +0000

    CCX score sheet test now passes, though all it does it create an empty Excel doc.

--- app/models/ccx_score_sheet.rb
+++ app/models/ccx_score_sheet.rb
@@ -1,10 +1,18 @@
+require 'spreadsheet/excel'
+
 class CcxScoreSheet
+
+  include Spreadsheet
   
   def initialize(path)
     @path = path
   end
   
   def save!
-    
+    workbook = Excel.new(@path)
+    workbook.add_worksheet
+    workbook.add_worksheet
+    workbook.add_worksheet
+    workbook.close
   end
 end
