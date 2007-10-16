20e2eb7f280c4ff778881ec3a7e88f0ace394753
Scott Willson <scott@butlerpress.com>
2007-10-16 19:09:22 +0000

    Added check in migration to ensure Alias is really there

--- db/migrate/030_remove_bad_aliases.rb
+++ db/migrate/030_remove_bad_aliases.rb
@@ -3,12 +3,12 @@ class RemoveBadAliases < ActiveRecord::Migration
     Alias.transaction do
       bad_aliases = Alias.connection.select_values('select aliases.id from aliases, teams where aliases.name = teams.name')
       for bad_alias in bad_aliases
-        Alias.destroy(bad_alias)
+        Alias.destroy(bad_alias) if Alias.exists?(bad_alias)
       end
 
       bad_aliases = Alias.connection.select_values("select aliases.id from aliases, racers where trim(concat(first_name, ' ', last_name)) = aliases.name")
       for bad_alias in bad_aliases
-        Alias.destroy(bad_alias)
+        Alias.destroy(bad_alias) if Alias.exists?(bad_alias)
       end
     end
   end
