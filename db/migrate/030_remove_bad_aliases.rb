d220a7199562411068213fb94bc92b7fc8bed17e
Scott Willson <scott@butlerpress.com>
2007-10-16 00:01:21 +0000

    Added transaction

--- db/migrate/030_remove_bad_aliases.rb
+++ db/migrate/030_remove_bad_aliases.rb
@@ -1,13 +1,15 @@
 class RemoveBadAliases < ActiveRecord::Migration
   def self.up
-    bad_aliases = Alias.connection.select_values('select aliases.id from aliases, teams where aliases.name = teams.name')
-    for bad_alias in bad_aliases
-      Alias.destroy(bad_alias)
-    end
+    Alias.transaction do
+      bad_aliases = Alias.connection.select_values('select aliases.id from aliases, teams where aliases.name = teams.name')
+      for bad_alias in bad_aliases
+        Alias.destroy(bad_alias)
+      end
 
-    bad_aliases = Alias.connection.select_values("select aliases.id from aliases, racers where trim(concat(first_name, ' ', last_name)) = aliases.name")
-    for bad_alias in bad_aliases
-      Alias.destroy(bad_alias)
+      bad_aliases = Alias.connection.select_values("select aliases.id from aliases, racers where trim(concat(first_name, ' ', last_name)) = aliases.name")
+      for bad_alias in bad_aliases
+        Alias.destroy(bad_alias)
+      end
     end
   end
 
