8c888ba9772c05b0524f8812747004a5256be9f9
Scott Willson <scott@butlerpress.com>
2007-11-08 14:51:42 +0000

    Completed Age Graded BAR

--- app/models/competitions/age_graded_bar.rb
+++ app/models/competitions/age_graded_bar.rb
@@ -1,75 +1,74 @@
 module Competitions
   class AgeGradedBar < Competition
 
-    CATEGORIES = []
-
     def points_for(scoring_result)
       scoring_result.points
     end
 
     def source_results(race)
-      # Need date of birth range
-      # Need category map from Masters Men to Masters Men 30-34, etc.
-      # start_date_of_birth = last day of year prior to event year
-      # end_date_of_birth = last day of event year
       Result.find(:all,
                   :include => [:race, {:racer => :team}, :team, {:race => [{:standings => :event}, :category]}],
                   :conditions => [%Q{events.type = 'OverallBar' 
                     and categories.id = #{race.category.parent(true).id}
                     and racers.date_of_birth between '#{race.dates_of_birth.begin}' and '#{race.dates_of_birth.end}'
-                    and events.date >= '#{date.year}-01-01' 
-                    and events.date <= '#{date.year}-12-31'}],
+                    and events.date between '#{date.year}-01-01' and '#{date.year}-12-31'}],
                   :order => 'racer_id'
       )
     end
     
     def create_standings
       root_standings = standings.create(:event => self)
-      for category in create_categories
+      for category in categories
         root_standings.races.create!(:category => category)
       end
     end
     
-    def create_categories
-      if CATEGORIES.empty?
-        30.step(65, 5) do |age|
-          CATEGORIES << Category.new(:name => "Masters Men #{age}-#{age + 4}", :ages => (age)..(age + 4), :parent => Category.new(:name => 'Masters Men'))
-        end
-        CATEGORIES << Category.new(:name => 'Masters Men 70+', :ages => 70..999, :parent => Category.new(:name => 'Masters Men'))
-        
-        30.step(55, 5) do |age|
-          CATEGORIES << Category.new(:name => "Masters Women #{age}-#{age + 4}", :ages => (age)..(age + 4), :parent => Category.new(:name => 'Masters Women'))
-        end
-        CATEGORIES << Category.new(:name => 'Masters Women 60+', :ages => 60..999, :parent => Category.new(:name => 'Masters Women'))
-        
-        CATEGORIES << Category.new(:name => "Junior Men 10-12", :ages => 10..12, :parent => Category.new(:name => 'Junior Men'))
-        CATEGORIES << Category.new(:name => "Junior Men 13-14", :ages => 13..14, :parent => Category.new(:name => 'Junior Men'))
-        CATEGORIES << Category.new(:name => "Junior Men 15-16", :ages => 15..16, :parent => Category.new(:name => 'Junior Men'))
-        CATEGORIES << Category.new(:name => "Junior Men 17-18", :ages => 17..18, :parent => Category.new(:name => 'Junior Men'))
-        
-        CATEGORIES << Category.new(:name => "Junior Women 10-12", :ages => 10..12, :parent => Category.new(:name => 'Junior Women'))
-        CATEGORIES << Category.new(:name => "Junior Women 13-14", :ages => 13..14, :parent => Category.new(:name => 'Junior Women'))
-        CATEGORIES << Category.new(:name => "Junior Women 15-16", :ages => 15..16, :parent => Category.new(:name => 'Junior Women'))
-        CATEGORIES << Category.new(:name => "Junior Women 17-18", :ages => 17..18, :parent => Category.new(:name => 'Junior Women'))
-
-        for category in CATEGORIES
-          if Category.exists?(:name => category.parent.name)
-            category.parent = Category.find_by_name(category.parent.name)
-          else
-            category.parent.save!
-          end
-          
-          existing_category = Category.find_by_name(category.name)
-          if existing_category.nil?
-            category.save!
-          elsif existing_category.ages != category.ages || existing_category.parent != category.parent
-            existing_category.ages = category.ages
-            existing_category.parent = category.parent
-            existing_category.save!
-          end          
-        end
+    def categories
+      template_categories = []
+      30.step(65, 5) do |age|
+        template_categories << Category.new(:name => "Masters Men #{age}-#{age + 4}", :ages => (age)..(age + 4), :parent => Category.new(:name => 'Masters Men'))
       end
-      CATEGORIES
+      template_categories << Category.new(:name => 'Masters Men 70+', :ages => 70..999, :parent => Category.new(:name => 'Masters Men'))
+      
+      30.step(55, 5) do |age|
+        template_categories << Category.new(:name => "Masters Women #{age}-#{age + 4}", :ages => (age)..(age + 4), :parent => Category.new(:name => 'Masters Women'))
+      end
+      template_categories << Category.new(:name => 'Masters Women 60+', :ages => 60..999, :parent => Category.new(:name => 'Masters Women'))
+      
+      template_categories << Category.new(:name => "Junior Men 10-12", :ages => 10..12, :parent => Category.new(:name => 'Junior Men'))
+      template_categories << Category.new(:name => "Junior Men 13-14", :ages => 13..14, :parent => Category.new(:name => 'Junior Men'))
+      template_categories << Category.new(:name => "Junior Men 15-16", :ages => 15..16, :parent => Category.new(:name => 'Junior Men'))
+      template_categories << Category.new(:name => "Junior Men 17-18", :ages => 17..18, :parent => Category.new(:name => 'Junior Men'))
+      
+      template_categories << Category.new(:name => "Junior Women 10-12", :ages => 10..12, :parent => Category.new(:name => 'Junior Women'))
+      template_categories << Category.new(:name => "Junior Women 13-14", :ages => 13..14, :parent => Category.new(:name => 'Junior Women'))
+      template_categories << Category.new(:name => "Junior Women 15-16", :ages => 15..16, :parent => Category.new(:name => 'Junior Women'))
+      template_categories << Category.new(:name => "Junior Women 17-18", :ages => 17..18, :parent => Category.new(:name => 'Junior Women'))
+
+      categories = []
+      for template_category in template_categories
+        if Category.exists?(:name => template_category.parent.name)
+          template_category.parent = Category.find_by_name(template_category.parent.name)
+        else
+          template_category.parent.save!
+        end
+        
+        category = Category.find_by_name(template_category.name)
+        if category.nil?
+          template_category.save!
+          category = template_category
+        elsif category.ages != template_category.ages || category.parent != template_category.parent
+          category.ages = template_category.ages
+          category.parent = template_category.parent
+          category.save!            
+        end
+        raise "#{category.name} not valid" unless category.valid?
+        raise "#{category.name} is new record" if category.new_record?
+        raise "#{category.name} does no exist" unless Category.exists?(category.id)
+        raise "#{category.name} ages equal 0..999" if category.ages == (0..999)
+        categories << category
+      end
+      categories
     end
 
     def friendly_name
