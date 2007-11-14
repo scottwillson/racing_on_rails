18fe4002f131d8a388f0e7d24dae2cf39df22a35
Scott Willson <scott@butlerpress.com>
2007-11-14 20:31:46 +0000

    Changed migration look for Cat 5 category by name

--- db/migrate/034_update_cat5_position.rb
+++ db/migrate/034_update_cat5_position.rb
@@ -1,8 +1,10 @@
 class UpdateCat5Position < ActiveRecord::Migration
   def self.up
-    cat_5 = Category.find(145)
-    cat_5.position = 50
-    cat_5.save!
+    cat_5 = Category.find_by_name('Category 5 Men')
+    if cat_5
+      cat_5.position = 50
+      cat_5.save!
+    end
   end
 
   def self.down
