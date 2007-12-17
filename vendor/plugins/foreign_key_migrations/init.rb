8c6a86e5699777e57554800a8c68b4b9765fcf00
Scott Willson <scott@butlerpress.com>
2007-12-17 15:07:01 +0000

    Trying to remove old plugin versions and switch to author repos

--- vendor/plugins/foreign_key_migrations/init.rb
+++
@@ -1,2 +0,0 @@
-ActiveRecord::ConnectionAdapters::AbstractAdapter.send(:include, RedHillConsulting::ForeignKeyMigrations::AbstractAdapter)
-ActiveRecord::ConnectionAdapters::TableDefinition.send(:include, RedHillConsulting::ForeignKeyMigrations::TableDefinition)
