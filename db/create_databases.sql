33049efc29a3766090aabc71c8efb7b5ebe482b8
Scott Willson <scott@butlerpress.com>
2008-02-08 04:11:42 +0000

    Added specific database usernames for dev and test instead of just using root with no password

--- db/create_databases.sql
+++ db/create_databases.sql
@@ -1,2 +1,4 @@
 create database if not exists racing_on_rails_development;
+grant all privileges on racing_on_rails_development.* to 'ror_development'@'localhost';
 create database if not exists  racing_on_rails_test;
+grant all privileges on racing_on_rails_test.* to 'ror_test'@'localhost';
