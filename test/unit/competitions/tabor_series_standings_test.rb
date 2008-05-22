d40cf7274b44440332ae0155a263142ea41233fb
Scott Willson <scott@butlerpress.com>
2008-05-22 04:11:30 +0000

    Finished simple Tabor Series test

--- test/unit/competitions/tabor_series_standings_test.rb
+++ test/unit/competitions/tabor_series_standings_test.rb
@@ -21,7 +21,8 @@ class TaborSeriesStandingsTest < ActiveSupport::TestCase
     cat_3_race.results.create!(:place => 1, :racer => racers(:weaver))
     cat_3_race.results.create!(:place => 3, :racer => racers(:tonkin))
     
-    masters_race = event_standings.races.create!(:category => categories(:masters_35_plus_women))
+    masters_40_plus_women_category = Category.create!(:name => "Women Masters 40+")
+    masters_race = event_standings.races.create!(:category => masters_40_plus_women_category)
     masters_race.results.create!(:place => 15, :racer => racers(:alice))
     masters_race.results.create!(:place => 16, :racer => racers(:molly))
     
@@ -30,7 +31,7 @@ class TaborSeriesStandingsTest < ActiveSupport::TestCase
     overall_standings = series.standings.first
     assert_equal(8, overall_standings.races.size, "Overall races")
 
-    cat_3_overall_race = overall_standings.races.detect { |race| race.category == categories(:cat_3)}
+    cat_3_overall_race = overall_standings.races.detect { |race| race.category == categories(:cat_3) }
     assert_not_nil(cat_3_overall_race, "Should have Cat 3 overall race")
     assert_equal(2, cat_3_overall_race.results.size, "Cat 3 race results")
     cat_3_overall_race.results(true).sort!
@@ -42,9 +43,16 @@ class TaborSeriesStandingsTest < ActiveSupport::TestCase
     assert_equal("2", result.place, "Cat 3 second result place")
     assert_equal(50, result.points, "Cat 3 second result points")
     assert_equal(racers(:tonkin), result.racer, "Cat 3 second result racer")
+
+    masters_40_plus_women_overall_race = overall_standings.races.detect { |race| race.category == masters_40_plus_women_category }
+    assert_not_nil(masters_40_plus_women_overall_race, "Should have Masters Women overall race")
+    assert_equal(1, masters_40_plus_women_overall_race.results.size, "Masters Women race results")
+    result = masters_40_plus_women_overall_race.results.first
+    assert_equal("1", result.place, "Masters Women first result place")
+    assert_equal(11, result.points, "Masters Women first result points")
+    assert_equal(racers(:alice), result.racer, "Masters Women first result racer")
   end
   
-  # Ensure category names are mapped
   # Ensure skips races from different years
   # test replaces previous standings
   # best 5 of 6
