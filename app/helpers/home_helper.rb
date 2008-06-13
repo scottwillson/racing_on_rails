2472e1e84dd14bea0364288af20fb702d6662801
Scott Willson <scott@butlerpress.com>
2008-06-13 00:46:02 +0000

    Added a 3-column layout and changed dimenstions slighty

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,7 +1,14 @@
 module HomeHelper
   def photo_width_x_height(width, height)
     @photo_width = width
-    @photo_height = height    
-    @wide_photo = (@photo_width > 285)
+    @photo_height = height
+    case @photo_width
+    when 0..285
+      @photo_columns = 1
+    when 286..574
+      @photo_columns = 2
+    else
+      @photo_columns = 3
+    end
   end
 end
