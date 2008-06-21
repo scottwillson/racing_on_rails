4f6310870bf81f947229a8ed2c9e103a644479a9
Scott Willson <scott@butlerpress.com>
2008-06-21 16:55:38 +0000

    Fixed error in subscription mailer fields and temporarily added my address for testing

--- app/models/subscription_mailer.rb
+++ app/models/subscription_mailer.rb
@@ -2,7 +2,8 @@ class SubscriptionMailer < ActionMailer::Base
 
   def subscription_request(subscription)
     @subject    = "Mailing List Subscription: #{subscription.name}"
-    @recipients = 'webmaster@americantrackracing.com'
+    @recipients = 'scott@butlerpress.com'
+    # @recipients = 'webmaster@americantrackracing.com'
     @from       = "#{subscription.name} <#{subscription.email}>"
     body :subscription => subscription
   end
