28e291e8f688e7fdb66da12340f5f3c01f8d6ae0
Scott Willson <scott@butlerpress.com>
2008-06-21 16:58:07 +0000

    Switched back to correct email address

--- app/models/subscription_mailer.rb
+++ app/models/subscription_mailer.rb
@@ -2,8 +2,7 @@ class SubscriptionMailer < ActionMailer::Base
 
   def subscription_request(subscription)
     @subject    = "Mailing List Subscription: #{subscription.name}"
-    @recipients = 'scott@butlerpress.com'
-    # @recipients = 'webmaster@americantrackracing.com'
+    @recipients = 'webmaster@americantrackracing.com'
     @from       = "#{subscription.name} <#{subscription.email}>"
     body :subscription => subscription
   end
