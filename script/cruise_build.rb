f417280284da97b0093aba13af22cd4a13a52e9b
Scott Willson <scott@butlerpress.com>
2008-08-21 17:34:26 +0000

    Put svn co and rake task in same exec statement for unified return code

--- script/cruise_build.rb
+++ script/cruise_build.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 
+# Yes, this could be more abstracted, etc. Choosing explicitness for now.
+# svn co and rake co need to be in same exec, or cruise just ends build after svn returns
+#
+# Intentionally doing svn co and rake cruise in separate processes to ensure that all of the
+# local overrides are loaded by Rake.
+
 project_name = ARGV.first
 
 case project_name
 when "racing_on_rails"
   # Nothing else to get
 when "atra"
-  exec("svn co svn+ssh://butlerpress.com/var/repos/atra/trunk local")
+  exec("svn co svn+ssh://butlerpress.com/var/repos/atra/trunk local && rake cruise")
 else
   raise "Don't know how to build project named: '#{project_name}'"
 end
-
-exec("rake cruise")
