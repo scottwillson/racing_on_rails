17cff5e471c51f7b9330113a5cfa01484622b4bf
Scott Willson <scott@butlerpress.com>
2008-08-21 17:27:14 +0000

    Add ATRA in as a test

--- script/cruise_build.rb
+++ script/cruise_build.rb
@@ -1,3 +1,14 @@
 #!/usr/bin/env ruby
 
-p "My project name is: #{ARGV.inspect}"
+project_name = ARGV.first
+
+case project_name
+when "racing_on_rails"
+  # Nothing else to get
+when "atra"
+  exec("svn co svn+ssh://butlerpress.com/var/repos/atra/trunk local")
+else
+  raise "Don't know how to build project named: '#{project_name}'"
+end
+
+exec("rake cruise")
