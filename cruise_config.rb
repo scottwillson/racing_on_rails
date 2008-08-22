c37ddbb52b7c85cadc8dc145d0e20511ab2d7984
Scott Willson <scott@butlerpress.com>
2008-08-22 02:13:09 +0000

    Add cc.rb trigger for local part of repository

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,4 +1,5 @@
 Project.configure do |project|
   project.email_notifier.emails = ['scott@butlerpress.com']
   project.build_command = "./script/cruise_build.rb #{project.name}"
+  project.triggered_by ChangeInLocalTrigger.new(project)
 end
