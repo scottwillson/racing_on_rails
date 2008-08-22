59cb26d137f0e04e4083323c1cb359fabe28333e
Scott Willson <scott@butlerpress.com>
2008-08-22 23:32:43 +0000

    Removed unused trigger from config

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,5 +1,4 @@
 Project.configure do |project|
   project.email_notifier.emails = ['scott@butlerpress.com']
   project.build_command = "./script/cruise_build.rb #{project.name}"
-  # project.triggered_by ChangeInLocalTrigger.new(project)
 end
