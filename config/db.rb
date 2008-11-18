8452ef652613dd74808d87ff0e7462a85def93df
Scott Willson <scott@butlerpress.com>
2008-11-18 00:48:15 +0000

    Fix a few file existence/return code problems

--- config/db.rb
+++ config/db.rb
@@ -7,29 +7,32 @@ namespace :db do
     db.remote_db_cleanup
   end 
 
-  desc "Dump the current database to a MySQL file" 
+  # Dump the current database to a MySQL file
   task :database_dump, :roles => :db, :only => { :primary => true } do
     load 'config/environment.rb'
     abcs = ActiveRecord::Base.configurations
     run("mysqldump -u #{abcs["production"]["username"]} -p#{abcs["production"]["password"]} --compress --ignore-table=#{abcs["production"]["database"]}.posts #{abcs["production"]["database"]} > db/production.sql")
+    run("rm -f db/production.sql.bz2")
     run("bzip2 db/production.sql")
   end
 
-  desc 'Downloads db/production_data.sql from the remote production environment to your local machine'
+  # Downloads db/production_data.sql from the remote production environment to your local machine
   task :remote_db_download, :roles => :db, :only => { :primary => true } do
+    system "rm -f db/production.sql.bz2"
     get "db/production.sql.bz2", "db/production.sql.bz2"
   end
 
-  desc "Loads the production data downloaded into db/production_data.sql into your local development database" 
+  #Loads the production data downloaded into db/production_data.sql into your local development database
   task :production_data_load, :roles => :db, :only => { :primary => true } do
-    exec "bzip2 -d db/production.sql.bz2"
+    system "rm -f db/production.sql"
+    system "bzip2 -d db/production.sql.bz2"
     load 'config/environment.rb'
     abcs = ActiveRecord::Base.configurations
     `mysql -u #{abcs[RAILS_ENV]["username"]} #{abcs[RAILS_ENV]["database"]} < db/production.sql`
     exec("rm db/production.sql")
   end
 
-  desc 'Cleans up data dump file'
+   #Cleans up data dump file
   task :remote_db_cleanup, :roles => :db, :only => { :primary => true } do  
     run "rm db/production.sql.bz2"
   end 
