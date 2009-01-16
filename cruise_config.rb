e9181eafcd2f920f7a1e9380ae42a90c750fee56
Scott Willson <scott@butlerpress.com>
2009-01-16 22:14:13 +0000

    Add WSBA admin email to cc.rb CI notifications

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,4 +1,4 @@
 Project.configure do |project|
-  project.email_notifier.emails = ['scott@butlerpress.com']
+  project.email_notifier.emails = ['scott@butlerpress.com', "wsbaracing@gmail.com"]
   project.build_command = "./script/cruise_build.rb #{project.name}"
 end
