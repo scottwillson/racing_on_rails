633c327af7beb4a0819dba788db75722238ce9dd
Scott Willson <scott@butlerpress.com>
2009-04-22 22:07:14 +0000

    Hopefully fix UTF8 migration

--- db/migrate/20090422162313_convert_to_utf8.rb
+++ db/migrate/20090422162313_convert_to_utf8.rb
@@ -8,33 +8,34 @@ class ConvertToUtf8 < ActiveRecord::Migration
     
     latin1_dump = 'latin1_dump.sql'
     utf8_dump   = 'utf8_dump.sql'
-
-    print "Dumping database... "
+    
+    say "Dumping database... "
     system "mysqldump --user=#{db_user} --password='#{db_pass}' --add-drop-table --default-character-set=latin1 --insert-ignore --skip-set-charset #{db_name} > #{latin1_dump}"
-    puts "done"
+    say "done"
     
-    print "Converting dump to UTF8... "    
+    say "Converting dump to UTF8... "    
     system "iconv -f ISO-8859-1 -t UTF-8 #{latin1_dump} | sed 's/latin1/utf8/' > #{utf8_dump}"
-    puts "done"
+    say "done"
     
-    print "Recreating database..."
+    say "Recreating database..."
     system "mysql --user=#{db_user} --password='#{db_pass}' --execute=\"DROP DATABASE #{db_name};\""
     system "mysql --user=#{db_user} --password='#{db_pass}' --execute=\"CREATE DATABASE #{db_name} CHARACTER SET utf8 COLLATE utf8_unicode_ci;\""
-    puts "done"
+    say "done"
     
-    test_db_name = db_config[:test][:database]
-    test_db_user = db_config[:test][:username]
-    test_db_pass = db_config[:test][:password] || ''
-    print "Recreating test database..."
+    say "Importing UTF8 dump..."
+    system "mysql --user=#{db_user} --password='#{db_pass}' --default-character-set=utf8 #{db_name} < #{utf8_dump}"
+    say "done"
+    
+    # What could possibly go wrong?
+    say "Recreating test database..."
+    test_db_name = db_name.gsub("development", "test")
+    test_db_user = db_user.gsub("development", "test")
+    test_db_pass = db_pass
     system "mysql --user=#{test_db_user} --password='#{test_db_pass}' --execute=\"DROP DATABASE #{test_db_name};\""
     system "mysql --user=#{test_db_user} --password='#{test_db_pass}' --execute=\"CREATE DATABASE #{test_db_name} CHARACTER SET utf8 COLLATE utf8_unicode_ci;\""
-    puts "done"
+    say "done"
     
-    print "Importing UTF8 dump..."
-    system "mysql --user=#{db_user} --password='#{db_pass}' --default-character-set=utf8 #{db_name} < #{utf8_dump}"
-    puts "done"
-    
-    puts " *** don't forget to delete temp files #{latin1_dump} and #{utf8_dump}"
+    say " *** don't forget to delete temp files #{latin1_dump} and #{utf8_dump}"
   end
 
   def self.down
