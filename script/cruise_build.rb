d6df449fce6ed313e0b8da827bc7b84462a32f5d
Scott Willson <scott@butlerpress.com>
2009-05-03 11:58:12 -0700

    Drop local dir before fetching local

--- script/cruise_build.rb
+++ script/cruise_build.rb
@@ -11,7 +11,7 @@ case project_name
 when "racing_on_rails"
   exec "rake cruise"
 when "aba", "atra", "obra", "wsba"
-  exec "git clone git@butlerpress.com:#{project_name}.git local && rake cruise"
+  exec "rm -rf local && git clone git@butlerpress.com:#{project_name}.git local && rake cruise"
 else
   raise "Don't know how to build project named: '#{project_name}'"
 end
