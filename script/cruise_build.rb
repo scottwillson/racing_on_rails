2935b6fc29d921e992aec629eca6032a8f234c6d
Scott Willson <scott@butlerpress.com>
2009-05-03 10:25:07 -0700

    Try to switch cc.rb local hack from SVN to Git

--- script/cruise_build.rb
+++ script/cruise_build.rb
@@ -1,17 +1,18 @@
 #!/usr/bin/env ruby
 
-# The svn:externals is a bit of a hack, but seems like the easiest solution to pulling in different projects
+# The git clone is a bit of a hack, but seems like the easiest solution to pulling in different projects
 #
-# Intentionally doing svn co and rake cruise in separate processes to ensure that all of the
+# Intentionally doing git clone and rake cruise in separate processes to ensure that all of the
 # local overrides are loaded by Rake.
 
 project_name = ARGV.first
 
 case project_name
 when "racing_on_rails"
-  exec("rake cruise")
+  exec "rake cruise"
 when "aba", "atra", "obra", "wsba"
-  exec(%Q{svn propset "svn:externals" "local svn+ssh://butlerpress.com/var/repos/#{project_name}/trunk" . && rake cruise})
+  exec "git clone git@butlerpress.com:#{application}.git local"
+  exec "rake cruise"
 else
   raise "Don't know how to build project named: '#{project_name}'"
 end
