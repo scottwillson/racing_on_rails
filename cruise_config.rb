884363fc9010a3a452053e628f2f84daf1dec2ac
Scott Willson <scott@butlerpress.com>
2009-05-15 17:07:02 -0700

    Don't need branch in project config anymore?

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,5 +1,4 @@
 Project.configure do |project|
   project.email_notifier.emails = ["scott@butlerpress.com"]
   project.build_command = "./script/cruise_build.rb #{project.name}"
-  project.source_control.branch = "montanacycling"
 end
