e74a20831599e281a849702f2502210920f36aa0
Scott Willson <scott@butlerpress.com>
2009-06-21 20:05:42 -0700

    Make old competition data consistent with newer

---
+++ db/migrate/20090621233142_set_correct_type_for_old_events.rb
@@ -0,0 +1,31 @@
+# TODO CascadeCrossOverall
+# TODO RiderRankings
+# TODO WSBA BARR
+class SetCorrectTypeForOldEvents < ActiveRecord::Migration
+  def self.up
+    Bar.find(:all, :conditions => ["date < ?", Date.new(2009)]).each do |discipline_bar_parent|
+      discipline_bar_parent.children.each do |discipline_bar|
+        discipline_bar.parent = nil
+        discipline_bar.discipline = discipline_bar.name
+        discipline_bar.name = "#{discipline_bar.year} #{discipline_bar.discipline} BAR"
+        discipline_bar.type = "Bar"
+        discipline_bar.save!
+      end
+      discipline_bar_parent.children(true)
+      discipline_bar_parent.destroy
+    end
+    
+    AgeGradedBar.update_all("discipline = 'Age Graded'")
+    
+    if ASSOCIATION.short_name == "OBRA"
+      Event.find([14320, 14706, 14542]).each do |cross_crusade_overall|
+        cross_crusade_overall.type = "CrossCrusadeOverall"
+        cross_crusade_overall.save!
+      end
+      
+      tabor_overall_2008 = Event.find(14319)
+      tabor_overall_2008.type = "TaborOverall"
+      tabor_overall_2008.save!
+    end
+  end
+end
