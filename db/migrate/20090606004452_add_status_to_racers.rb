6ab998908f2e9db833364448b1a72d6d3187de66
Scott Willson <scott@butlerpress.com>
2009-06-23 19:37:58 -0700

    Fix outdated reference to racers in migration

--- db/migrate/20090606004452_add_status_to_racers.rb
+++ db/migrate/20090606004452_add_status_to_racers.rb
@@ -2,10 +2,10 @@ class AddStatusToRacers < ActiveRecord::Migration
   def self.up
     return if ASSOCIATION.short_name == "MBRA"
 
-    add_column :racers, :status, :string
+    add_column :people, :status, :string
   end
 
   def self.down
-    remove_column :racers, :status
+    remove_column :people, :status
   end
 end
