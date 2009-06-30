81c0a10e5e3798cdc772ecafb1550364a3ed67a7
Al Pendergrass <al.pendergrass@gmail.com>
2009-06-29 20:57:46 -0600

    fix adding usac fields

--- db/migrate/20090607004047_add_usac_fields_to_results.rb
+++ db/migrate/20090607004047_add_usac_fields_to_results.rb
@@ -1,14 +1,21 @@
 class AddUsacFieldsToResults < ActiveRecord::Migration
   def self.up
-    return if ASSOCIATION.short_name == "MBRA"
-    add_column :results, :gender, :string, :limit => 8
-    add_column :results, :category_class, :string, :limit => 16
-    add_column :results, :age_group, :string, :limit => 16
+    if ASSOCIATION.short_name == "MBRA"
+      rename_column :results, :ages, :age_group
+    else
+      add_column :results, :gender, :string, :limit => 8
+      add_column :results, :category_class, :string, :limit => 16
+      add_column :results, :age_group, :string, :limit => 16
+    end
   end
 
   def self.down
-    remove_column :results, :gender
-    remove_column :results, :category_class
-    remove_column :results, :age_group
+    if ASSOCIATION.short_name == "MBRA"
+      rename_column :results, :age_group, :ages
+    else
+      remove_column :results, :gender
+      remove_column :results, :category_class
+      remove_column :results, :age_group
+    end
   end
 end
