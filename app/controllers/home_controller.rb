ce6a3dcc380c525f85c8ab8a03b4436f716eba4f
Al Pendergrass <al.pendergrass@gmail.com>
2009-07-09 16:43:14 -0600

    fix news selection for home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -24,7 +24,7 @@ class HomeController < ApplicationController
     @news_category = ArticleCategory.find( :all, :conditions => ["name = 'news'"] )
     @recent_news = Article.find(
       :all,
-      :conditions => ['updated_at > ? and article_category_id = ?', cutoff, @news_category],
+      :conditions => ['created_at > ? and article_category_id = ?', cutoff, @news_category],
       :order => 'created_at desc'
     )
 
