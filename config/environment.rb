294056e580ee6b835859dc6e4eb1327cc73dbbd0
Scott Willson <scott@butlerpress.com>
2009-07-28 11:13:38 -0700

    Add gem lib name for Selenium client

--- config/environment.rb
+++ config/environment.rb
@@ -18,7 +18,7 @@ Rails::Initializer.run do |config|
   config.gem "fastercsv"
   config.gem "pdf-writer", :lib => "pdf/writer"
   # For tests
-  config.gem "selenium-client", :lib => false
+  config.gem "selenium-client", :lib => "selenium/client"
   
   # Racing on Rails has many foreign key constraints, so :sql is required
   config.active_record.schema_format = :sql
