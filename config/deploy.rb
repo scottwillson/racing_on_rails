4ec1d21a40895001d3f8986a446a02fab55ace0e
Scott Willson <scott@butlerpress.com>
2009-10-15 16:51:20 -0700

    Use git remote cache for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,10 +2,11 @@ require 'mongrel_cluster/recipes'
 load "config/db"
 load "local/config/deploy.rb" if File.exists?("local/config/deploy.rb")
 
+set :scm, "git"
 set :repository, "git://github.com/scottwillson/racing_on_rails.git"
 set :site_local_repository, "git@butlerpress.com:#{application}.git"
 set :branch, "master"
-set :scm, "git"
+set :deploy_via, :remote_cache
 
 set :deploy_to, "/usr/local/www/rails/#{application}"
 
