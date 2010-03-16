96deebb59465605022558f971efb70cd61add922
Scott Willson <scott@butlerpress.com>
2010-03-16 15:15:18 -0700

    Brute-force the cruise script slashes

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,5 +1,9 @@
 Project.configure do |project|
   project.email_notifier.emails = ["scott@butlerpress.com"]
   # project.email_notifier.emails = ["scott@butlerpress.com", "al.pendergrass@gmail.com", "ryan@cyclocrazed.com"]
-  project.build_command = File.expand_path("./script/cruise_build.rb #{project.name}")
+  if RUBY_PLATFORM[/mswin/]
+    project.build_command = "ruby script\\cruise_build.rb #{project.name}"
+  else
+    project.build_command = "./script/cruise_build.rb #{project.name}"
+  end
 end
