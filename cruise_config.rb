9e7f78b84bd84b360c5b497f389ab2e7b9cbc96b
unknown <sw@.(none)>
2010-03-16 15:02:31 -0700

    Use File.expand_path for cruise build path to try and get Windows working

--- cruise_config.rb
+++ cruise_config.rb
@@ -1,5 +1,5 @@
 Project.configure do |project|
   project.email_notifier.emails = ["scott@butlerpress.com"]
   # project.email_notifier.emails = ["scott@butlerpress.com", "al.pendergrass@gmail.com", "ryan@cyclocrazed.com"]
-  project.build_command = "./script/cruise_build.rb #{project.name}"
+  project.build_command = File.expand_path("./script/cruise_build.rb #{project.name}")
 end
