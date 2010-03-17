d2bbb938086ba9e3ce0209128ba80f1e3c20c5c8
Scott Willson <scott@butlerpress.com>
2010-03-17 15:49:57 -0700

    Skip drag-and-drop on Chrome

--- test/acceptance/admin/categories_test.rb
+++ test/acceptance/admin/categories_test.rb
@@ -8,12 +8,14 @@ class CategoriesTest < WebDriverTestCase
 
     masters_35_plus_id = Category.find_by_name("Masters 35+").id
     assert_element :tag_name => "div"
-    assert_element :css => "div#unknown_category_root div#category_#{masters_35_plus_id}"
-    assert_no_element :css => "div#category_root div#category_#{masters_35_plus_id}"
+    assert_element :xpath => "//div[@id='unknown_category_root']//div[@id='category_#{masters_35_plus_id}']"
+    assert_no_element :xpath => "//div[@id='category_root']//div[@id='category_#{masters_35_plus_id}']"
 
-    drag_and_drop_by(-400, 116, :css => "div#unknown_category_root div#category_#{masters_35_plus_id}")
-    wait_for_no_element :css => "div#unknown_category_root div#category_#{masters_35_plus_id}"
-    wait_for_element :css => "div#category_root div#category_#{masters_35_plus_id}"
-    assert_element :css => "div#category_root div#category_#{masters_35_plus_id}"
+    # No drag-and-drop in chrome
+    unless driver.bridge.browser == :chrome
+      drag_and_drop_by(-400, 116, :xpath => "//div[@id='unknown_category_root']//div[@id='category_#{masters_35_plus_id}']")
+      wait_for_no_element :xpath => "//div[@id='unknown_category_root']//div[@id='category_#{masters_35_plus_id}']"
+      wait_for_element :xpath => "//div[@id='category_root']//div[@id='category_#{masters_35_plus_id}']"
+    end
   end
 end
