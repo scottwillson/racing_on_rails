2faa2e502014b23a3065bf53fd71ea1694e632a3
Scott Willson <scott@butlerpress.com>
2010-05-03 10:57:29 -0700

    Add more time formats

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,4 +1,6 @@
 Date::DATE_FORMATS[:long_with_week_day] = "%a, %B %d, %Y"
 Date::DATE_FORMATS[:short_with_week_day] = "%a %m/%d"
-Date::DATE_FORMATS[:mdy] = "%m/%d/%y"
+Time::DATE_FORMATS[:mdy] = "%m/%d/%y"
 Date::DATE_FORMATS[:my] = "%m/%y"
+Time::DATE_FORMATS[:long_and_friendly_time] = "%I:%M %p"
+Time::DATE_FORMATS[:long_and_friendly] = "%B %d, %Y"
