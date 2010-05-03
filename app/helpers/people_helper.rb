69fad1938c8cf4a6d4aac6f2a50dd6ceb51d5ac7
Scott Willson <scott@butlerpress.com>
2010-05-03 08:59:43 -0700

    Add person pronoun helper

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -10,4 +10,12 @@ module PeopleHelper
   def official?
     current_person.try :official?
   end
+
+  def pronoun(person, other_person)
+    if person == other_person
+      "me"
+    else
+      person.name
+    end
+  end
 end
