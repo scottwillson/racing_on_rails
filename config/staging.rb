ebd983638acdf644b67573f050165fc19a71dd20
Scott Willson <scott@butlerpress.com>
2010-05-20 11:23:19 -0700

    Create robots.txt for staging

--- config/staging.rb
+++ config/staging.rb
@@ -5,3 +5,11 @@ load "config/db"
 load "local/config/staging.rb" if File.exists?("local/config/staging.rb")
 
 set :rails_env, "staging"
+
+namespace :deploy do
+  task :robots do
+    run "echo \"User-Agent: *\nDisallow: /\n\" > #{release_path}/public/robots.txt"
+  end
+end
+
+after "deploy:update_code", "deploy:local_code", "deploy:copy_cache", "deploy:robots"
