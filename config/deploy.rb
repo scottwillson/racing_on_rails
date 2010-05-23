0d872642518299050b96ac9e4b2516df2055ca50
Scott Willson <scott@butlerpress.com>
2010-05-23 14:21:07 -0700

    Fix error in deploy when missing :site_local_repository_branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ load "local/config/deploy.rb" if File.exists?("local/config/deploy.rb")
 set :scm, "git"
 set :repository, "git://github.com/scottwillson/racing_on_rails.git"
 set :site_local_repository, "git@butlerpress.com:#{application}.git"
+set :site_local_repository_branch, nil
 set :branch, "master"
 set :deploy_via, :remote_cache
 
