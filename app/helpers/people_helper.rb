e12d9673a654c79a08106f98b7a52f509696b21e
Scott Willson <scott@butlerpress.com>
2010-06-06 15:56:53 -0700

    be smarter about return URL after creating editor

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -11,6 +11,25 @@ module PeopleHelper
     current_person.try :official?
   end
 
+  def editor_for?(person, *attributes, &block)
+    subject = case person
+    when Person
+      person
+    else
+      person.try(:person)
+    end
+      
+    if (attributes && (attributes.any? { |a| person[a].blank? || subject[a].blank? })) || current_person.can_edit?(subject)
+      if block
+        concat(capture(&block))
+      else
+        true
+      end
+    else
+      false
+    end
+  end
+
   def pronoun(person, other_person)
     if person == other_person
       "me"
