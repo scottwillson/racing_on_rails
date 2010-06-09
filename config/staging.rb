42f6c9e483193f7f3dc2b813125184287451b3bb
Scott Willson <scott@butlerpress.com>
2010-06-09 13:01:45 -0700

    Restart Unicorn in staging

--- config/staging.rb
+++ config/staging.rb
@@ -7,6 +7,18 @@ load "local/config/staging.rb" if File.exists?("local/config/staging.rb")
 set :rails_env, "staging"
 
 namespace :deploy do
+  task :start do
+    run "/usr/local/etc/rc.d/unicorn start"
+  end
+  
+  task :stop do
+    run "/usr/local/etc/rc.d/unicorn stop"
+  end
+  
+  task :restart do
+    run "/usr/local/etc/rc.d/unicorn restart"
+  end
+  
   task :robots do
     run "echo \"User-Agent: *\nDisallow: /\n\" > #{release_path}/public/robots.txt"
   end
