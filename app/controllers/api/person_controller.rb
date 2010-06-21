94055727ca82bf0a3a7e5f90ac52efcd1c8f1241
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-06-21 05:51:40 -0700

    Updated people API search to use the same methods as the admin controller

--- app/controllers/api/person_controller.rb
+++ app/controllers/api/person_controller.rb
@@ -1,17 +1,27 @@
 class Api::PersonController < ApplicationController
   def index
-    conditions = { }
-    if params[:license]
-      conditions[:license] = params[:license]
+    people = []
+    select = [:first_name, :last_name, :date_of_birth, :license, :gender]
+
+    # name
+    if params[:name]
+      people = people + Person.find_all_by_name_like(params[:name], :select => select.join(','))
     end
-    people = Person.all(:conditions => conditions)
+
+    # license
+    if params[:license]
+      people = people + Person.find_all_by_license(params[:license], :select => select.join(','))
+    end
+
+    # order
+    people.stable_sort_by(:first_name).stable_sort_by(:last_name)
+
+    # limit
+    people = people[0, 5]
+
     respond_to do |format|
-      format.xml {
-        render :xml => people.to_xml(
-          :only    => [:first_name, :last_name, :date_of_birth, :license, :gender],
-          :include => [:team]
-        )
-      }
+      format.xml { render :xml => people.to_xml }
+      format.json { render :json => people.to_json }
     end
   end
 end
