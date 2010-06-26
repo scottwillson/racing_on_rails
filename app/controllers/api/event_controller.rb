f8b8f300190934daa6acb747bbdfa3d98ca1a972
Kris Wallsmith <kris.wallsmith@gmail.com>
2010-06-25 23:25:07 -0700

    Optimized events service and added more associations.

--- app/controllers/api/event_controller.rb
+++ app/controllers/api/event_controller.rb
@@ -1,11 +1,16 @@
 class Api::EventController < ApplicationController
   def index
-    events = Event.paginate(:order => :date, :page => params[:page])
+    events = Event.paginate(
+      :include  => { :races => [ :category ] },
+      :order    => "date DESC",
+      :page     => params[:page],
+      :per_page => 10
+    )
 
     only = [:id, :parent_id, :name, :type, :discipline, :city, :cancelled, :beginner_friendly]
-    for_include = {
+    includes = {
       :races => {
-        :only => [:id, :city, :distance, :state, :laps, :field_size, :time, :finishers, :notes],
+        :only => [:id, :distance, :city, :state, :laps, :field_size, :time, :finishers, :notes],
         :include => {
           :category => {
             :only => [:id, :name, :ages_begin, :ages_end, :friendly_param]
@@ -15,8 +20,8 @@ class Api::EventController < ApplicationController
     }
 
     respond_to do |format|
-      format.xml { render :xml => events.to_xml(:only => only, :include => for_include) }
-      format.json { render :json => events.to_json(:only => only, :include => for_include) }
+      format.xml { render :xml => events.to_xml(:only => only, :include => includes) }
+      format.json { render :json => events.to_json(:only => only, :include => includes) }
     end
   end
 end
--- test/functional/api/event_controller_test.rb
+++ test/functional/api/event_controller_test.rb
@@ -5,7 +5,34 @@ class Api::EventControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_equal "application/xml", @response.content_type
-    assert_select "discipline", "Time Trial"
-    assert_select "name", "Jack Frost"
+    [
+      "record > beginner-friendly",
+      "record > cancelled",
+      "record > city",
+      "record > discipline",
+      "record > id",
+      "record > name",
+      "record > parent-id",
+      "record > type",
+      "record > races",
+      "races > race",
+      "race > city",
+      "race > distance",
+      "race > field-size",
+      "race > finishers",
+      "race > id",
+      "race > laps",
+      "race > notes",
+      "race > state",
+      "race > time",
+      "race > category",
+      "category > ages-begin",
+      "category > ages-end",
+      "category > friendly-param",
+      "category > id",
+      "category > name"
+    ].each do |key|
+      assert_select key
+    end
   end
 end
