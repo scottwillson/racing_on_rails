82c2dfe6a642418bc4a9a5a5a3ad82f09bfdb2e7
Scott Willson <scott.willson@gmail.com>
2010-12-03 11:02:58 -0800

    Add Selenium dependencies

--- config/environments/acceptance.rb
+++ config/environments/acceptance.rb
@@ -7,7 +7,9 @@ config.action_controller.perform_caching             = true
 config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 
-config.gem "json_pure"
+config.gem "ffi"
+config.gem "childprocess"
+config.gem "json_pure", :lib => "json"
 config.gem "selenium-webdriver"
 config.gem "mocha"
 config.gem "minitest", :lib => "minitest/unit"
