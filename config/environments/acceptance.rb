7aafe5fffec943827de7c7fb35bfa7e7e0b33e0e
Scott Willson <scott.willson@gmail.com>
2010-12-03 09:22:30 -0800

    Vendor json_pure gem

--- config/environments/acceptance.rb
+++ config/environments/acceptance.rb
@@ -7,6 +7,7 @@ config.action_controller.perform_caching             = true
 config.action_controller.allow_forgery_protection    = false
 config.action_mailer.delivery_method = :test
 
+config.gem "json_pure"
 config.gem "selenium-webdriver"
 config.gem "mocha"
 config.gem "minitest", :lib => "minitest/unit"
