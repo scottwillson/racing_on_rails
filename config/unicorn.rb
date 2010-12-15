f7fc39aaf014b344e9ba1fa19e4dae21fcace313
Scott Willson <scott.willson@gmail.com>
2010-12-14 19:41:34 -0800

    Add basic Unicorn config

---
+++ config/unicorn.rb
@@ -0,0 +1,28 @@
+# unicorn_rails -c /usr/local/etc/unicorn/wsba.rb -E production -D
+
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+worker_processes 2
+preload_app true
+timeout 90
+
+# Using this method we get 0 downtime deploys.
+before_fork do |server, worker|
+  old_pid = RAILS_ROOT + '/tmp/pids/unicorn.pid.oldbin'
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+
+  # per-process listener ports for debugging/admin:
+  addr = "127.0.0.1:#{9035 + worker.nr}"
+  server.listen(addr, :tries => -1, :delay => 5, :backlog => 128)
+  worker.user('app', 'rail') if Process.euid == 0
+end
