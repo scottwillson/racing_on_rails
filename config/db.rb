225ef06d5687404277602ef42bc8a9800f060b7d
Scott Willson <scott.willson@gmail.com>
2011-01-11 18:56:42 -0800

    Fix DB Rake task in 1.9

--- config/db.rb
+++ config/db.rb
@@ -11,7 +11,7 @@ namespace :db do
   task :database_dump, :roles => :db, :only => { :primary => true } do
     run("rm -f db/production.sql") if File.exists?("db/production.sql") 
     run("rm -f db/production.sql.bz2") if File.exists?("db/production.sql.bz2") 
-    load("config/environment.rb") unless defined?(Rails)
+    load File.expand_path("../../config/environment.rb", __FILE__)
     abcs = ActiveRecord::Base.configurations
     run("mkdir -p db")
     run("mysqldump -u #{abcs["production"]["username"]} -p#{abcs["production"]["password"]} --compress --ignore-table=#{abcs["production"]["database"]}.posts #{abcs["production"]["database"]} > db/production.sql")
@@ -30,7 +30,7 @@ namespace :db do
   # By default, delete downloaded SQL. To keep it, set SAVE=true. Example:
   # cap production_data_refresh SAVE=true
   task :production_data_load, :roles => :db, :only => { :primary => true } do
-    load("config/environment.rb") unless defined?(Rails)
+    load File.expand_path("../../config/environment.rb", __FILE__)
     abcs = ActiveRecord::Base.configurations
     dev_db = abcs[RAILS_ENV]["database"]
     `mysql -u #{abcs[RAILS_ENV]["username"]} -e 'drop database if exists #{dev_db}'`
@@ -39,7 +39,6 @@ namespace :db do
       system "rm -f db/production.sql" if File.exists?("db/production.sql")
       system "bzip2 -d db/production.sql.bz2"
     end
-    load("config/environment.rb") unless defined?(Rails)
     `mysql -u #{abcs[RAILS_ENV]["username"]} #{dev_db} < db/production.sql`
     exec("rm db/production.sql") if File.exists?("db/production.sql") && ENV["SAVE"].nil?
   end
