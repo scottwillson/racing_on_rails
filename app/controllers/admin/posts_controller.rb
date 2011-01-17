6a9c2aaf203adca83e0d89cd6529344e98e14f83
Scott Willson <scott.willson@gmail.com>
2011-01-16 18:57:04 -0800

    Add edit and create

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -7,5 +7,21 @@ module Admin
       @mailing_list = MailingList.find(params[:mailing_list_id])
       @posts = @mailing_list.posts
     end
+    
+    def new
+      @mailing_list = MailingList.find(params[:mailing_list_id])
+      @post = @mailing_list.posts.build
+      render :edit
+    end
+    
+    def create
+      @mailing_list = MailingList.find(params[:mailing_list_id])
+      @post = @mailing_list.posts.build(params[:post])
+      if @post.save
+        redirect_to edit_admin_mailing_list_post_path(@mailing_list, @post)
+      else
+        render :edit
+      end
+    end
   end
 end
--- test/functional/admin/posts_controller_test.rb
+++ test/functional/admin/posts_controller_test.rb
@@ -21,4 +21,26 @@ class Admin::PostsControllerTest < ActionController::TestCase
     get :index, :mailing_list_id => mailing_lists(:obra_chat).to_param
     assert_redirected_to new_person_session_url(secure_redirect_options)
   end
+  
+  def test_new
+    login_as :administrator
+    get :new, :mailing_list_id => mailing_lists(:obra_chat).to_param
+    assert_response :success
+  end
+  
+  def test_create
+    login_as :administrator
+    post :create, :mailing_list_id => mailing_lists(:obra_chat).to_param, :post => {
+      :from_name => "Mike Murray",
+      :from_email_address => "mmurray@obra.org",
+      :subject => "No More Masters Races",
+      :body => "That is all",
+      "date(1i)"=>"2009",
+      "date(2i)"=>"11",
+      "date(3i)"=>"22"
+    }
+    assert_not_nil assigns(:post), @post
+    assert assigns(:post).errors.empty?, assigns(:post).errors.full_messages.join(", ")
+    assert_redirected_to edit_admin_mailing_list_post_path(mailing_lists(:obra_chat), assigns(:post))
+  end
 end
