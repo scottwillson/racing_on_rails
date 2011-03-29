1490fe224257fa1e0e0179d6123fcea7728b717f
Scott Willson <scott.willson@gmail.com>
2011-03-29 15:45:49 -0700

    Use new mailer API

--- app/models/editor_request_mailer.rb
+++ app/models/editor_request_mailer.rb
@@ -3,19 +3,27 @@ class EditorRequestMailer < ActionMailer::Base
     # Not thread-safe. Won't work for multiple associations.
     ActionMailer::Base.default_url_options[:host] = RacingAssociation.current.rails_host
     
-    recipients    "#{editor_request.editor.name} <#{editor_request.editor.email}>"
-    from          "#{RacingAssociation.current.short_name} Help <#{RacingAssociation.current.email}>"
-    subject       "#{editor_request.person.name} #{RacingAssociation.current.short_name} account access granted"
-    body          :editor_request => editor_request, :edit_url => edit_person_url(editor_request.person)
+    @editor_request = editor_request
+    @edit_url = edit_person_url(editor_request.person)
+
+    mail(
+      :to => "#{editor_request.editor.name} <#{editor_request.editor.email}>",
+      :from => "#{RacingAssociation.current.short_name} Help <#{RacingAssociation.current.email}>",
+      :subject => "#{editor_request.person.name} #{RacingAssociation.current.short_name} account access granted"
+    )
   end
   
-  def request(editor_request)
+  def editor_request(editor_request)
     # Not thread-safe. Won't work for multiple associations.
     ActionMailer::Base.default_url_options[:host] = RacingAssociation.current.rails_host
     
-    recipients    "#{editor_request.person.name} <#{editor_request.person.email}>"
-    from          "#{RacingAssociation.current.short_name} Help <#{RacingAssociation.current.email}>"
-    subject       "#{editor_request.editor.name} would like access to your #{RacingAssociation.current.short_name} account"
-    body          :editor_request => editor_request, :edit_url => edit_person_editor_request_url(editor_request.person, editor_request.token)
+    @editor_request = editor_request
+    @edit_url = edit_person_editor_request_url(editor_request.person, editor_request.token)
+
+    mail(
+      :to => "#{editor_request.person.name} <#{editor_request.person.email}>",
+      :from => "#{RacingAssociation.current.short_name} Help <#{RacingAssociation.current.email}>",
+      :subject => "#{editor_request.editor.name} would like access to your #{RacingAssociation.current.short_name} account"
+    )
   end
 end
