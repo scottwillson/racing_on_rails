6d984a0509a90848c029668f2ad062e072ce9dc6
Scott Willson <scott.willson@gmail.com>
2011-04-05 08:44:51 -0700

    Look in the correct column

--- test/acceptance/admin/velodromes_test.rb
+++ test/acceptance/admin/velodromes_test.rb
@@ -7,10 +7,10 @@ class VelodromesTest < WebDriverTestCase
 
     open "/admin/velodromes"
 
-    assert_table("velodromes_table", 1, 0, /^Alpenrose Dairy/)
-    assert_table("velodromes_table", 1, 1, /^http:\/\/www.obra.org\/track\//)
-    assert_table("velodromes_table", 2, 0, /^Valley Preferred Cycling Center/)
-    assert_table("velodromes_table", 2, 1, /^http:\/\/www.lvvelo.org\//)
+    assert_table("velodromes_table", 1, 1, /^Alpenrose Dairy/)
+    assert_table("velodromes_table", 1, 2, /^http:\/\/www.obra.org\/track\//)
+    assert_table("velodromes_table", 2, 1, /^Valley Preferred Cycling Center/)
+    assert_table("velodromes_table", 2, 2, /^http:\/\/www.lvvelo.org\//)
 
     click "velodrome_#{Velodrome.find_by_name('Valley Preferred Cycling Center').id}_website"
     wait_for_element :css => "form.editor_field input"
@@ -20,7 +20,7 @@ class VelodromesTest < WebDriverTestCase
 
     refresh
     wait_for_element "velodromes_table"
-    assert_table("velodromes_table", 2, 1, /^http:\/\/example.com/)
+    assert_table("velodromes_table", 2, 2, /^http:\/\/example.com/)
 
     click "edit_#{Velodrome.find_by_name('Valley Preferred Cycling Center').id}"
     wait_for_element "velodrome_name"
