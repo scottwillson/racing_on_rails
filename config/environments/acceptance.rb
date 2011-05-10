3c1c86cd41ee621aea371314ad1d6c97a1feef1d
Scott Willson <scott.willson@gmail.com>
2011-05-10 09:17:32 -0700

    Remove deprecated config

--- config/environments/acceptance.rb
+++ config/environments/acceptance.rb
@@ -1,17 +1,11 @@
-config.cache_classes = true
-config.whiny_nils = true
+RacingOnRails::Application.configure do
+  config.cache_classes = true
+  config.whiny_nils = true
+  config.active_support.deprecation = :stderr
 
-config.action_controller.consider_all_requests_local = true
-config.action_controller.perform_caching             = true
+  config.consider_all_requests_local = true
+  config.action_controller.perform_caching             = true
 
-config.action_controller.allow_forgery_protection    = false
-config.action_mailer.delivery_method = :test
-
-# Selenium has specific requirements
-config.gem "rubyzip", :lib => "zip/zip"
-config.gem "ffi", :version => "0.6.3"
-config.gem "childprocess", :version => "0.1.4"
-config.gem "json_pure", :lib => "json"
-config.gem "selenium-webdriver"
-config.gem "mocha"
-config.gem "minitest", :lib => "minitest/unit"
+  config.action_controller.allow_forgery_protection    = false
+  config.action_mailer.delivery_method = :test
+end
--- config/environments/performance.rb
+++ config/environments/performance.rb
@@ -2,7 +2,7 @@ config.cache_classes = true
 config.whiny_nils = true
 
 # Show full error reports and disable caching
-config.action_controller.consider_all_requests_local = false
+config.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
 config.action_view.debug_rjs                         = true
