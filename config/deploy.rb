6fb5e350884e11caf26af3131940d30df6f1de0c
Scott Willson <scott.willson@gmail.com>
2011-06-05 18:07:59 -0700

    Try Bundler again in prod

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,5 +75,30 @@ namespace :deploy do
   end
 end
 
-after "deploy:rollback:revision"
-after "deploy:update_code", "deploy:local_code", "deploy:symlinks", "deploy:copy_cache"
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(release_path, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+
+  task :install, :roles => :app do
+    run "cd #{release_path} && bundle install --deployment --without test development acceptance"
+
+    on_rollback do
+      if previous_release
+        run "cd #{previous_release} && bundle install --deployment --without test development acceptance"
+      else
+        logger.important "no previous release to rollback to, rollback of bundler:install skipped"
+      end
+    end
+  end
+
+  task :bundle_new_release, :roles => :db do
+    bundler.create_symlink
+    bundler.install
+  end
+end
+
+after "deploy:rollback:revision", "bundler:install"
+after "deploy:update_code", "bundler:bundle_new_release", "deploy:local_code", "deploy:symlinks", "deploy:copy_cache"
