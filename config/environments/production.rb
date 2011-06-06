5d0e15335234ec8197693092980c8b6506e377b6
Scott Willson <scott.willson@gmail.com>
2011-06-05 19:12:44 -0700

    Require crufty syslog_logger explicitly

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,5 +21,6 @@ config.action_view.cache_template_loading            = true
 # Disable delivery errors, bad email addresses will be ignored
 # config.action_mailer.raise_delivery_errors = false
 
+require "syslog_logger"
 config.logger = ::SyslogLogger.new("racing_on_rails")
 config.logger.level = ::Logger::INFO
