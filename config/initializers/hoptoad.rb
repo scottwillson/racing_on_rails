76f860f39b23caca3f49b7818e2a72ff368001a7
Scott Willson <scott.willson@gmail.com>
2011-08-06 18:25:55 -0700

    No Hoptoad in acceptance

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,3 +1,5 @@
-HoptoadNotifier.configure do |config|
-  config.api_key = "670155d8819071244378479137fb73bb"
+unless Rails.env.acceptance?
+  HoptoadNotifier.configure do |config|
+    config.api_key = '670155d8819071244378479137fb73bb'
+  end
 end
