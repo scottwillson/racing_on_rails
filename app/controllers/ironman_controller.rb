2ec3984d969e9e0b7a13a8c3cb1ea9238a2277fa
Scott Willson <scott.willson@gmail.com>
2011-09-20 19:08:27 -0700

    Ignore bogus Ironman page params

--- app/controllers/ironman_controller.rb
+++ app/controllers/ironman_controller.rb
@@ -3,12 +3,16 @@ class IronmanController < ApplicationController
     @year = params['year'] || Date.today.year.to_s
     date = Date.new(@year.to_i, 1, 1)
     @ironman = Ironman.first(:conditions => ['date = ?', date])
+
+    page = params['page'].to_i rescue 1
+    page = 1 if page < 1
+    
     if @ironman && !@ironman.races.empty?
       @results = Result.paginate( :conditions => ['race_id = ?', @ironman.races.first.id],
                                   :include => [:person],
                                   :order => 'cast(place as signed)',
                                   :limit  =>  200,
-                                  :page =>  params['page']
+                                  :page =>  page
                                 )
     end
     @years = Ironman.years
--- test/functional/competitions/ironman_controller_test.rb
+++ test/functional/competitions/ironman_controller_test.rb
@@ -2,7 +2,8 @@ require File.expand_path("../../../test_helper", __FILE__)
 
 # :stopdoc:
 class IronmanControllerTest < ActionController::TestCase
-  def test_index
+  def setup
+    super
     big_team = Team.create(:name => "T" * 60)
     weaver = people(:weaver)
     weaver.team = big_team
@@ -10,16 +11,38 @@ class IronmanControllerTest < ActionController::TestCase
     weaver.first_name = "f" * 60
     weaver.last_name = "T" * 60
 
-    Ironman.calculate!(2004)
+    Ironman.calculate! 2004
     Ironman.calculate!
+  end
 
+  def test_index_with_year
     get(:index, :year => "2004")
     assert_response(:success)
     assert_template("ironman/index")
     assert_not_nil(assigns["ironman"], "Should assign ironman")
     assert_not_nil(assigns["year"], "Should assign year")
     assert_not_nil(assigns["years"], "Should assign years")
+  end
 
+  def test_index_with_page
+    get :index, :page => "2", :year => "2004"
+    assert_response(:success)
+    assert_template("ironman/index")
+    assert_not_nil(assigns["ironman"], "Should assign ironman")
+    assert_not_nil(assigns["year"], "Should assign year")
+    assert_not_nil(assigns["years"], "Should assign years")
+  end
+
+  def test_index_with_bogus_page
+    get :index, :page => "http", :year => "2004"
+    assert_response(:success)
+    assert_template("ironman/index")
+    assert_not_nil(assigns["ironman"], "Should assign ironman")
+    assert_not_nil(assigns["year"], "Should assign year")
+    assert_not_nil(assigns["years"], "Should assign years")
+  end
+
+  def test_index
     get :index
     assert_response(:success)
     assert_template("ironman/index")
