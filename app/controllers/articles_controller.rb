b6bef6d85298b79a98599ff9cf8abc0ec64b6e9a
Al Pendergrass <al.pendergrass@gmail.com>
2012-01-17 22:18:41 -0700

    Changes to support De-SPAing MBRA site.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,6 @@ class ArticlesController < ApplicationController
   def show
     expires_in 1.hour, :public => true
     @article = Article.find(params[:id])
+    render :layout => !request.xhr?
   end
 end
