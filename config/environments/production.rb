30d9071c6d6593d8493a74179a69a4e5019a7647
Scott Willson <scott.willson@gmail.com>
2012-02-09 10:37:59 -0800

    Log to local4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,6 @@ RacingOnRails::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.logger = ::SyslogLogger.new("racing_on_rails", Syslog::LOG_LOCAL7)
+  config.logger = ::SyslogLogger.new("racing_on_rails", Syslog::LOG_LOCAL4)
   config.logger.level = ::Logger::INFO
 end
