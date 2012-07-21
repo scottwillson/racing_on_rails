8ec193a6c35c2f781d59a6f2eaf2b85e6309ddd7
Scott Willson <scott.willson@gmail.com>
2012-07-21 07:41:39 -0700

    Synch with OBRA settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,10 @@
 RacingOnRails::Application.configure do
-  config.cache_classes = false
-  config.whiny_nils = true
-  config.consider_all_requests_local = true
-  config.action_controller.perform_caching = false
+  config.action_controller.perform_caching   = false
   config.action_mailer.raise_delivery_errors = false
-  config.active_support.deprecation = :log
-  config.assets.compress = false
-  config.assets.debug = true
+  config.active_support.deprecation          = :log
+  config.assets.compress                     = false
+  config.assets.debug                        = true
+  config.cache_classes                       = false
+  config.consider_all_requests_local         = true
+  config.whiny_nils                          = true
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,31 +1,14 @@
 RacingOnRails::Application.configure do
-  config.cache_classes = true
-
-  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'
-
-  config.assets.compress = true
-  config.assets.css_compressor = :yui
-  config.assets.js_compressor = :uglifier
-
-  config.serve_static_assets = false
-  config.assets.compile = false
-
-  # Enable serving of images, stylesheets, and javascripts from an asset server
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable threaded mode
-  # config.threadsafe!
-
-  config.i18n.fallbacks = true
-
-  config.active_support.deprecation = :notify
-  
-  config.logger = Logger::Syslog.new("racing_on_rails", Syslog::LOG_LOCAL4)
-  config.logger.level = ::Logger::INFO
+  config.active_support.deprecation        = :notify
+  config.assets.compress                   = true
+  config.assets.css_compressor             = :yui
+  config.assets.js_compressor              = :uglifier
+  config.cache_classes                     = true
+  config.consider_all_requests_local       = false
+  config.i18n.fallbacks                    = true
+  config.logger                            = Logger::Syslog.new("racing_on_rails", Syslog::LOG_LOCAL4)
+  config.logger.level                      = ::Logger::INFO
+  config.serve_static_assets               = false
 end
