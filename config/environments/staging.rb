91dd7fd5701a1740422d4db9d5cf0bc856c82d46
Scott Willson <scott.willson@gmail.com>
2012-08-28 14:55:26 -0700

    Fix staging environment for 3.2

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,10 +1,12 @@
-config.cache_classes = true
-config.whiny_nils = true
+RacingOnRails::Application.configure do
+  config.cache_classes = true
+  config.whiny_nils = true
 
-# Show full error reports and disable caching
-config.consider_all_requests_local = true
-config.action_controller.perform_caching             = true
-config.action_view.cache_template_loading            = true
+  # Show full error reports and disable caching
+  config.consider_all_requests_local = true
+  config.action_controller.perform_caching             = true
+  config.action_view.cache_template_loading            = true
 
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :sendmail
+end
