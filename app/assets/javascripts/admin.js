416644d04dac110d6a9a207dcd31ac5097720c02
Scott Willson <scott.willson@gmail.com>
2012-09-19 19:55:34 -0700

    Require ckeditor JS after everything else

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,10 +1,9 @@
 /*
  *= require categories
- *= require ckeditor/init
  *= require events
  *= require first_aid_providers
  *= require people
  *= require results
  *= require teams
+ *= require ckeditor/init
 */
-
