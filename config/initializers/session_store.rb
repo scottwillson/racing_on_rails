8b4394e0375c582f3ec64a28090ac3f94fb0fc06
Scott Willson <scott.willson@gmail.com>
2012-09-24 11:01:35 -0700

    Ditch comments

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
 RacingOnRails::Application.config.session_store :cookie_store, :key => '_racing_on_rails_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# RacingOnRails::Application.config.session_store :active_record_store
