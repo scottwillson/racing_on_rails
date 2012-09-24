cea932bb6887bb7f85e6beb8ed520ef2705f6538
Scott Willson <scott.willson@gmail.com>
2012-09-24 14:22:09 -0700

    No syslog in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,8 +14,6 @@ RacingOnRails::Application.configure do
   config.cache_classes                       = true
   config.consider_all_requests_local         = true
   config.i18n.fallbacks                      = true
-  config.logger                              = Logger::Syslog.new("racing_on_rails", Syslog::LOG_LOCAL4)
-  config.logger.level                        = ::Logger::INFO
   config.serve_static_assets                 = false
   config.whiny_nils                          = true
 end
