89fa77be87f18d82611d121089e938b1f65313cd
Scott Willson <scott.willson@gmail.com>
2012-10-19 15:54:38 -0700

    Look for FK not association to not trigger SQL

--- app/models/competitions/score.rb
+++ app/models/competitions/score.rb
@@ -16,7 +16,9 @@ class Score < ActiveRecord::Base
   belongs_to :source_result, :class_name => 'Result', :foreign_key => 'source_result_id'
   belongs_to :competition_result, :class_name => 'Result', :foreign_key => 'competition_result_id'
   
-  validates_presence_of :source_result, :competition_result, :points
+  # Intentionally validate ids. validates_presence_of :association causes it to load.
+  # TODO Try :inverse to fix this
+  validates_presence_of :source_result_id, :competition_result_id, :points
   validates_numericality_of :points
   
   def discipline
