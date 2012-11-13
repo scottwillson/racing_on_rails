3357bdfe1817f2409e7474130f77a91c6d33cb81
Scott Willson <scott.willson@gmail.com>
2012-11-13 07:53:44 -0800

    Use config digest to fingerprint assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,7 @@ RacingOnRails::Application.configure do
   config.action_controller.perform_caching = true
   config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
   config.active_support.deprecation        = :notify
+  config.assets.digest                     = true
   config.cache_classes                     = true
   config.consider_all_requests_local       = false
   config.i18n.fallbacks                    = true
