8b39978577c8445b98fa942a9a4feb6af64ad497
Scott Willson <scott.willson@gmail.com>
2012-11-15 10:52:18 -0800

    Allow app to startup without database tables

--- config/initializers/ssl.rb
+++ config/initializers/ssl.rb
@@ -1,1 +1,5 @@
-RacingOnRails::Application.config.force_ssl = RacingAssociation.current.ssl?
+begin
+  RacingOnRails::Application.config.force_ssl = RacingAssociation.current.ssl?  
+rescue ActiveRecord::StatementInvalid => e
+  logger.warn "#{e} when loading RacingAssociation model. Table racing_associations might not exist."
+end
