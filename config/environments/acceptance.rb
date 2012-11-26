f2c81dabc156b304ecffb8c7afad6d8b1280d3ae
Scott Willson <scott.willson@gmail.com>
2012-11-25 21:18:54 -0800

    Organize config

--- config/environments/acceptance.rb
+++ config/environments/acceptance.rb
@@ -1,11 +1,9 @@
 RacingOnRails::Application.configure do
-  config.cache_classes = true
-  config.whiny_nils = true
-  config.active_support.deprecation = :stderr
-
-  config.consider_all_requests_local = true
-  config.action_controller.perform_caching             = false
-
-  config.action_controller.allow_forgery_protection    = false
-  config.action_mailer.delivery_method = :test
+  config.action_controller.allow_forgery_protection = false
+  config.action_controller.perform_caching          = false
+  config.action_mailer.delivery_method              = :test
+  config.active_support.deprecation                 = :stderr
+  config.cache_classes                              = true
+  config.consider_all_requests_local                = true
+  config.whiny_nils                                 = true
 end
