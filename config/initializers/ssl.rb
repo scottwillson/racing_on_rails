9282dbbe187683dddd7e12fc3509a5970864bd70
Scott Willson <scott.willson@gmail.com>
2012-11-25 21:20:45 -0800

    Don't set force_ssl in initializers

--- config/initializers/ssl.rb
+++
@@ -1,5 +0,0 @@
-begin
-  RacingOnRails::Application.config.force_ssl = RacingAssociation.current.ssl?  
-rescue ActiveRecord::StatementInvalid => e
-  logger.warn "#{e} when loading RacingAssociation model. Table racing_associations might not exist."
-end
