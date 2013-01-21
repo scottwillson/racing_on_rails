907db3a9d0ef6075d5ef542f882a50eeb221096b
Scott Willson <scott.willson@gmail.com>
2013-01-20 16:59:04 -0800

    Fix error for current OR cup

--- app/controllers/oregon_cup_controller.rb
+++ app/controllers/oregon_cup_controller.rb
@@ -3,7 +3,7 @@ class OregonCupController < ApplicationController
     if params[:year] && params[:year].to_i > 0
       @year = params[:year].to_i
     else
-      @year = Time.zone.today.year.to_s
+      @year = Time.zone.today.year
     end
     
     @oregon_cup = OregonCup.find_for_year(@year) || OregonCup.new
