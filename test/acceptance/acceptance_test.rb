78d87d06c75a2b874eec2dd31869bbb34f543c1b
Scott Willson <scott.willson@gmail.com>
2013-02-08 21:34:22 -0800

    Add turn

--- test/acceptance/acceptance_test.rb
+++ test/acceptance/acceptance_test.rb
@@ -3,7 +3,8 @@ ENV["RAILS_ENV"] = "acceptance"
 require File.expand_path(File.dirname(__FILE__) + "/../../config/environment")
 require "capybara/rails"
 require "minitest/autorun"
-
+require "turn/autorun"
+ 
 # Capybara supports a number of drivers/browsers. AcceptanceTest default is RackTest for non-JavaScript tests
 # and Firefox for JS test. Note that most tests require JS.
 #
@@ -225,13 +226,13 @@ class AcceptanceTest < ActiveSupport::TestCase
   def reset_session
     Capybara.reset_sessions!
   end
-
+  
   def before_teardown
     unless @passed
       save_page
     end
   end
-  
+
   def say_if_verbose(text)
     if ENV["VERBOSE"].present?
       puts text
