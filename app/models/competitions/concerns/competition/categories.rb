45c94c529b22587362e2a89a1060312fce1d93fa
Scott Willson <scott.willson@gmail.com>
2013-03-23 16:22:21 -0700

    Add category_names to allow shared create_races across competitions. Add source_events? method to scope results by source events or not.

--- app/models/competitions/concerns/competition/categories.rb
+++ app/models/competitions/concerns/competition/categories.rb
@@ -1,6 +1,15 @@
 module Concerns
   module Competition
     module Categories
+      def category_names
+        [ friendly_name ]
+      end
+
+      # Only consider results from categories. Default to false: use all races in year.
+      def categories?
+        false
+      end
+
       # Array of ids (integers)
       # +race+ category, +race+ category's siblings, and any competition categories
       def category_ids_for(race)
