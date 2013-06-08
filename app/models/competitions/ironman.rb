b81c3027e0473824706c74fdc5c3267dd9859e32
Scott Willson <scott.willson@gmail.com>
2013-06-08 08:46:42 -0700

    Use superclass years method

--- app/models/competitions/ironman.rb
+++ app/models/competitions/ironman.rb
@@ -7,17 +7,6 @@ class Ironman < Competition
   def friendly_name
     'Ironman'
   end
-
-  def Ironman.years
-    years = []
-    results = connection.select_all(
-      "select distinct extract(year from date) as year from events where type = 'Ironman'"
-    )
-    results.each do |year|
-      years << year.values.first.to_i
-    end
-    years.sort.reverse
-  end
   
   def points_for(source_result)
     1
