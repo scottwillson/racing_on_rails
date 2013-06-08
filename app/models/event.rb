55062097ac9d3adb9624a36ed0c3aa9c6150ac9c
Scott Willson <scott.willson@gmail.com>
2013-06-08 08:50:27 -0700

    Update comments

--- app/models/event.rb
+++ app/models/event.rb
@@ -179,7 +179,7 @@ class Event < ActiveRecord::Base
 
   end
     
-  # Defaults state to RacingAssociation.current.state, date to today, name to New Event mm-dd-yyyy
+  # Defaults state to RacingAssociation.current.state, date to today, name to Untitled
   # NumberIssuer: RacingAssociation.current.short_name
   # Child events use their parent's values unless explicity overriden. And you cannot override
   # parent values by passing in blank or nil attributes to initialize, as there is
