df5f8ce0a3e13ac49dbc7f28af6426e3214b4ced
Scott Willson <scott.willson@gmail.com>
2013-06-08 08:14:21 -0700

    Make year an instance variable

--- app/controllers/bar_controller.rb
+++ app/controllers/bar_controller.rb
@@ -9,7 +9,7 @@ class BarController < ApplicationController
 
   # Default to Overall BAR with links to disciplines
   def show
-    year = params['year'].to_i
+    @year = params['year'].to_i
 
     discipline = Discipline[params['discipline']]
     if discipline.nil?
@@ -17,15 +17,15 @@ class BarController < ApplicationController
       return render(:show)
     end
     
-    if year < 2007 && discipline == Discipline[:age_graded]
-      return redirect_to("http://#{RacingAssociation.current.static_host}/bar/#{year}/overall_by_age.html")
-    elsif year < 2006 && year >= 2001
-      return redirect_to("http://#{RacingAssociation.current.static_host}/bar/#{year}")
+    if @year < 2007 && discipline == Discipline[:age_graded]
+      return redirect_to("http://#{RacingAssociation.current.static_host}/bar/#{@year}/overall_by_age.html")
+    elsif @year < 2006 && @year >= 2001
+      return redirect_to("http://#{RacingAssociation.current.static_host}/bar/#{@year}")
     end
     
-    @overall_bar = OverallBar.find_for_year(year)
+    @overall_bar = OverallBar.find_for_year(@year)
     unless @overall_bar
-      flash.now[:warn] = "Could not find Overall BAR for #{year}"
+      flash.now[:warn] = "Could not find Overall BAR for #{@year}"
       return render(:show)
     end
 
