df1b5a3c29d4fae36725742be77ab7a2dee9ac2d
Scott Willson <scott.willson@gmail.com>
2013-09-10 13:50:27 -0700

    Fix bad date assertions

--- test/functional/application_helper_test.rb
+++ test/functional/application_helper_test.rb
@@ -11,12 +11,15 @@ class ApplicationHelperTest < ActionView::TestCase
 
   def test_to_excel
     assert_equal("", to_excel(""), "''")
-    assert_equal(nil, to_excel(nil), "nil")
+    assert_equal("", to_excel(nil), "nil")
     assert_equal("word", to_excel("word"), "word")
     assert_equal("This is a sentence", to_excel("This is a sentence"), "This is a sentence")
     assert_equal("This has a tab", to_excel("This has\ta tab"), 'This has\ta tab')
     assert_equal("This has   tabs ", to_excel("This has\t\t\ttabs\t"), 'This has\t\t\ttabs\t')
     assert_equal("expected  ", to_excel("expected\r\n"), "expected\r\n")
     assert_equal("  ", to_excel("\n\n"), "\\n\\n")
+    assert_equal("\"foo\"", to_excel("\"foo\""), "Should escape double quotes")
+    assert_equal("\"a,b\"", to_excel("a,b"), "Should escape double quotes")
+    assert_equal("9/8/2010", to_excel(Time.zone.local(2010, 9, 8).to_date), "date 2010-09-08")
   end
 end
