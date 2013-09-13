543a18d4fa8bad691338e27b4486fd8da19ff966
Scott Willson <scott.willson@gmail.com>
2013-09-13 12:38:29 -0700

    Add Race#additional_race_only

---
+++ db/migrate/20130913164501_add_additional_race_only.rb
@@ -0,0 +1,13 @@
+class AddAdditionalRaceOnly < ActiveRecord::Migration
+  def up
+    if RacingAssociation.current.short_name == "OBRA" || RacingAssociation.current.short_name == "NABRA"
+      add_column :races, :additional_race_only, :boolean, :default => false, :null => false
+    end
+  end
+
+  def down
+    if RacingAssociation.current.short_name == "OBRA" || RacingAssociation.current.short_name == "NABRA"
+      remove_column :races, :additional_race_only
+    end
+  end
+end
