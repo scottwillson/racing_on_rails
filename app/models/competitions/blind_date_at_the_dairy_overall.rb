a9749cbc55558fdfebfd03483a7f7468920cb352
Scott Willson <scott.willson@gmail.com>
2013-09-18 11:52:40 -0700

    Add Blind Date series calculations

---
+++ app/models/competitions/blind_date_at_the_dairy_overall.rb
@@ -0,0 +1,36 @@
+class BlindDateAtTheDairyOverall < Overall
+  def self.parent_event_name
+    "Blind Date at the Dairy"
+  end
+
+  def category_names
+    [
+      "Beginner Men",
+      "Beginner Women",
+      "Fixed",
+      "Junior Men 10-13",
+      "Junior Men 14-18",
+      "Junior Women 10-13",
+      "Junior Women 14-18",
+      "Masters Men A 40+",
+      "Masters Men B 35+",
+      "Masters Men C 35+",
+      "Men A",
+      "Men B",
+      "Men C",
+      "Singlespeed",
+      "Stampede",
+      "Women A",
+      "Women B",
+      "Women C"
+    ]
+  end
+
+  def default_bar_points
+    1
+  end
+  
+  def point_schedule
+    [ 0, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
+  end
+end
