77b0a462db3949d3fa89cbc8e8573dc213f9efce
Scott Willson <scott.willson@gmail.com>
2013-11-22 16:35:34 -0800

    Remove unused action

--- app/controllers/admin/race_numbers_controller.rb
+++ app/controllers/admin/race_numbers_controller.rb
@@ -1,10 +1,5 @@
 module Admin
   class RaceNumbersController < Admin::AdminController
-    def new
-      @person = Person.find(params[:person_id])
-      @race_number = @person.race_numbers.build
-    end
-  
     def destroy
       @race_number = RaceNumber.find(params[:id])
       @race_number.destroy
