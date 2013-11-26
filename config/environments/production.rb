ef77b62a59a1a0c818a76f92b702b17ab2c31608
Scott Willson <scott.willson@gmail.com>
2013-11-26 08:24:21 -0800

    Only log forbidden attributes in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,13 +1,16 @@
 RacingOnRails::Application.configure do
-  config.action_controller.perform_caching = true
-  config.action_dispatch.x_sendfile_header = "X-Accel-Redirect"
-  config.active_support.deprecation        = :notify
-  config.assets.digest                     = true
-  config.cache_classes                     = true
-  config.consider_all_requests_local       = false
-  config.i18n.fallbacks                    = true
-  config.logger                            = Logger::Syslog.new("racing_on_rails", Syslog::LOG_LOCAL4)
-  config.logger.level                      = ::Logger::INFO
-  config.serve_static_assets               = false
-  config.assets.precompile                += %w( racing_association.js ie.css racing_association.css racing_association_ie.css admin.js raygun.js )
+  config.action_controller.perform_caching                  = true
+  config.action_controller.action_on_unpermitted_parameters = :raise
+  config.action_dispatch.x_sendfile_header                  = "X-Accel-Redirect"
+  config.active_support.deprecation                         = :notify
+  config.assets.digest                                      = true
+  config.cache_classes                                      = true
+  config.consider_all_requests_local                        = false
+  config.i18n.fallbacks                                     = true
+  config.logger                                             = Logger::Syslog.new("racing_on_rails", Syslog::LOG_LOCAL4)
+  config.logger.level                                       = ::Logger::INFO
+  config.serve_static_assets                                = false
+  config.assets.precompile                += %w(
+    racing_association.js ie.css racing_association.css racing_association_ie.css admin.js raygun.js
+  )
 end
