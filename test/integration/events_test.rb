8933d86b971254efa32ffe0f40d64085e9689210
Scott Willson <scott.willson@gmail.com>
2013-12-18 06:31:15 -0800

    Fix year boundary test failure

--- test/integration/events_test.rb
+++ test/integration/events_test.rb
@@ -3,19 +3,21 @@ require File.expand_path("../../test_helper", __FILE__)
 # :stopdoc:
 class EventsTest < ActionController::IntegrationTest
   def test_index
-    FactoryGirl.create(:event)
-    FactoryGirl.create(:event)
+    Timecop.freeze(Time.zone.local(2013, 3)) do
+      FactoryGirl.create(:event)
+      FactoryGirl.create(:event)
     
-    get "/events"
-    assert_redirected_to schedule_path
+      get "/events"
+      assert_redirected_to schedule_path
 
-    get "/events.xml"
-    assert_response :success
-    xml = Hash.from_xml(@response.body)
-    assert_not_nil xml["single_day_events"], "Should have :single_day_events root element in #{xml}"
+      get "/events.xml"
+      assert_response :success
+      xml = Hash.from_xml(response.body)
+      assert_not_nil xml["single_day_events"], "Should have :single_day_events root element in #{xml}"
 
-    get "/events.json"
-    assert_response :success
-    assert_equal 2, JSON.parse(@response.body).size, "Should have JSON array"
+      get "/events.json"
+      assert_response :success
+      assert_equal 2, JSON.parse(response.body).size, "Should have JSON array"
+    end
   end
 end
