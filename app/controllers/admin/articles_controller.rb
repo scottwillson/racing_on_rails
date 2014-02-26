ee15ad658a04a2e0db1e1dd85b22e3dc78dfbece
Scott Willson <scott.willson@gmail.com>
2014-02-26 07:35:48 -0800

    Add :display and :article_category_id to permitted article categories

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -71,7 +71,7 @@ module Admin
     private
 
     def article_params
-      params.require(:article).permit(:body, :position, :title)
+      params.require(:article).permit(:article_category_id, :body, :display, :position, :title)
     end
   end
 end
