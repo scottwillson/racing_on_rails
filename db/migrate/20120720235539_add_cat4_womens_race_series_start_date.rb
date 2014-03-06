0b131652688a2b449523e450258c2b65f2a57f09
Scott Willson <scott.willson@gmail.com>
2014-03-06 07:56:40 -0800

    Remove old migrations

--- db/migrate/20120720235539_add_cat4_womens_race_series_start_date.rb
+++
@@ -1,9 +0,0 @@
-class AddCat4WomensRaceSeriesStartDate < ActiveRecord::Migration
-  def self.up
-    add_column :racing_associations, :cat4_womens_race_series_start_date, :date
-  end
-
-  def self.down
-    remove_column :racing_associations, :cat4_womens_race_series_start_date
-  end
-end
