a9a6febe0effa0ebbabba9a5a0fb628617b18c01
Scott Willson <scott.willson@gmail.com>
2014-03-21 08:43:21 -0700

    Add competition notes so standard event results template can be used for competitions

--- app/models/competitions/rider_rankings.rb
+++ app/models/competitions/rider_rankings.rb
@@ -4,4 +4,12 @@ class RiderRankings < Competition
   def friendly_name
     'Rider Rankings'
   end
+  
+  def notes
+    if year.to_i > 2011
+      "The WSBA is using the default USA Cycling ranking system from 2012 onward. Please see http://www.usacycling.org/events/rr.php."
+    else
+      self[:notes]
+    end
+  end
 end
