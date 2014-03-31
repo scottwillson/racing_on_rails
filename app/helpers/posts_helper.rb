11a5fe0535f420090b79e76b84ad40f19e60daa5
Scott Willson <scott.willson@gmail.com>
2014-03-30 17:25:58 -0700

    Show time of day for posts from today

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,9 @@
 module PostsHelper
   def last_reply_at(post)
     date = post.last_reply_at || post.date
-    if date.year == Time.zone.today.year
+    if date.to_date == Time.zone.today
+      date.to_s(:long_and_friendly_time)
+    elsif date.year == Time.zone.today.year
       date.to_s(:short_month_date)
     else
       date.to_s(:short_month_date_year)
