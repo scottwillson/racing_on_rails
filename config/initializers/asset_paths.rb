f7a5fabd846d1e4f77030160acbd68cf5c3296d3
Scott Willson <scott.willson@gmail.com>
2014-04-12 15:05:02 -0700

    Add vendor fonts to asset path and simplify font declaration

--- config/initializers/asset_paths.rb
+++ config/initializers/asset_paths.rb
@@ -1,3 +1,8 @@
-Rails.application.config.assets.paths.insert 0, Rails.root.join("local", "app", "assets", "images")
-Rails.application.config.assets.paths.insert 0, Rails.root.join("local", "app", "assets", "javascripts")
-Rails.application.config.assets.paths.insert 0, Rails.root.join("local", "app", "assets", "stylesheets")
+RacingOnRails::Application.configure do
+  config.assets.paths.insert 0, Rails.root.join("local", "app", "assets", "images")
+  config.assets.paths.insert 0, Rails.root.join("local", "app", "assets", "javascripts")
+  config.assets.paths.insert 0, Rails.root.join("local", "app", "assets", "stylesheets")
+
+  config.assets.paths << Rails.root.join("vendor", "app", "assets", "fonts")
+end
+
