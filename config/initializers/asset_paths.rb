54ccdbe6a2eab06b2a5508414a3615507b8ceb80
Scott Willson <scott.willson@gmail.com>
2014-04-12 16:34:34 -0700

    That'll fix it

--- config/initializers/asset_paths.rb
+++ config/initializers/asset_paths.rb
@@ -2,7 +2,5 @@ RacingOnRails::Application.configure do
   config.assets.paths.insert 0, Rails.root.join("local", "app", "assets", "images")
   config.assets.paths.insert 0, Rails.root.join("local", "app", "assets", "javascripts")
   config.assets.paths.insert 0, Rails.root.join("local", "app", "assets", "stylesheets")
-
-  config.assets.paths << Rails.root.join("vendor", "app", "assets", "fonts")
 end
 
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,10 +1,6 @@
 RacingOnRails::Application.configure do
   config.assets.precompile                += %w(
     admin.js
-    glyphicons-regular.eot
-    glyphicons-regular.svg
-    glyphicons-regular.ttf
-    glyphicons-regular.woff
     racing_association.css
     racing_association.js
     raygun.js
