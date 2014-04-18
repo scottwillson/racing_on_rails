a6a00308f18cd9e088370d5da316a4d4456dd6b2
Scott Willson <scott.willson@gmail.com>
2014-04-17 19:17:24 -0700

    Use root namespace for Person

--- test/models/racing_on_rails/vestal_versions/versioned_test.rb
+++ test/models/racing_on_rails/vestal_versions/versioned_test.rb
@@ -5,7 +5,7 @@ module RacingOnRails
     # :stopdoc:
     class VersionedTest < ActiveSupport::TestCase
       test "updated by person name" do
-        person = Person.create!(updated_by: "Bike race")
+        person = ::Person.create!(updated_by: "Bike race")
         assert_equal "Bike race", person.updated_by_person_name, "updated_by_person_name"
       end
     end
