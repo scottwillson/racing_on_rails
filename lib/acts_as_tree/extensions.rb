0642d0e3125f85b4654bdbe6a66193f4889e6aa6
Scott Willson <scott.willson@gmail.com>
2014-04-24 19:23:00 -0700

    Copy acts_as_tree implementation of ancestors and descendants

--- lib/acts_as_tree/extensions.rb
+++ lib/acts_as_tree/extensions.rb
@@ -7,20 +7,13 @@ module ActsAsTree
     #   subchild1.ancestors # => [child1, root]
     def ancestors
       node, nodes = self, []
-
-      while node.parent do
-        nodes << node = node.parent
-        if nodes.include?(node.parent)
-          break
-        end
-      end
-
+      nodes << node = node.parent while node.parent
       nodes
     end
 
     # Dupe from acts_as_tree gem so Event can use it.
     def descendants
-      children.each_with_object(children) {|child, arr|
+      children.each_with_object(children.to_a) {|child, arr|
         arr.concat child.descendants
       }.uniq
     end
