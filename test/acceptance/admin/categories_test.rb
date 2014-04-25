795f4872c631f78ca6ddc0df7b5f0617ad934b59
Scott Willson <scott.willson@gmail.com>
2014-04-24 20:53:38 -0700

    Use Glyhphicons for admin category pages

--- test/acceptance/admin/categories_test.rb
+++ test/acceptance/admin/categories_test.rb
@@ -29,12 +29,12 @@ class CategoriesTest < AcceptanceTest
     find("#category_#{women_4.id}").drag_to(find("#category_#{masters_35_plus.id}_row"))
     assert page.has_no_selector? ".unknown_category_root #category_#{women_4.id}"
     assert page.has_no_selector? ".association_category_root #category_#{women_4.id}"
-    assert page.has_selector? "#disclosure_#{masters_35_plus.id}.collapsed"
+    assert page.has_selector? "#disclosure_#{masters_35_plus.id}.glyphicon-collapse"
 
     find("#disclosure_#{masters_35_plus.id}").click
     assert page.has_selector? ".association_category_root #category_#{women_4.id}"
     assert page.has_selector? "#disclosure_#{masters_35_plus.id}"
-    assert page.has_no_selector? "#disclosure_#{masters_35_plus.id}.collapsed"
+    assert page.has_no_selector? "#disclosure_#{masters_35_plus.id}.glyphicon-collapse"
     assert page.has_selector? "#category_#{masters_35_plus.id}_children #category_#{women_4.id}"
   end
 end
