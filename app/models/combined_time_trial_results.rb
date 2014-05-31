5520de69fe6005f4dbb9f4da3a766ae3949ed1ce
Scott Willson <scott.willson@gmail.com>
2014-05-31 07:22:54 -0700

    Expire cache after calculating combined TT results

--- app/models/combined_time_trial_results.rb
+++ app/models/combined_time_trial_results.rb
@@ -107,6 +107,8 @@ class CombinedTimeTrialResults < Event
         result.update(place: place.to_s)
         place = place + 1
       end
+
+      ApplicationController.expire_cache
     end
 
     true
