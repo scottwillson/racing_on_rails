ac38a599bc73990219a9fb8a64017ce669a4e546
Scott Willson <scott.willson@gmail.com>
2014-07-25 18:28:46 -0700

    Add ETag, last modified and cache headers for teams page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,7 +1,5 @@
 # Public page of all Teams
 class TeamsController < ApplicationController
-  caches_page :index
-
   def index
     if RacingAssociation.current.show_all_teams_on_public_page?
       @teams = Team.all
@@ -9,5 +7,7 @@ class TeamsController < ApplicationController
       @teams = Team.where(member: true).where(show_on_public_page: true)
     end
     @discipline_names = Discipline.names
+
+    fresh_when @teams, public: true
   end
 end
