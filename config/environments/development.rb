6249f6e7eb04dae1348a8e6b7ee5309d6d55f058
Scott Willson <scott.willson@gmail.com>
2014-07-28 09:00:58 -0700

    Control dev env caching with ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 RacingOnRails::Application.configure do
   config.action_controller.action_on_unpermitted_parameters = :raise
-  config.action_controller.perform_caching                  = false
+  config.action_controller.perform_caching                  = ENV["PERFORM_CACHING"] || false
   config.action_mailer.raise_delivery_errors                = false
   config.active_record.migration_error                      = :page_load
   config.active_support.deprecation                         = :log
@@ -8,7 +8,7 @@ RacingOnRails::Application.configure do
   config.assets.debug                                       = true
   config.assets.js_compressor                               = false
   config.assets.raise_runtime_errors                        = true
-  config.cache_classes                                      = false
+  config.cache_classes                                      = ENV["CACHE_CLASSES"] || false
   config.consider_all_requests_local                        = true
   config.eager_load                                         = false
   config.middleware.use                                       Rack::LiveReload
