4c2845ef6d9709c88149e78dd2e9a2f64e9aa4e3
Scott Willson <scott.willson@gmail.com>
2014-07-29 11:00:10 -0700

    Extract scopes from home page controller to models

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,6 @@
 class HomeController < ApplicationController
   respond_to :html
 
-  caches_page :index
-
   before_filter :require_administrator, except: :index
 
   # Show homepage
@@ -11,44 +9,22 @@ class HomeController < ApplicationController
   # * upcoming_events
   # * recent_results: Events with Results within last two weeks
   def index
-    assign_home
-
     @page_title = RacingAssociation.current.name
+    assign_home
+    @photo = @home.photo
+    @posts = recent_posts
 
     @upcoming_events = Event.upcoming(@home.weeks_of_upcoming_events)
-
-    @events_with_recent_results = Event.
-      includes(parent: :parent).
-      where("type != ?", "Event").
-      where("type is not null").
-      where("events.date >= ?", @home.weeks_of_recent_results.weeks.ago).
-      where("id in (select event_id from results where competition_result = false and team_competition_result = false)").
-      order("updated_at desc")
-
-    if RacingAssociation.current.show_only_association_sanctioned_races_on_calendar?
-      @events_with_recent_results = @events_with_recent_results.where(sanctioned_by: RacingAssociation.current.default_sanctioned_by)
-    end
-
-    @most_recent_event_with_recent_result = Event.
-      includes(races: [ :category, :results ]).
-      includes(:parent).
-      where("type != ?", "Event").
-      where("type is not null").
-      where("events.date >= ?", @home.weeks_of_recent_results.weeks.ago).
-      where(sanctioned_by: RacingAssociation.current.default_sanctioned_by).
-      where("id in (select event_id from results where competition_result = false and team_competition_result = false)").
-      order("updated_at desc").
-      first
+    @events_with_recent_results = Event.with_recent_results(@home.weeks_of_recent_results.weeks.ago)
+    @most_recent_event_with_recent_result = Event.most_recent_with_recent_result(
+      @home.weeks_of_recent_results.weeks.ago,
+      RacingAssociation.current.default_sanctioned_by
+    ).first
 
     @news_category = ArticleCategory.where(name: "news").first
     if @news_category
-      @recent_news = Article.
-        where("created_at > :cutoff OR updated_at > :cutoff", cutoff: @home.weeks_of_upcoming_events.weeks.ago).
-        where(article_category_id: @news_category.id)
+      @recent_news = Article.recent_news(@home.weeks_of_upcoming_events.weeks.ago, @news_category)
     end
-
-    @photo = @home.photo
-    @posts = recent_posts
 
     render_page
   end
@@ -60,7 +36,6 @@ class HomeController < ApplicationController
   def update
     assign_home
     if @home.update(home_params)
-      expire_cache
       redirect_to edit_home_path
     else
       render :edit
--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,11 @@ class Article < ActiveRecord::Base
   belongs_to :article_category
   acts_as_list scope: :article_category
 
+  scope :recent_news, lambda { |weeks, category|
+    where("created_at > :cutoff OR updated_at > :cutoff", cutoff: weeks).
+    where(article_category_id: category)
+  }
+
   def <=>(other)
     return -1 unless other
     position <=> other.position
