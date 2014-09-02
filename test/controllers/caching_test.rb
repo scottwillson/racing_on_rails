2a37b1cbd2c62fce427cd9b9d66bef158b732c39
Scott Willson <scott.willson@gmail.com>
2014-09-01 19:33:35 -0700

    Touch RacingAssociation when cache expires

--- test/controllers/caching_test.rb
+++ test/controllers/caching_test.rb
@@ -8,11 +8,13 @@ class CachingTest < ActionController::TestCase
 
   test "expire_cache" do
     controller = TestController.new
+    TestController.expects(:expire_cache)
     controller.send :expire_cache
   end
 
   test "class expire_cache" do
-    controller = TestController.new
+    RacingAssociation.current.update_column :updated_at, 2.days.ago
     TestController.expire_cache
+    assert RacingAssociation.current.reload.updated_at > 2.days.ago
   end
 end
