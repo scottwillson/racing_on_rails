c249a1127fef22b0099b663f191a8002862db878
Scott Willson <scott.willson@gmail.com>
2014-09-05 13:05:11 -0700

    Add realisic benchmarks for performance test

--- test/performance/results.rb
+++ test/performance/results.rb
@@ -1,10 +1,10 @@
 CelluloidBenchmark::Session.define do
-  benchmark :results_index
+  benchmark :results_index, 1.2
   page = get("http://obra.staging.rocketsurgeryllc.com/results")
 
   20.times do
     link = page.links_with(class: "event").sample
-    benchmark :results_event_show, 5
+    benchmark :results_event_show, 0.6
     link.click
   end
 end
