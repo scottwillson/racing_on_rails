de1e868056eb8b2f430207700c086a60406ab8a6
Scott Willson <scott.willson@gmail.com>
2014-09-08 13:27:43 -0700

    Set :app_hostname in local config

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,3 @@
 role :app, "app@obra.staging.rocketsurgeryllc.com"
 role :web, "app@obra.staging.rocketsurgeryllc.com"
 role :db,  "app@obra.staging.rocketsurgeryllc.com"
-
-set :app_hostname, "obra.staging.rocketsurgeryllc.com"
