d3d7d521ce3b098649af5db22580bc36d9592142
Scott Willson <scott.willson@gmail.com>
2014-09-22 15:37:18 -0700

    Create separate versions (sizes) for homepage photo

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,10 +4,21 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   storage :file
-  process resize_to_limit: [ 1440, 900 ]
-  process quality: 85
-  process :interlace
-  process :save_dimensions
+
+  version :original
+
+  version :desktop do
+    process resize_to_limit: [ 1440, 900 ]
+    process quality: 85
+    process :interlace
+    process :save_dimensions
+  end
+
+  version :mobile do
+    process resize_to_limit: [ 480, 320 ]
+    process quality: 85
+    process :interlace
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
