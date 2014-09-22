03adcc003d848cdcb7de9781bf1d7e4340252d8e
Scott Willson <scott.willson@gmail.com>
2014-09-22 10:23:37 -0700

    Don't care if people#fullname isn't there

--- db/migrate/20140921012247_remove_outdated_tables.rb
+++ db/migrate/20140921012247_remove_outdated_tables.rb
@@ -1,6 +1,7 @@
 class RemoveOutdatedTables < ActiveRecord::Migration
   def change
-    remove_column :people, :fullname
+    remove_column(:people, :fullname) rescue nil
+    
     %w{ duplicates_racers engine_schema_info historical_names images news_items promoters racers standings users }.each do |name|
       if table_exists?(name)
         drop_table name
