b8664285666e3ed4b2145071926b6d1b9e47b853
Scott Willson <scott.willson@gmail.com>
2014-09-23 16:55:50 -0700

    Expose array sort method for test

--- lib/array/stable_sort.rb
+++ lib/array/stable_sort.rb
@@ -2,7 +2,7 @@ class Array
   # Sort by +method+ and preserve existing order. Ruby 1.8 sort_by does not preserve order.
   def stable_sort_by(method, order = :asc)
     if order == :asc
-      _stable_merge_sort { |x, y|
+      merge_sort { |x, y|
         if x.send(method).nil?
           true
         elsif !x.send(method).nil? && y.send(method).nil?
@@ -12,7 +12,7 @@ class Array
         end
       }
     elsif order == :desc
-      _stable_merge_sort { |x, y|
+      merge_sort { |x, y|
         if y.send(method).nil?
           true
         elsif !y.send(method).nil? && x.send(method).nil?
@@ -26,16 +26,16 @@ class Array
     end
   end
 
-  private
-
   # Sort is stable only if predicate includes an equal comparison. Example: x.name <= y.name
-  def _stable_merge_sort(&predicate)
+  def merge_sort(&predicate)
     return self.dup if size <= 1
     mid = size / 2
     left  = self[0, mid].dup
     right = self[mid, size].dup
-    _stable_merge(left._stable_merge_sort(&predicate), right._stable_merge_sort(&predicate), &predicate)
+    _stable_merge(left.merge_sort(&predicate), right.merge_sort(&predicate), &predicate)
   end
+
+  private
 
   def _stable_merge(left, right, &predicate)
     sorted = []
