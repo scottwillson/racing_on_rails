e24a3e1277d95a1cf4ed0f0b319a6fb2009e86a7
Scott Willson <scott.willson@gmail.com>
2014-09-26 15:01:42 -0700

    Make find_all_bar_for_discipline a class method

--- app/models/events/competitions.rb
+++ app/models/events/competitions.rb
@@ -15,12 +15,12 @@ module Events
       has_one :combined_results, class_name: "CombinedTimeTrialResults", foreign_key: "parent_id", dependent: :destroy
       has_many :competitions, through: :competition_event_memberships, source: :competition, class_name: "Competitions::Competition"
       has_many :competition_event_memberships, class_name: "Competitions::CompetitionEventMembership"
-    end
 
-    def self.find_all_bar_for_discipline(discipline, year = Time.zone.today.year)
-      discipline_names = [discipline]
-      discipline_names << 'Circuit' if discipline.downcase == 'road'
-      Event.distinct.year(year).where(discipline: discipline_names).where("bar_points > 0")
+      def self.find_all_bar_for_discipline(discipline, year = Time.zone.today.year)
+        discipline_names = [discipline]
+        discipline_names << 'Circuit' if discipline.downcase == 'road'
+        Event.distinct.year(year).where(discipline: discipline_names).where("bar_points > 0")
+      end
     end
 
     # Set point value/factor for this Competition. Convenience method to hide CompetitionEventMembership complexity.
