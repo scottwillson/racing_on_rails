22b12bb93b3b7c1214cad80922b5072999f4d13d
Scott Willson <scott.willson@gmail.com>
2014-09-29 10:55:56 -0700

    Decompose Result#CreateIfBestResultForRaceExtension a bit

--- app/models/result.rb
+++ app/models/result.rb
@@ -2,10 +2,9 @@ module CreateIfBestResultForRaceExtension
   def create_if_best_result_for_race(attributes)
     source_result = attributes[:source_result]
     scores = proxy_association.owner.scores
+
     scores.each do |score|
-      same_race  = (score.source_result.race_id  == source_result.race_id)
-      same_person = (score.source_result.person_id == source_result.person_id)
-      if same_race && score.source_result.person && same_person
+      if same_race?(score, source_result) && same_person?(score, source_result)
         if attributes[:points] > score.points
           scores.delete score
         else
@@ -13,7 +12,17 @@ module CreateIfBestResultForRaceExtension
         end
       end
     end
+
     create attributes
+  end
+
+  def same_race?(score, result)
+    score.source_result.person &&
+    score.source_result.race_id  == result.race_id
+  end
+
+  def same_person?(score, result)
+    score.source_result.person_id == result.person_id
   end
 end
 
