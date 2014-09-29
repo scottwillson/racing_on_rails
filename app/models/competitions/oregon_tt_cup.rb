84a1d20ba2ce8444ed9e6b4cd3c07d58d9b39ffc
Scott Willson <scott.willson@gmail.com>
2014-09-29 15:19:24 -0700

    Refactor OregonTTCup to use smaller methods. Still awful.

--- app/models/competitions/oregon_tt_cup.rb
+++ app/models/competitions/oregon_tt_cup.rb
@@ -113,11 +113,33 @@ module Competitions
       ids
     end
 
+    # Source events' categories don't match competition's categories.
+    # Some need to be split. For example: Junior Men 10-18 to Junior Men 10-12, Junior Men 13-14, etc.
+    # Some need to combined: For example: Masters Men 30-34 and Masters Men 35-39 to Masters Men 30-39
+    # Both splitting and combining add races to the source events before calculation
     def before_calculate
-      source_events.each do |event|
-        event.races.select { |r| r.created_by.kind_of?(OregonTTCup) }.each(&:destroy)
-      end
+      destroy_or_tt_cup_races
+      set_distances
 
+      missing_categories.each do |event, categories|
+        categories.each do |competition_category|
+          split_races event, competition_category
+          combine_races event, competition_category
+        end
+      end
+    end
+
+    # Destroy races created just to calculate the Oregon TT Cup
+    def destroy_or_tt_cup_races
+      source_events.each do |event|
+        event.races.select { |r| r.created_by.kind_of?(OregonTTCup) }.
+        each(&:destroy)
+      end
+    end
+
+    # Ensure distance is set so times can be adjusted. Some categories with different
+    # distances are combined.
+    def set_distances
       source_events.each do |event|
         event.races.each do |race|
           if event.id == 22500 && (race.category.name == "Masters Women 55+" || race.category.name == "Masters Men 65+")
@@ -129,81 +151,9 @@ module Competitions
           end
         end
       end
-
-      missing_categories.each do |event, categories|
-        categories.each do |competition_category|
-          races_to_split = event.races.select do |r|
-            r.category.age_group? &&
-            competition_category.age_group? &&
-            ((r.category.ages_end == 999 && competition_category.ages_end == 999) || (r.category.ages_end != 999 && competition_category.ages_end != 99)) &&
-            r.category.gender     == competition_category.gender &&
-            r.category.ages       != competition_category.ages &&
-            r.category.ages_begin <= competition_category.ages_begin &&
-            r.category.ages_end   >= competition_category.ages_end
-          end
-
-          if races_to_split.present?
-            existing_race = event.races.detect { |r| r.category == competition_category }
-            race = existing_race || event.races.create!(category: competition_category, bar_points: 0, updated_by: self)
-            races_to_split.each do |race_to_split|
-              race_to_split.results.select do |result|
-                result.person &&
-                result.person.racing_age &&
-                competition_category.ages.include?(result.person.racing_age) &&
-                result.time &&
-                result.time > 0
-              end.each do |result|
-                race.results.create!(
-                  place: result.place,
-                  person: result.person,
-                  team: result.team,
-                  time: result.time
-                )
-              end
-            end
-            race.place_results_by_time
-          end
-
-          races_to_combine = event.races.select do |r|
-            r.category.age_group? &&
-            competition_category.age_group? &&
-            r.category.gender     == competition_category.gender &&
-            r.category.ages       != competition_category.ages &&
-            (
-              (r.category.ages_begin >= competition_category.ages_begin && r.category.ages_end <= competition_category.ages_end) ||
-              (r.category.ages_begin > competition_category.ages_begin &&
-               r.category.ages_begin < competition_category.ages_end &&
-               r.category.ages_end == 999)
-            )
-          end
-
-          if races_to_combine.present?
-            existing_race = event.races.detect { |r| r.category == competition_category }
-            race = existing_race || event.races.create!(category: competition_category, bar_points: 0, updated_by: self)
-            races_to_combine.each do |race_to_combine|
-              race_to_combine.results.select do |result|
-                result.time &&
-                result.time > 0 &&
-                (result.person.racing_age.nil? || (result.person.racing_age >= competition_category.ages_begin && result.person.racing_age <= competition_category.ages_end))
-              end.each do |result|
-                time = result.time
-                if result.event_id == 22500 && result.race.distance.present? && result.race.distance.to_f < 24.9
-                  time = result.time * 2.2
-                end
-                race.results.create!(
-                  place: result.place,
-                  person: result.person,
-                  team: result.team,
-                  time: time
-                )
-              end
-            end
-            race.place_results_by_time
-          end
-        end
-      end
     end
 
+    # Find competition categories that should be in source events, but are not
     def missing_categories
       missing_categories = Hash.new { |hash, key| hash[key] = [] }
       source_events(true).each do |source_event|
@@ -221,6 +171,98 @@ module Competitions
       end
 
       missing_categories
+    end
+
+    def split_races(event, competition_category)
+      races_to_split = select_races_to_split(event, competition_category)
+
+      if races_to_split.present?
+        existing_race = event.races.detect { |r| r.category == competition_category }
+        race = existing_race || event.races.create!(category: competition_category, bar_points: 0, updated_by: self)
+
+        races_to_split.each do |race_to_split|
+          split_race race_to_split, race, competition_category
+        end
+
+        race.place_results_by_time
+      end
+    end
+
+    def select_races_to_split(event, competition_category)
+      event.races.select do |r|
+        r.category.age_group? &&
+        competition_category.age_group? &&
+        ((r.category.and_over? && competition_category.and_over?) || (r.category.ages_end != 999 && competition_category.ages_end != 99)) &&
+        r.category.gender     == competition_category.gender &&
+        r.category.ages       != competition_category.ages &&
+        r.category.ages_begin <= competition_category.ages_begin &&
+        r.category.ages_end   >= competition_category.ages_end
+      end
+    end
+
+    def split_race(race_to_split, race, competition_category)
+      race_to_split.results.select do |result|
+        result.person &&
+        result.person.racing_age &&
+        competition_category.ages.include?(result.person.racing_age) &&
+        result.time &&
+        result.time > 0
+      end.each do |result|
+        race.results.create!(
+          place: result.place,
+          person: result.person,
+          team: result.team,
+          time: result.time
+        )
+      end
+    end
+
+    def combine_races(event, competition_category)
+      races_to_combine = select_races_to_combine(event.races, competition_category)
+
+      if races_to_combine.present?
+        existing_race = event.races.detect { |r| r.category == competition_category }
+        race = existing_race || event.races.create!(category: competition_category, bar_points: 0, updated_by: self)
+
+        races_to_combine.each do |race_to_combine|
+          combine_race race_to_combine, race
+        end
+        race.place_results_by_time
+      end
+    end
+
+    def select_races_to_combine(races, competition_category)
+      races.select do |r|
+        r.category.age_group? &&
+        competition_category.age_group? &&
+        r.category.gender     == competition_category.gender &&
+        r.category.ages       != competition_category.ages &&
+        (
+          (r.category.ages_begin >= competition_category.ages_begin && r.category.ages_end <= competition_category.ages_end) ||
+          (r.category.ages_begin > competition_category.ages_begin &&
+           r.category.ages_begin < competition_category.ages_end &&
+           r.category.ages_end.and_over?)
+        )
+      end
+    end
+
+    def combine_race(race_to_combine, race)
+      race_to_combine.results.select do |result|
+        result.time &&
+        result.time > 0 &&
+        (result.person.racing_age.nil? || (result.person.racing_age >= competition_category.ages_begin && result.person.racing_age <= competition_category.ages_end))
+      end.each do |result|
+        time = result.time
+        if result.event_id == 22500 && result.race.distance.present? && result.race.distance.to_f < 24.9
+          time = result.time * 2.2
+        end
+        race.results.create!(
+          place: result.place,
+          person: result.person,
+          team: result.team,
+          time: time
+        )
+      end
     end
   end
 end
