e9aca05c388bcc2ac412c854cea543d47fc1882b
Scott Willson <scott.willson@gmail.com>
2014-10-22 12:12:32 -0700

    Fix nil/out-of-bound exception in MBRA BAR caused by small fields

--- app/models/competitions/mbra_bar.rb
+++ app/models/competitions/mbra_bar.rb
@@ -109,7 +109,12 @@ module Competitions
         else
           # if multiple riders got the same place (must be a TTT or tandem team or... ?), then they split the points...
           # this screws up the scoring of match sprints where riders eliminated in qualifying heats all earn the same place
-          points = point_schedule[source_result.numeric_place] * source_result.race.bar_points #/ team_size.to_f
+          points = point_schedule[source_result.numeric_place]
+          if points
+            points = points * source_result.race.bar_points
+          else
+            points = 0
+          end
         end
       }
       points
