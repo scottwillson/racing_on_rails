56e07ca8675505adb8c6e1a14e9c3c27d29b6bd5
Scott Willson <scott.willson@gmail.com>
2014-10-27 09:36:11 -0700

    Fix the fix post-coffee

--- db/migrate/20141027155355_update_more_mbra_bar_categories.rb
+++ db/migrate/20141027155355_update_more_mbra_bar_categories.rb
@@ -3,26 +3,48 @@ class UpdateMoreMbraBarCategories < ActiveRecord::Migration
     if RacingAssociation.current.short_name == "MBRA"
       Discipline.transaction do
         Competitions::MbraBar.year(2014).each(&:destroy_races)
-        Category.where(name: "{:Name=>\"Masters Men 40+\"}").first.destroy!
-        Category.where(name: "{:Name=>\"Masters Men 50+\"}").first.destroy!
-        Category.where(name: "{:Name=>\"Masters Men 60+\"}").first.destroy!
-        Category.where(name: "{:Name=>\"Masters Women 40+\"}").first.destroy!
-        Category.where(name: "{:Name=>\"Masters Women 50+\"}").first.destroy!
+        [
+          "{:Name=>\"Masters Men 40+\"}",
+          "{:Name=>\"Masters Men 50+\"}",
+          "{:Name=>\"Masters Men 60+\"}",
+          "{:Name=>\"Masters Women 40+\"}",
+          "{:Name=>\"Masters Women 50+\"}"
+        ].each do |name|
+          category = Category.where(name: name).first
+          if category
+            category.races.each(&:destroy!)
+            category.destroy!
+          end
+        end
 
-        %w{ Road Cyclocross }.each do |name|
-          discipline = Discipline.where(name: name).first
-          discipline.bar_categories << Category.find_or_create_by_normalized_name("Masters Men 40+")
-          discipline.bar_categories << Category.find_or_create_by_normalized_name("Masters Men 50+")
-          discipline.bar_categories << Category.find_or_create_by_normalized_name("Masters Men 60+")
-          discipline.bar_categories << Category.find_or_create_by_normalized_name("Masters Women 40+")
+        %w{ Road Cyclocross }.each do |discipline_name|
+          discipline = Discipline.where(name: discipline_name).first
+          [ 
+            "Masters Men 40+",
+            "Masters Men 50+",
+            "Masters Men 60+",
+            "Masters Women 40+"
+          ].each do |name|
+            category = Category.find_or_create_by_normalized_name(name)
+            unless discipline.bar_categories.include?(category)
+              discipline.bar_categories << category
+            end
+          end
         
           discipline.bar_categories.delete Category.find_or_create_by_normalized_name("Masters A Men")
           discipline.bar_categories.delete Category.find_or_create_by_normalized_name("Masters B Men")
         end
         
         discipline = Discipline.where(name: "Mountain Bike").first
-        discipline.bar_categories << Category.find_or_create_by_normalized_name("Masters Men 50+")
-        discipline.bar_categories << Category.find_or_create_by_normalized_name("Masters Women 50+")
+        [ 
+          "Masters Men 50+",
+          "Masters Women 50+"
+        ].each do |name|
+          category = Category.find_or_create_by_normalized_name(name)
+          unless discipline.bar_categories.include?(category)
+            discipline.bar_categories << category
+          end
+        end
         
         Category.cleanup!
       end
