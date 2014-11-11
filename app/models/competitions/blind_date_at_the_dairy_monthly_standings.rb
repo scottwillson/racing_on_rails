e8674daf1ea8635e36f3db6b9a3c3ce3d12568cd
Scott Willson <scott.willson@gmail.com>
2014-11-11 09:35:58 -0800

    Use Calculatore for Blind Date monthly standings

--- app/models/competitions/blind_date_at_the_dairy_monthly_standings.rb
+++ app/models/competitions/blind_date_at_the_dairy_monthly_standings.rb
@@ -1,5 +1,7 @@
 module Competitions
   class BlindDateAtTheDairyMonthlyStandings < Competition
+    include Competitions::Calculations::CalculatorAdapter
+
     def self.parent_event_name
       "Blind Date at the Dairy"
     end
@@ -7,7 +9,7 @@ module Competitions
     def self.calculate!(year = Time.zone.today.year)
       ActiveSupport::Notifications.instrument "calculate.#{name}.competitions.racing_on_rails" do
         transaction do
-          parent = ::Series.year(year).where(name: parent_event_name).first
+          parent = ::WeeklySeries.year(year).where(name: parent_event_name).first
 
           if parent && parent.any_results_including_children?
             [ 9, 10 ].each do |month|
@@ -57,68 +59,13 @@ module Competitions
     end
 
     def point_schedule
-      [ 0, 15, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
+      [ 15, 12, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ]
     end
 
-    # source_results must be in person-order
-    def source_results(race)
-      return [] if source_events.empty?
-
-      event_ids = source_events.map(&:id).join(', ')
-      category_ids = category_ids_for(race).join(', ')
-
-      Result.find_by_sql(
-        %Q{ SELECT results.* FROM results
-            JOIN races ON races.id = results.race_id
-            JOIN categories ON categories.id = races.category_id
-            JOIN events ON races.event_id = events.id
-            WHERE place between 1 and #{point_schedule.size - 1}
-                and categories.id in (#{category_ids})
-                and events.id in (#{event_ids})
-            order by person_id
-         }
-      )
-    end
-
-    # If same rider places twice in same race, only highest result counts
-    def create_competition_results_for(results, race)
-      competition_result = nil
-      results.each_with_index do |source_result, index|
-        logger.debug("#{self.class.name} scoring result: #{source_result.date} race: #{source_result.race.name} pl: #{source_result.place} mem pl: #{source_result.members_only_place if place_members_only?} #{source_result.last_name} #{source_result.team_name}") if logger.debug?
-
-        person = source_result.person
-        points = points_for(source_result)
-
-        # We repeat some calculations here if a person is disallowed
-        if points > 0.0 &&
-           (!parent.completed? || (parent.completed? && raced_minimum_events?(person, race))) &&
-             (!members_only? || member?(person, source_result.date))
-
-          if first_result_for_person?(source_result, competition_result)
-            # Intentionally not using results association create method. No need to hang on to all competition results.
-            # In fact, this could cause serious memory issues with the Ironman
-            competition_result = Result.create!(
-               person: person,
-               team: (person ? person.team : nil),
-               race: race)
-          end
-
-          competition_result.scores.create_if_best_result_for_race(
-            source_result: source_result,
-            competition_result: competition_result,
-            points: points
-          )
-        end
-
-        # Aggressive memory management. If competition has a race with many results,
-        # the results array can become a large, uneeded, structure
-        results[index] = nil
-        if index > 0 && index % 1000 == 0
-          logger.debug("GC start after record #{index}")
-          GC.start
-        end
-
-      end
+    def source_results_query(race)
+      super.
+      where("races.category_id in (?)", category_ids_for(race)).
+      where("results.event_id" => source_events)
     end
 
     def add_source_events
