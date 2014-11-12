58b3d8749cd6396542865f3d2602128d708662eb
Scott Willson <scott.willson@gmail.com>
2014-11-11 18:38:01 -0800

    Remove retired Cat 4 Womens Series logic

--- app/models/competitions/cat4_womens_race_series.rb
+++ app/models/competitions/cat4_womens_race_series.rb
@@ -1,52 +1,5 @@
 module Competitions
-  # Count pre-set list of series source_events + manually-entered non-series events
+  # Retired. Retain class for archived results
   class Cat4WomensRaceSeries < Competition
-    include Cat4WomensRaceSeriesModules::Points
-
-    def friendly_name
-      "Cat 4 Women's Race Series"
-    end
-
-    def source_results(race)
-      _start_date = RacingAssociation.current.cat4_womens_race_series_start_date || date.beginning_of_year
-      _end_date = RacingAssociation.current.cat4_womens_race_series_end_date || self.end_date
-      Result.
-        includes(race: [ :category, :event ]).
-        where("place > 0 or place is null or place = ''").
-        where("results.name is not null").
-        where("results.name != ''").
-        where("categories.id" => category_ids_for(race)).
-        where("(events.type = 'SingleDayEvent' or events.type is null or events.id in (?))", source_events.map(&:id)).
-        where("(events.ironman is true or events.id in (?))", source_events.map(&:id)).
-        where("events.date between ? and ?", _start_date, _end_date).
-        order(:person_id).
-        references(:category, :event)
-    end
-
-    def participation_points?
-      RacingAssociation.current.award_cat4_participation_points?
-    end
-
-    def association_point_schedule
-      RacingAssociation.current.cat4_womens_race_series_points
-    end
-
-    def members_only?
-      false
-    end
-
-    def create_races
-      races.create category: category
-    end
-
-    def cat_4_categories
-      [ category ] + category.descendants
-    end
-
-    def category
-      @category ||=
-        RacingAssociation.current.cat4_womens_race_series_category ||
-        Category.find_or_create_by(name: "Women Cat 4")
-    end
   end
 end
