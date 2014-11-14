4f3e2b0ca97a4a5a0e5c554a3406b78c9ef8ca61
Scott Willson <scott.willson@gmail.com>
2014-11-14 12:47:50 -0800

    Work around Cross Crusade junior results

--- app/models/competitions/ironman.rb
+++ app/models/competitions/ironman.rb
@@ -27,5 +27,27 @@ module Competitions
     def source_results_query(race)
       super.where("events.ironman" => true)
     end
+    
+    # Workaround for Cross Crusade Junior results reporting
+    def after_source_results(results)
+      results.reject { |r| r["category_name"].in?(junior_categories) && r["event_id"].in?(cross_crusade_2014) }
+    end
+    
+    def junior_categories
+      [
+        "Junior Men 10-12",
+        "Junior Men 13-14",
+        "Junior Men 15-16",
+        "Junior Men 17-18",
+        "Junior Women 10-12",
+        "Junior Women 13-14",
+        "Junior Women 15-16",
+        "Junior Women 17-18"
+      ]
+    end
+    
+    def cross_crusade_2014
+      SingleDayEvent.where(parent_id: 22445).pluck(:id)
+    end
   end
 end
