364f3d4ac96aea88b5627cea5e1aadadd0e24763
Scott Willson <scott.willson@gmail.com>
2014-11-18 20:45:48 -0800

    Add Athena to the Cross Crusade overall

--- app/models/competitions/cross_crusade_overall.rb
+++ app/models/competitions/cross_crusade_overall.rb
@@ -10,6 +10,7 @@ module Competitions
 
     def category_names
       [
+        "Athena",
         "Beginner Men",
         "Beginner Women",
         "Category A",
@@ -62,7 +63,7 @@ module Competitions
     def set_name
       self.name = "Series Overall"
     end
-    
+
     # Use combined Junior results from race day. Don't combine all the age groups races into one.
     def category_ids_for(race)
       case race.name
