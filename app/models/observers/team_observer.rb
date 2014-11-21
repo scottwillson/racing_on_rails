41a7325bb2cfbdabbee71242281ccbe22103668b
Scott Willson <scott.willson@gmail.com>
2014-11-21 13:07:40 -0800

    Update Result#team_member when Tema changes

--- app/models/observers/team_observer.rb
+++ app/models/observers/team_observer.rb
@@ -1,13 +1,13 @@
 class TeamObserver < ActiveRecord::Observer
   def after_destroy(team)
-    Result.where(team_id: team.id).update_all(team_id: nil, team_name: nil)
+    Result.where(team_id: team.id).update_all(team_id: nil, team_name: nil, team_member: false)
     true
   end
 
   def after_update(team)
-    if team.name_changed?
+    if team.name_changed? || team.member_changed?
       team.results.each do |result|
-        if result.team_name != team.name(result.year)
+        if result.team_name != team.name(result.year) || result.team_member? != team.member?
           result.cache_attributes! :non_event
         end
       end
