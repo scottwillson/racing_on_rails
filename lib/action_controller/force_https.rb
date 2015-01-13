0bc29106222c0fbe28c49df651866d546d7d4f10
Scott Willson <scott.willson@gmail.com>
2015-01-13 11:38:40 -0800

    Don't force https in development

--- lib/action_controller/force_https.rb
+++ lib/action_controller/force_https.rb
@@ -15,7 +15,7 @@ module ActionController
       def force_https(options = {})
         options.delete(:host)
         before_filter(options) do
-          if !request.ssl? && RacingAssociation.current.ssl?
+          if !Rails.env.development? && !request.ssl? && RacingAssociation.current.ssl?
             redirect_to "https://#{request.host}#{request.original_fullpath}", status: :moved_permanently
             flash.keep
             false
