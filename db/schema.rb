7668dfa5f3e110e89a60d43946ea563a7af0d901
Scott Willson <scott.willson@gmail.com>
2015-01-21 17:41:32 -0800

    Enlarge order error message column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212193605) do
+ActiveRecord::Schema.define(version: 20150122013156) do
 
   create_table "adjustments", force: true do |t|
     t.integer  "order_id"
@@ -433,7 +433,7 @@ ActiveRecord::Schema.define(version: 20141212193605) do
     t.datetime "purchase_time"
     t.string   "ip_address"
     t.boolean  "waiver_accepted"
-    t.string   "error_message"
+    t.string   "error_message",     limit: 2048
     t.string   "previous_status"
     t.boolean  "suggest",                                                 default: true
     t.decimal  "old_purchase_fees",              precision: 10, scale: 2
