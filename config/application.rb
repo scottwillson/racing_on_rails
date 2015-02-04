8817ad0006995b7f0ef9dc0ecd745302cefa33ab
Scott Willson <scott.willson@gmail.com>
2015-02-04 13:40:01 -0800

    Rev asset version to fix mistake with assets

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,7 @@ module RacingOnRails
     config.encoding = "utf-8"
 
     config.assets.enabled = true
-    config.assets.version = "1.1"
+    config.assets.version = "1.2"
 
     config.time_zone = "Pacific Time (US & Canada)"
 
