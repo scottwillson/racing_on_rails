48d446a69f93e92ba23d890e4dc40c5597dce6fe
Scott Willson <scott.willson@gmail.com>
2015-02-15 18:50:46 -0800

    Precompile bootstrap fonts

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,6 @@
 RacingOnRails::Application.configure do
   config.assets.precompile += %w(
+    %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
     admin.js
     racing_association.css
     racing_association.js
