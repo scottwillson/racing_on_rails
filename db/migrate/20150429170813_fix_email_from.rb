ed92988fc33629d8d875dc10836e4f1fd8afabe6
Scott Willson <scott.willson@gmail.com>
2015-04-29 10:59:06 -0700

    Add migration to fix post from email

---
+++ db/migrate/20150429170813_fix_email_from.rb
@@ -0,0 +1,33 @@
+class FixEmailFrom < ActiveRecord::Migration
+  def change
+    Post.transaction do
+      %w{ obra obrarace }.each do |list_name|
+        if File.exists?("tmp/#{list_name}.txt")
+          puts "=== #{list_name} ==="
+
+          members = {}
+          File.readlines("tmp/#{list_name}.txt").each do |member|
+            if member["<"]
+              name = member[/([^<]+)/].strip
+              email = member.match(/<(.*)>/)[1]
+            else
+              name = member
+              email = member
+            end
+            members[name] = email
+          end
+
+          MailingList.where(name: list_name).first.posts.where("from_email like '%list.obra.org'").each do |post|
+            email = members[post.from_name] || Person.where_name_or_number_like(post.from_name).last.try(:email)
+            if email.nil?
+              puts "!! '#{post.from_name}' not found"
+            else
+              puts "OK #{post.from_name} to #{email}"
+              post.update_column :from_email, email
+            end
+          end
+        end
+      end
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404143640) do
+ActiveRecord::Schema.define(version: 20150429170813) do
 
   create_table "adjustments", force: true do |t|
     t.integer  "order_id"
