a962a4c0b555cd6b756891c1b28ce63449a3ca09
Scott Willson <scott.willson@gmail.com>
2015-05-24 12:43:24 -0700

    Add working (per spec) version of Short Track Team Standings. Needs rework, pending rules clarification.

--- app/models/competitions/portland_short_track_series/team_standings.rb
+++ app/models/competitions/portland_short_track_series/team_standings.rb
@@ -40,37 +40,187 @@ module Competitions
         true
       end
 
+      def members_only?
+        false
+      end
+
       def source_events?
         true
       end
 
-      def results_per_event
-        4
+      def use_source_result_points?
+        true
       end
 
       def maximum_events(race)
         6
       end
 
-      # Unique reshuffling of results for this competition
+      # Unique reshuffling of results for this competition before calculation
       def after_source_results(results, race)
-        results = select_source_event_results(results, race)
-
+        # Partition by event and apply all below:
         # Partition results into categories based on participant age and source result gender
         # - M/F: 10-14, 15-18, 19-34, 35-44, 45-54, 55+
         # - infer age from category if there is no participant age
         # Sort by ability category, then by place
         # reject bottom 10% from each category except the "lowest" (Cat 3s)
         # assign points from 0-100 by 100 * ( n - p + 1 ) / n where n = age/gender category size and p = place
-        # select top 4 team results (should be handled by results_per_event), though … based on last year's results,
+        # rules say: select top 4 team results (should be handled by results_per_event),
+        #   though … based on last year's results,
         #   it looks like we're taking top-4 in each category
+        # refactor: after partitioning, each partition can be transformed independently
+        # refactor: can remove results = bits now
 
-        results
+        source_event_ids(race).map do |event_id|
+          _results = results.select { |r| r["event_id"] == event_id }
+
+          _results = _results.map do |r|
+            if r["category_name"] == "Clydesdale"
+              r["category_ability"] = 2
+            end
+            if r["category_name"] == "Singlespeed"
+              r["category_ability"] = 0
+            end
+            r
+          end
+
+          _results = partition_results_by_age_and_gender(_results)
+          _results = sort_results_by_ability_category_and_place(_results)
+          _results = reject_worst_results(_results)
+          _results = add_points(_results)
+          _results = take_top_4(_results)
+          _results.values.flatten
+        end.
+        flatten
       end
 
-      def select_source_event_results(results, race)
-        source_event_ids = source_event_ids(race)
-        results.select { |r| r["event_id"].in?(source_event_ids) }
+      def partition_results_by_age_and_gender(results)
+        partitioned_results = Hash.new { |h, k| h[k] = [] }
+
+        results.each do |result|
+          category = partition_category_for(result)
+          raise("No category found for #{result}") unless category
+          partitioned_results[category] << result
+        end
+
+        partitioned_results
+      end
+
+      def partition_category_for(result)
+        partition_categories.detect do |category|
+          gender = result["gender"] || result["person_gender"] || "M"
+
+          age = result["age"] || result["category_ages_begin"]
+          if age == 0
+            age = 19
+          end
+
+          ages_begin = category.ages_begin
+          if ages_begin == 0
+            ages_begin = 19
+          end
+
+          ages_begin <= age &&
+          category.ages_end   >= age &&
+          category.gender     == gender
+        end
+      end
+
+      def partition_categories
+        @partition_categories ||= find_or_create_partition_categories
+      end
+
+      def find_or_create_partition_categories
+        %w{ Men Women }.map do |gender|
+          [ "10-14", "15-18", "19-34", "35-44", "45-54", "55+" ].map do |ages|
+            Category.find_or_create_by_normalized_name("#{gender} #{ages}")
+          end
+        end.
+        flatten
+      end
+
+      def sort_results_by_ability_category_and_place(results)
+        sorted_results = Hash.new
+        results.each do |category, category_results|
+          sorted_results[category] = sort_by_ability_category_and_place(category_results)
+        end
+        sorted_results
+      end
+
+      def sort_by_ability_category_and_place(results)
+        results.sort do |x, y|
+          diff = x["category_ability"] <=> y["category_ability"]
+
+          if diff == 0
+            numeric_place(x) <=> numeric_place(y)
+          else
+            diff
+          end
+        end
+      end
+
+      # Similar methods work on object attributes, not hash values
+      def numeric_place(result)
+        if result["place"] && result["place"].to_i > 0
+          result["place"].to_i
+        else
+          Float::INFINITY
+        end
+      end
+
+      def reject_worst_results(results)
+        filtered_results = Hash.new
+        results.each do |category, category_results|
+          filtered_results[category] = reject_worst_category_results(category_results)
+        end
+        filtered_results
+      end
+
+      def reject_worst_category_results(results)
+        results.
+        group_by { |r| r["category_ability"] }.
+        map do |category_ability, category_results|
+          if category_ability < 3
+            category_results[ 0, (category_results.size * 0.9).ceil ]
+          else
+            category_results
+          end
+        end.
+        flatten
+      end
+
+      def add_points(results)
+        results_with_points = Hash.new
+        results.each do |category, category_results|
+          results_with_points[category] = add_category_points(category_results)
+        end
+        results_with_points
+      end
+
+      def add_category_points(results)
+        results.map.with_index do |result, index|
+          place = index + 1
+          result["points"] = 100.0 * ((results.size - place) + 1) / results.size
+          result
+        end
+      end
+
+      def take_top_4(results)
+        top_results = Hash.new
+        results.each do |category, category_results|
+          top_results[category] = take_top_4_in_category(category_results)
+        end
+        top_results
+      end
+
+      def take_top_4_in_category(results)
+        results.
+        group_by { |r| r["participant_id"] }.
+        map do |participant_id, participant_results|
+          [ participant_id, participant_results[0, 4] ]
+        end.
+        map(&:last).
+        flatten
       end
     end
   end
--- test/models/events/competitions/portland_short_track_series_test.rb
+++ test/models/events/competitions/portland_short_track_series_test.rb
@@ -5,11 +5,134 @@ module Competitions
   class PortlandShortTrackSeriesTest < ActiveSupport::TestCase
     test "calculate" do
       weekly_series = FactoryGirl.create(:weekly_series, name: "Portland Short Track Series MTB STXC")
-      weekly_series.races.create!(category: Category.create!(name: "Pro")).results.create!(place: 1, person: Person.new)
-      
+      weekly_series.races.create!(category: Category.create!(name: "Elite Men")).results.create!(place: 1, person: Person.new, age: 30)
+
       PortlandShortTrackSeries::Overall.calculate!
       PortlandShortTrackSeries::MonthlyStandings.calculate!
       PortlandShortTrackSeries::TeamStandings.calculate!
+    end
+
+    test "partition_results_by_age_and_gender" do
+      results = [
+        { "category_name" => "Junior Men 10-14", "age" => 10, "gender" => "M" },
+        { "category_name" => "Junior Men 10-14", "age" => 14, "gender" => "M" }
+      ]
+      team_standings = PortlandShortTrackSeries::TeamStandings.new
+
+      partitioned_results = team_standings.partition_results_by_age_and_gender(results)
+      assert_equal 1, partitioned_results.keys.size, partitioned_results.keys
+      assert_equal "Men 10-14", partitioned_results.keys.first.name
+      assert_equal [ results ], partitioned_results.values
+    end
+
+    test "partition_category_for" do
+      team_standings = PortlandShortTrackSeries::TeamStandings.new
+
+      result = { "category_name" => "Junior Men 10-14", "age" => 11, "gender" => "M", "category_ages_begin" => 10 }
+      assert_equal "Men 10-14", team_standings.partition_category_for(result).name, "Junior Men 10-14"
+
+      result = { "category_name" => "Category 2 Women U35", "age" => 33, "gender" => "F", "category_ages_begin" => 19 }
+      assert_equal "Women 19-34", team_standings.partition_category_for(result).name, "Category 2 Women U35"
+
+      result = { "category_name" => "Elite Men", "age" => 22, "gender" => "M", "category_ages_begin" => 0 }
+      assert_equal "Men 19-34", team_standings.partition_category_for(result).name, "Elite Men"
+
+      result = { "category_name" => "Men 19-34", "gender" => "M", "category_ages_begin" => 19 }
+      assert_equal "Men 19-34", team_standings.partition_category_for(result).name, "Men 19-34, no result age"
+    end
+
+    test "sort_results_by_ability_category_and_place" do
+      team_standings = PortlandShortTrackSeries::TeamStandings.new
+
+      results = [
+        { "category_ability" => 3, "place" => "4" },
+        { "category_ability" => 0, "place" => "5" },
+        { "category_ability" => 2, "place" => "4" },
+        { "category_ability" => 2, "place" => "63" },
+        { "category_ability" => 2, "place" => "9" },
+        { "category_ability" => 2, "place" => "51" },
+        { "category_ability" => 1, "place" => "4" },
+      ]
+
+      expected = [
+        { "category_ability" => 0, "place" => "5" },
+        { "category_ability" => 1, "place" => "4" },
+        { "category_ability" => 2, "place" => "4" },
+        { "category_ability" => 2, "place" => "9" },
+        { "category_ability" => 2, "place" => "51" },
+        { "category_ability" => 2, "place" => "63" },
+        { "category_ability" => 3, "place" => "4" },
+      ]
+
+      actual = team_standings.sort_by_ability_category_and_place(results)
+
+      assert_equal expected, actual
+    end
+
+    test "reject_worst_category_results" do
+      team_standings = PortlandShortTrackSeries::TeamStandings.new
+
+      results = [
+        { "category_ability" => 0, "place" => "5" },
+        { "category_ability" => 1, "place" => "4" },
+        { "category_ability" => 2, "place" => "4" },
+        { "category_ability" => 2, "place" => "9" },
+        { "category_ability" => 2, "place" => "51" },
+        { "category_ability" => 2, "place" => "63" },
+        { "category_ability" => 2, "place" => "64" },
+        { "category_ability" => 2, "place" => "65" },
+        { "category_ability" => 2, "place" => "66" },
+        { "category_ability" => 2, "place" => "66" },
+        { "category_ability" => 2, "place" => "66" },
+        { "category_ability" => 2, "place" => "67" },
+        { "category_ability" => 2, "place" => "68" },
+        { "category_ability" => 2, "place" => "69" },
+        { "category_ability" => 3, "place" => "102" },
+      ]
+
+      expected = [
+        { "category_ability" => 0, "place" => "5" },
+        { "category_ability" => 1, "place" => "4" },
+        { "category_ability" => 2, "place" => "4" },
+        { "category_ability" => 2, "place" => "9" },
+        { "category_ability" => 2, "place" => "51" },
+        { "category_ability" => 2, "place" => "63" },
+        { "category_ability" => 2, "place" => "64" },
+        { "category_ability" => 2, "place" => "65" },
+        { "category_ability" => 2, "place" => "66" },
+        { "category_ability" => 2, "place" => "66" },
+        { "category_ability" => 2, "place" => "66" },
+        { "category_ability" => 2, "place" => "67" },
+        { "category_ability" => 2, "place" => "68" },
+        { "category_ability" => 3, "place" => "102" },
+      ]
+
+      actual = team_standings.reject_worst_category_results(results)
+
+      assert_equal expected, actual
+    end
+
+    test "add_category_points" do
+      team_standings = PortlandShortTrackSeries::TeamStandings.new
+      results = [
+        { "category_ability" => 0, "place" => "1" },
+        { "category_ability" => 2, "place" => "1" },
+        { "category_ability" => 2, "place" => "2" },
+        { "category_ability" => 3, "place" => "1" },
+        { "category_ability" => 3, "place" => "2" },
+      ]
+
+      expected = [
+        { "category_ability" => 0, "place" => "1", "points" => 100 },
+        { "category_ability" => 2, "place" => "1", "points" => 80 },
+        { "category_ability" => 2, "place" => "2", "points" => 60 },
+        { "category_ability" => 3, "place" => "1", "points" => 40 },
+        { "category_ability" => 3, "place" => "2", "points" => 20 },
+      ]
+
+      actual = team_standings.add_category_points(results)
+
+      assert_equal expected, actual
     end
   end
 end
