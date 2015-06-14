9a8fcba69dee990190a27476b45f1cd764c7320a
Scott Willson <scott.willson@gmail.com>
2015-06-13 20:51:46 -0700

    Set created_at for all people

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150527151802) do
+ActiveRecord::Schema.define(version: 20150614034807) do
 
   create_table "adjustments", force: true do |t|
     t.integer  "order_id"
