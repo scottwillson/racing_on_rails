e7f5fecc4e3c6508aa7371e754b548218eeac30c
Scott Willson <scott.willson@gmail.com>
2015-06-17 12:26:24 -0700

    Merge people who have their first and last names swapped

---
+++ db/migrate/20150617190957_merge_people_with_swapped_first_and_last_names.rb
@@ -0,0 +1,20 @@
+class MergePeopleWithSwappedFirstAndLastNames < ActiveRecord::Migration
+  def change
+    Person.current = RacingAssociation.current.person
+
+    Person.transaction do
+      Person.where.not(first_name: nil).where.not(last_name: nil).where.not(license: nil).find_each do |person|
+        if (person.first_name.size > 3 || person.last_name.size > 3) &&
+          Person.where(first_name: person.last_name).where(last_name: person.first_name).exists?
+
+          Person.where(first_name: person.last_name).where(last_name: person.first_name).each do |to_merge|
+            if to_merge.license.blank? && person.results.count > to_merge.results.count && to_merge.orders.count == 0
+              say "Merge #{to_merge.name} => #{person.name} (created by #{to_merge.created_by.try(&:name)})"
+              Person.find(person.id).merge(Person.find(to_merge))
+            end
+          end
+        end
+      end
+    end
+  end
+end
