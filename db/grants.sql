fb3b81feeb57b70ab5533d3f5fa86a3d270ede43
Scott Willson <scott.willson@gmail.com>
2015-08-12 15:06:42 -0700

    Use branch DB names

--- db/grants.sql
+++ db/grants.sql
@@ -1,2 +1,2 @@
-grant all privileges on racing_on_rails_development.* to 'ror_development'@'localhost';
-grant all privileges on racing_on_rails_test.* to 'ror_test'@'localhost';
+grant all privileges on racing_on_rails_4_development.* to 'ror_development'@'localhost';
+grant all privileges on `racing_on_rails_4_test%`.* to 'ror_test'@'localhost';
