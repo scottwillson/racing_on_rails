65e872dd4aeef47db84e6fcc57e016b6a49bfde2
Scott Willson <scott.willson@gmail.com>
2015-09-05 21:06:14 -0700

    Fix test

--- test/models/events/competitions/oregon_junior_cyclocross_series/overall_test.rb
+++ test/models/events/competitions/oregon_junior_cyclocross_series/overall_test.rb
@@ -5,14 +5,14 @@ module Competitions
     # :stopdoc:
     class OverallTest < ActiveSupport::TestCase
       test "calculate" do
-        series = OregonJuniorCyclocrossSeries.create!(date: Date.new(2004))
+        series = OregonJuniorCyclocrossSeries::Overall.create!(date: Date.new(2004))
         event = SingleDayEvent.create!(date: Date.new(2004))
         boys_10_12 = Category.find_or_create_by(name: "Boys 10-12")
         person = FactoryGirl.create(:person)
         event.races.create!(category: boys_10_12).results.create!(place: 3, person: person)
         series.source_events << event
 
-        OregonJuniorCyclocrossSeries.calculate!
+        OregonJuniorCyclocrossSeries::Overall.calculate!
       end
     end
   end
