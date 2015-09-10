1d501c19f37db0b6493081edf986a28e7ec15b5a
Scott Willson <scott.willson@gmail.com>
2015-09-10 15:18:16 -0700

    Add blank event team memberships table to fix old migrations

--- db/migrate/20150616203143_merge_people_with_same_name_and_email.rb
+++ db/migrate/20150616203143_merge_people_with_same_name_and_email.rb
@@ -1,5 +1,28 @@
 class MergePeopleWithSameNameAndEmail < ActiveRecord::Migration
+
   def change
+    create_table :event_teams do |t|
+      t.references :event, null: false
+      t.references :team, null: false
+
+      t.timestamps
+
+      t.index :event_id
+      t.index :team_id
+      t.index [ :event_id, :team_id ], unique: true
+    end
+
+    create_table :event_team_memberships do |t|
+      t.references :event_team, null: false
+      t.references :person, null: false
+
+      t.timestamps
+
+      t.index :event_team_id
+      t.index :person_id
+      t.index [ :event_team_id, :person_id ], unique: true
+    end
+
     Person.current = RacingAssociation.current.person
 
     Person.transaction do
