2bd9978622423dd61fb28c37fbbf1cbca78c8e42
Scott Willson <scott.willson@gmail.com>
2015-10-02 19:15:24 -0700

    Fix Event duplication. Blacklist a couple new Event attributes

--- app/models/events/defaults.rb
+++ app/models/events/defaults.rb
@@ -45,9 +45,11 @@ module Events
 
     def attributes_for_duplication
       _attributes = attributes.dup
-      _attributes.delete "id"
       _attributes.delete "created_at"
+      _attributes.delete "id"
+      _attributes.delete "slug"
       _attributes.delete "updated_at"
+      _attributes.delete "year"
       _attributes
     end
 
