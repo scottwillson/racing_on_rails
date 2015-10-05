5fc92be39f705c3526ad77c7edf27464f99e92f6
Scott Willson <scott.willson@gmail.com>
2015-10-05 14:01:29 -0700

    Map categories for Cross Crusade Call-ups

--- app/models/competitions/cross_crusade_callups.rb
+++ app/models/competitions/cross_crusade_callups.rb
@@ -4,9 +4,9 @@ module Competitions
 
     def category_names
       [
-        "Category A",
-        "Category B",
-        "Category C",
+        "Men A",
+        "Men B",
+        "Men C",
         "Clydesdale",
         "Junior Men",
         "Junior Women",
@@ -45,12 +45,45 @@ module Competitions
       if race.name == "Masters 35+ A"
         categories << Category.find_or_create_by(name: "Masters Men A 40+")
       end
+      if race.name == "Masters 35+ B"
+        categories << Category.find_or_create_by(name: "Masters Men B 40+")
+      end
+      if race.name == "Masters 35+ C"
+        categories << Category.find_or_create_by(name: "Masters Men C 40+")
+        categories << Category.find_or_create_by(name: "Men C 35+")
+      end
+      if race.name == "Masters 50+"
+        categories << Category.find_or_create_by(name: "Men 50+")
+        categories << Category.find_or_create_by(name: "Masters Men 50+")
+        categories << Category.find_or_create_by(name: "Masters 50+")
+      end
+      if race.name == "Masters 60+"
+        categories << Category.find_or_create_by(name: "Men 60+")
+        categories << Category.find_or_create_by(name: "Masters Men 60+")
+        categories << Category.find_or_create_by(name: "Masters 60+")
+      end
+      if race.name == "Masters Women 45+"
+        categories << Category.find_or_create_by(name: "Women 45+")
+      end
+      if race.name == "Unicycle"
+        categories << Category.find_or_create_by(name: "Stampede")
+      end
+
       categories
+    end
+
+    def source_event_types
+      [ SingleDayEvent, Event, Competitions::BlindDateAtTheDairyOverall ]
     end
 
     def after_source_results(results, race)
       results.each do |result|
         result["multiplier"] = 1
+      end
+
+      results.reject do |result|
+        jr_cats = [ "Junior Men", "Junior Women", "Junior Men (12-18)", "Junior Women (12-18)" ]
+        result["event_id"] != 24249 && result["category_name"][/Junior/] && (!result["category_name"].in?(jr_cats))
       end
     end
   end
