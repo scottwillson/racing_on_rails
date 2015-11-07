7b545cc1f7ba4a53f97f4e8374d1c77b495e9c3d
Scott Willson <scott.willson@gmail.com>
2015-11-06 21:43:43 -0800

    Decompose complex points method

--- app/models/competitions/calculations/points.rb
+++ app/models/competitions/calculations/points.rb
@@ -36,20 +36,27 @@ module Competitions
           return rules[:missing_result_penalty]
         end
 
-        if rules[:points_schedule_from_field_size]
-          base_points = (result.field_size - numeric_place(result)) + 1
-        else
-          base_points = rules[:point_schedule][numeric_place(result) - 1]
-        end
+        points = points_from_place(result, rules) || 0
+        bonus = place_bonus_points(result, rules)
+        team_size = (result.team_size || 1.0).to_f
 
-        (((base_points || 0) + place_bonus_points(result, rules)) / (result.team_size || 1.0).to_f) *
-        (result.multiplier || 1 ).to_f *
+        ((points + bonus) / team_size) *
+        multiplier(result) *
         last_event_multiplier(result, rules) *
-        field_size_multiplier(result, rules[:field_size_bonus])
+        field_size_multiplier(result, rules)
       end
 
       def point_schedule?(rules)
         rules[:point_schedule] || rules[:points_schedule_from_field_size]
+      end
+
+      # Basic: this place is worth this many points
+      def points_from_place(result, rules)
+        if rules[:points_schedule_from_field_size]
+          (result.field_size - numeric_place(result)) + 1
+        else
+          rules[:point_schedule][numeric_place(result) - 1]
+        end
       end
 
       def place_bonus_points(result, rules)
@@ -58,6 +65,10 @@ module Competitions
         else
           0
         end
+      end
+
+      def multiplier(result)
+        (result.multiplier || 1 ).to_f
       end
 
       def last_event_multiplier(result, rules)
@@ -73,7 +84,8 @@ module Competitions
         result.date == rules[:end_date]
       end
 
-      def field_size_multiplier(result, field_size_bonus)
+      def field_size_multiplier(result, rules)
+        field_size_bonus = rules[:field_size_bonus]
         if (result.multiplier.nil? || result.multiplier == 1) && field_size_bonus && result.field_size >= 75
           1.5
         else
