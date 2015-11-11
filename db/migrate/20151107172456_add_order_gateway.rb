d433f71cabada12f2e24d16bc9a7b16612f90838
Scott Willson <scott.willson@gmail.com>
2015-11-10 16:28:56 -0800

    Update purchased orders gateway

--- db/migrate/20151107172456_add_order_gateway.rb
+++ db/migrate/20151107172456_add_order_gateway.rb
@@ -2,5 +2,9 @@ class AddOrderGateway < ActiveRecord::Migration
   def change
     add_column :orders, :gateway, :string, null: true, default: nil
     add_index :orders, :gateway
+
+    if defined?(Order)
+      Order.purchased.update_all gateway: "elavon"
+    end
   end
 end
