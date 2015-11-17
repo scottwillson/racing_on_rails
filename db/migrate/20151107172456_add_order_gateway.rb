e00ff4f805ce174b807e6b09432b479ac31d81cb
Scott Willson <scott.willson@gmail.com>
2015-11-17 08:21:30 -0800

    Fix gateway migration

--- db/migrate/20151107172456_add_order_gateway.rb
+++ db/migrate/20151107172456_add_order_gateway.rb
@@ -3,8 +3,8 @@ class AddOrderGateway < ActiveRecord::Migration
     add_column :orders, :gateway, :string, null: true, default: nil
     add_index :orders, :gateway
 
-    if defined?(Order)
-      Order.purchased.update_all gateway: "elavon"
+    if RacingAssociation.current.short_name != "OBRA"
+      Order.where(gateway: nil).purchased.update_all gateway: "elavon"
     end
   end
 end
