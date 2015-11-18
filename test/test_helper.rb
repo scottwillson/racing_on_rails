b17ff3f0b0201595d701bcccbc2e8a47a330a9e7
Scott Willson <scott.willson@gmail.com>
2015-11-18 08:46:27 -0800

    Only load Code Climate test reporter if configured with CODECLIMATE_REPO_TOKEN

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,7 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+if ENV['CODECLIMATE_REPO_TOKEN']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
 
 ENV["RAILS_ENV"] = "test"
 
