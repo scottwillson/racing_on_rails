0c4f95d2f768fd65476a6ec303c5ce86709f1ae8
Scott Willson <scott.willson@gmail.com>
2015-12-19 21:53:30 -0800

    Add missing assets to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,8 @@ RacingOnRails::Application.configure do
   config.assets.precompile += %w(
     %r(bootstrap-sass/assets/fonts/bootstrap/[\w-]+\.(?:eot|svg|ttf|woff2?)$)
     admin.js
+    apple-touch-icon.png
+    favicon.ico
     racing_association.css
     racing_association.js
   )
