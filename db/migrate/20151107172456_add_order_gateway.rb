a13d10727faa72ee90b97e92da1607d979f4c3de
Scott Willson <scott.willson@gmail.com>
2016-01-29 07:43:44 -0800

    Hack around dupe DB columns to finish deploy after bad migration

--- db/migrate/20151107172456_add_order_gateway.rb
+++ db/migrate/20151107172456_add_order_gateway.rb
@@ -1,7 +1,7 @@
 class AddOrderGateway < ActiveRecord::Migration
   def change
-    add_column :orders, :gateway, :string, null: true, default: nil
-    add_index :orders, :gateway
+    add_column(:orders, :gateway, :string, null: true, default: nil) rescue nil
+    add_index(:orders, :gateway) rescue nil
 
     if RacingAssociation.current.short_name == "NABRA"
       Order.where(gateway: nil).purchased.update_all gateway: "elavon"
