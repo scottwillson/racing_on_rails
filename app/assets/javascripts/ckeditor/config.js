9969b850b033c17f8ddfe4c064cd4b0fba44ae60
Scott Willson <scott.willson@gmail.com>
2016-05-24 09:24:12 -0700

    Add custom toolbar with more buttons for articles

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -1,5 +1,4 @@
-CKEDITOR.editorConfig = function( config )
-{
+CKEDITOR.editorConfig = function(config) {
   config.allowedContent = true;
 
   config.toolbar = [
@@ -8,6 +7,16 @@ CKEDITOR.editorConfig = function( config )
     { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule' ] },
     { name: 'paragraph', groups: [ 'list' ], items: [ 'NumberedList', 'BulletedList' ] },
     { name: 'styles', items: [ 'Format' ] },
+    { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Maximize' ] }
+  ];
+
+  config.toolbar_Articles = [
+    { name: 'undo', groups: [ 'undo' ], items: [ 'Undo', 'Redo' ] },
+    { name: 'links', items: [ 'Link', 'Unlink' ] },
+    { name: 'insert', items: [ 'Image', 'Table', 'HorizontalRule' ] },
+    { name: 'paragraph', groups: [ 'list' ], items: [ 'NumberedList', 'BulletedList' ] },
+    { name: 'styles', items: [ 'Styles', 'Format', 'Font', 'FontSize' ] },
+    { name: 'colors', items: [ 'TextColor', 'BGColor' ] },
     { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ], items: [ 'Bold', 'Italic', 'Underline', 'Maximize' ] }
   ];
 };
