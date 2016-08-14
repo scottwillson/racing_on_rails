7ad2bfa92c463e8897432b20f3f5d277df3eefb9
Scott Willson <scott.willson@gmail.com>
2016-08-14 15:01:35 -0700

    Add create user for latest MySQL

--- db/grants.sql
+++ db/grants.sql
@@ -1,2 +1,5 @@
+create user if not exists 'ror_development'@'localhost';
+create user if not exists 'ror_test'@'localhost';
+
 grant all privileges on racing_on_rails_development.* to 'ror_development'@'localhost';
-grant all privileges on `racing_on_rails_test%`.* to 'ror_test'@'localhost';
+grant all privileges on `racing_on_rails_test`.* to 'ror_test'@'localhost';
