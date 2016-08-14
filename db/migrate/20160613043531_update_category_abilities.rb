458f67029a58dff37acfd24412c5377e3945a161
Scott Willson <scott.willson@gmail.com>
2016-08-14 15:57:31 -0700

    Show category attributes

--- db/migrate/20160613043531_update_category_abilities.rb
+++ db/migrate/20160613043531_update_category_abilities.rb
@@ -4,6 +4,7 @@ class UpdateCategoryAbilities < ActiveRecord::Migration
       Category.all.each do |category|
         category.set_ability_range_from_name
         category.ages = category.ages_from_name(category.name)
+        puts "#{category.name} #{category.ages}"
         category.save!
       end
     end
