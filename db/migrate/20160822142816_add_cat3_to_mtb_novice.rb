60df81dbb744c6c69d83ce6056e6d2b40df98d60
Scott Willson <scott.willson@gmail.com>
2016-08-30 12:39:31 -0700

    Set reasonable parent for new MTB Novice cats

--- db/migrate/20160822142816_add_cat3_to_mtb_novice.rb
+++ db/migrate/20160822142816_add_cat3_to_mtb_novice.rb
@@ -5,6 +5,8 @@ class AddCat3ToMtbNovice < ActiveRecord::Migration
         name = race.name.gsub("Novice", "Novice (Category 3)")
         puts "#{race.name} => #{name}"
         category = Category.find_or_create_by_normalized_name(name)
+        category.parent = race.category.parent
+        category.save!
         race.category = category
         race.save!
       end
