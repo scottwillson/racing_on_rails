7948f7f67deb5227be9f817a72122f9d846e3b6f
Scott Willson <scott.willson@gmail.com>
2017-01-17 15:22:14 -0800

    Optimize team name and member updates for teams with many results

--- app/models/observers/team_observer.rb
+++ app/models/observers/team_observer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class TeamObserver < ActiveRecord::Observer
   def after_destroy(team)
     Result.where(team_id: team.id).update_all(team_id: nil, team_name: nil, team_member: false)
@@ -5,13 +7,14 @@ class TeamObserver < ActiveRecord::Observer
   end
 
   def after_update(team)
-    if team.name_changed? || team.member_changed?
-      team.results.each do |result|
-        if result.team_name != team.name(result.year) || result.team_member? != team.member?
-          result.cache_attributes! :non_event
-        end
-      end
+    if team.member_changed?
+      Result.where(team_id: team.id).update_all(team_member: team.member)
     end
+
+    if team.name_changed?
+      Result.where(team_id: team.id, year: RacingAssociation.current.year).update_all(team_name: team.name)
+    end
+
     true
   end
 end
