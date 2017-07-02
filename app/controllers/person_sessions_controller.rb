a20680f4e715cf96fd8af03af34c9774d6981af2
Scott Willson <scott.willson@gmail.com>
2017-07-02 08:20:08 -0700

    Sanitize Authlogic params

--- app/controllers/person_sessions_controller.rb
+++ app/controllers/person_sessions_controller.rb
@@ -1,17 +1,19 @@
+# frozen_string_literal: true
+
 class PersonSessionsController < ApplicationController
   force_https
   before_action :require_current_person, only: :show
 
   def new
     if current_person
-      return render(:show)
+      render(:show)
     else
       @person_session = PersonSession.new
     end
   end
 
   def create
-    @person_session = PersonSession.new(params[:person_session])
+    @person_session = PersonSession.new(person_session_params)
     @person_session.remember_me = true
     if @person_session.save
       flash.discard
@@ -27,8 +29,14 @@ class PersonSessionsController < ApplicationController
 
   def destroy
     session[:return_to] = nil
-    current_person_session.destroy if current_person_session
+    current_person_session&.destroy
     flash[:notice] = "You are logged-out"
     redirect_back_or_default new_person_session_url(secure_redirect_options)
+  end
+
+  private
+
+  def person_session_params
+    params_without_mobile.require(:person_session).permit(:email, :login, :password)
   end
 end
