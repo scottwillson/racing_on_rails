7118b61bf077ed0874858338a052645b7f56f589
Scott Willson <scott.willson@gmail.com>
2018-05-13 08:43:51 -0700

    Add paper trail fields to discount_codes and refunds

--- db/migrate/20180512183511_add_created_and_updated_by.rb
+++ db/migrate/20180512183511_add_created_and_updated_by.rb
@@ -1,5 +1,12 @@
 class AddCreatedAndUpdatedBy < ActiveRecord::Migration
   def change
+    change_table :discount_codes do |t|
+      t.integer :created_by_paper_trail_id, default: nil
+      t.string :created_by_paper_trail_type, default: nil
+      t.integer :updated_by_paper_trail_id, default: nil
+      t.string :updated_by_paper_trail_type, default: nil
+    end
+
     change_table :events do |t|
       t.integer :created_by_paper_trail_id, default: nil
       t.string :created_by_paper_trail_type, default: nil
@@ -29,6 +36,13 @@ class AddCreatedAndUpdatedBy < ActiveRecord::Migration
     end
 
     change_table :races do |t|
+      t.integer :created_by_paper_trail_id, default: nil
+      t.string :created_by_paper_trail_type, default: nil
+      t.integer :updated_by_paper_trail_id, default: nil
+      t.string :updated_by_paper_trail_type, default: nil
+    end
+
+    change_table :refunds do |t|
       t.integer :created_by_paper_trail_id, default: nil
       t.string :created_by_paper_trail_type, default: nil
       t.integer :updated_by_paper_trail_id, default: nil
