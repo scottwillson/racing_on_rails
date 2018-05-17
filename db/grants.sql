89834cd5997eb708d96f6cf44cd6cd0aca707a01
Scott Willson <scott.willson@gmail.com>
2018-05-17 07:31:42 -0700

    Remove _pt database prefix

--- db/grants.sql
+++ db/grants.sql
@@ -1,5 +1,5 @@
 create user if not exists 'ror_development'@'localhost';
 create user if not exists 'ror_test'@'localhost';
 
-grant all privileges on racing_on_rails_development_pt.* to 'ror_development'@'localhost';
-grant all privileges on `racing_on_rails_test_pt%`.* to 'ror_test'@'localhost';
+grant all privileges on racing_on_rails_development.* to 'ror_development'@'localhost';
+grant all privileges on `racing_on_rails_test%`.* to 'ror_test'@'localhost';
