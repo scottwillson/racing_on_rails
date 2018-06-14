79cf5d449a52bbc87233ea4795334e8dcebeffaf
Scott Willson <scott.willson@gmail.com>
2018-06-13 20:38:29 -0700

    Use Category 1 Men U45 in Monday STXC competition

--- app/models/competitions/portland_short_track_series/common.rb
+++ app/models/competitions/portland_short_track_series/common.rb
@@ -13,7 +13,7 @@ module Competitions
 
       def category_names
         [
-          "Category 1 Men 19-44",
+          "Category 1 Men U45",
           "Category 1 Men 45+",
           "Category 2 Men 40-49",
           "Category 2 Men 50-59",
@@ -48,9 +48,9 @@ module Competitions
           "Category 2 Men 60+" => "Category 3 Men 50+",
           "Category 2 Women U45" => ["Category 3 Women 10-13", "Category 3 Women 14-18", "Category 3 Women 19+"],
           "Category 2 Women 45+" => "Category 3 Women 19+",
-          "Category 1 Men 19-44" => ["Category 2 Men 40-49", "Category 2 Men U40"],
+          "Category 1 Men U45" => ["Category 2 Men 40-49", "Category 2 Men U40"],
           "Category 1 Men 45+" => ["Category 2 Men 40-49", "Category 2 Men 50-59", "Category 2 Men 60+"],
-          "Elite Men" => ["Category 1 Men 19-44", "Category 1 Men 45+"],
+          "Elite Men" => ["Category 1 Men U45", "Category 1 Men 45+"],
           "Elite/Category 1 Women" => ["Category 2 Women 45+", "Category 2 Women U45"]
         }
       end
