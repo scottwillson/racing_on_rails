9dab763d02379d1311ee0647f364ec0625f2285d
Scott Willson <scott.willson@gmail.com>
2018-09-01 15:11:37 -0700

    Fix TeamObserver updates

--- app/models/observers/team_observer.rb
+++ app/models/observers/team_observer.rb
@@ -3,16 +3,19 @@
 class TeamObserver < ActiveRecord::Observer
   def after_destroy(team)
     Result.where(team_id: team.id).update_all(team_id: nil, team_name: nil, team_member: false)
-    true
   end
 
   def around_update(team)
     member_changed = team.member_changed?
+    name_changed = team.name_changed?
 
     yield
 
     if member_changed
       Result.where(team_id: team.id).update_all(team_member: team.member)
+    end
+
+    if name_changed
       Result.where(team_id: team.id, year: RacingAssociation.current.year).update_all(team_name: team.name)
     end
   end
