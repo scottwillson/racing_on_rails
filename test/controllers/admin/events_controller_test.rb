32ed7229c07b105b6b12b6101d63ffbfa60c6a6b
Scott Willson <scott.willson@gmail.com>
2018-09-13 14:30:09 -0700

    Fix reload call

--- test/controllers/admin/events_controller_test.rb
+++ test/controllers/admin/events_controller_test.rb
@@ -190,8 +190,9 @@ module Admin
       assert_not_nil(assigns(:races), "@races")
       assert_not_nil(assigns(:combined_results), "@combined_results")
       assert_response(:success)
-      assert_equal(0, jack_frost.races.reload.count, "Races after destroy")
-      assert_nil(jack_frost.combined_results.reload, "Event should have not combined results after destroying races")
+      jack_frost.reload
+      assert_equal(0, jack_frost.races.count, "Races after destroy")
+      assert_nil(jack_frost.combined_results, "Event should have not combined results after destroying races")
     end
 
     test "events for year" do
