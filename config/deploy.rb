41bb96a1c18f75fa28341ae5834da8e504a0682e
Scott Willson <scott.willson@gmail.com>
2018-10-30 07:54:04 -0700

    Deploy branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,6 +19,7 @@ set :deploy_to, "/var/www/rails/#{fetch(:application)}"
 
 set :repo_url, "git://github.com/scottwillson/racing_on_rails.git"
 set :site_local_repo_url, "git@github.com:scottwillson/#{fetch(:application)}-local.git"
+set :branch, "rails-5.2.1"
 
 set :user, "app"
 
@@ -45,7 +46,7 @@ namespace :deploy do
 
         execute :rm, "-rf \"#{release_path}/lib/registration_engine\"" if test("[ -L \"#{release_path}/lib/registration_engine\" ]")
 
-        execute :git, "clone git@github.com:scottwillson/registration_engine.git #{release_path}/lib/registration_engine"
+        execute :git, "clone git@github.com:scottwillson/registration_engine.git -b rails-5.2.1 #{release_path}/lib/registration_engine"
       end
     end
   end
