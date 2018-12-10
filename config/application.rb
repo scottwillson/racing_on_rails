78c60cdc2246f85c30f834c2e455031cc3561d03
Scott Willson <scott.willson@gmail.com>
2018-12-09 23:57:15 -0800

    Rev asset versions

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,7 @@ module RacingOnRails
     config.encoding = "utf-8"
 
     config.assets.enabled = true
-    config.assets.version = "2.1"
+    config.assets.version = "2.2"
 
     config.time_zone = "Pacific Time (US & Canada)"
 
