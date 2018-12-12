a2a0964f83ba4a7b3de43d2434da61ca692aa9d2
Scott Willson <scott.willson@gmail.com>
2018-12-12 08:40:58 -0800

    Remove mailing list posting test

--- tests_isolated/acceptance/mailing_lists_test.rb
+++ tests_isolated/acceptance/mailing_lists_test.rb
@@ -19,46 +19,9 @@ class MailingListsTest < AcceptanceTest
     visit "/mailing_lists/#{mailing_list.id}/posts"
     assert_no_text "Cervelo for sale"
 
-    visit "/mailing_lists/#{mailing_list.id}/posts/new"
-
-    fill_in "post_from_name", with: "Scott"
-    fill_in "post_from_email", with: "scott.willson@gmail.com"
-    fill_in "post_subject", with: "Cervelo for sale"
-    fill_in "post_body", with: "My post message body"
-
-    click_button "Post"
-    assert_page_has_content "Your new post is now in the mailing queue"
-
-    visit "/mailing_lists/#{mailing_list.id}/posts"
 
     click_link "Schedule Changes"
     assert_page_has_content "This is a test message."
-
-    click_link "Reply Privately"
-
-    fill_in "post_from_name", with: "Don"
-    fill_in "post_body", with: "This is a special private reply"
-
-    click_button "Send"
-    assert_page_has_content "From email can't be blank"
-
-    fill_in "post_from_email", with: "don@butlerpress.com"
-
-    click_button "Send"
-    assert_page_has_content "Sent private reply"
-
-    visit "/mailing_lists/#{mailing_list.id}/posts/new"
-
-    fill_in "post_from_email", with: "scott.willson@gmail.com"
-    fill_in "post_subject", with: "New Message 2"
-    fill_in "post_body", with: "My post message body"
-
-    click_button "Post"
-    assert_page_has_content "From name can't be blank"
-
-    fill_in "post_from_name", with: "Scott"
-    click_button "Post"
-    assert_page_has_content "Your new post is now in the mailing queue"
 
     visit "/mailing_lists/#{mailing_list.id}/posts?subject=Schedule"
     visit "/mailing_lists/#{mailing_list.id}/posts?subject=xy"
