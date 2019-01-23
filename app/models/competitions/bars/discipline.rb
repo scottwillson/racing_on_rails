8c15a01934de6b171296936bf47492625a918d4a
Scott Willson <scott.willson@gmail.com>
2019-01-23 11:27:22 -0800

    Put Gravel and Gran Fondo disciplines into Gravel BAR

--- app/models/competitions/bars/discipline.rb
+++ app/models/competitions/bars/discipline.rb
@@ -6,7 +6,9 @@ module Competitions
       def disciplines_for(race)
         case race.discipline
         when "Road"
-          ["Road", "Road/Gravel", "Circuit"]
+          ["Road", "Circuit"]
+        when "Gravel"
+          ["Gravel", "Gran Fondo"]
         when "Mountain Bike"
           ["Mountain Bike", "Downhill", "Super D"]
         else
