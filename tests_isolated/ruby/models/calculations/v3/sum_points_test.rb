0953eb8276cd01f5094001f1f8f0cdc1401e04b0
Scott Willson <scott.willson@gmail.com>
2019-01-25 15:56:14 -0800

    Fix Ruby calculation tests

--- tests_isolated/ruby/models/calculations/v3/sum_points_test.rb
+++ tests_isolated/ruby/models/calculations/v3/sum_points_test.rb
@@ -8,11 +8,11 @@ require_relative "../../../../../app/models/calculations/v3/models"
 require_relative "../../../../../app/models/calculations/v3/models/calculated_result"
 require_relative "../../../../../app/models/calculations/v3/models/event_category"
 require_relative "../../../../../app/models/calculations/v3/steps"
-require_relative "../../../../../app/models/calculations/v3/steps/assign_points"
+require_relative "../../../../../app/models/calculations/v3/steps/sum_points"
 
 # :stopdoc:
-class Calculations::V3::AssignPointsTest < Ruby::TestCase
-  def test_assign_points
+class Calculations::V3::SumPointsTest < Ruby::TestCase
+  def test_calculate
     category = Calculations::V3::Models::Category.new("Masters Men")
     rules = Calculations::V3::Rules.new(categories: [category])
 
@@ -22,6 +22,7 @@ class Calculations::V3::AssignPointsTest < Ruby::TestCase
       participant: participant,
       place: "19"
     )
+    source_result.points = 75
 
     calculator = Calculations::V3::Calculator.new(rules: rules, source_results: [source_result])
 
@@ -29,8 +30,8 @@ class Calculations::V3::AssignPointsTest < Ruby::TestCase
     result = Calculations::V3::Models::CalculatedResult.new(participant, [source_result])
     event_category.results << result
 
-    event_categories = Calculations::V3::Steps::AssignPoints.calculate!(calculator)
+    event_categories = Calculations::V3::Steps::SumPoints.calculate!(calculator)
 
-    assert_equal 100, event_categories.first.results.first.source_results.first.points
+    assert_equal 75, event_categories.first.results.first.points
   end
 end
