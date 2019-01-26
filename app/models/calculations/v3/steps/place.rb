146a7a9ce7e040410c63506a504fa10ac829bbc7
Scott Willson <scott.willson@gmail.com>
2019-01-25 16:05:23 -0800

    Implement Place step

--- app/models/calculations/v3/steps/place.rb
+++ app/models/calculations/v3/steps/place.rb
@@ -3,8 +3,10 @@
 module Calculations::V3::Steps::Place
   def self.calculate!(calculator)
     calculator.event_categories.each do |category|
+      place = 0
       category.results.each do |result|
-        result.place = "1"
+        place += 1
+        result.place = place.to_s
       end
     end
   end
--- tests_isolated/ruby/models/calculations/v3/place_test.rb
+++ tests_isolated/ruby/models/calculations/v3/place_test.rb
@@ -32,4 +32,35 @@ class Calculations::V3::PlaceTest < Ruby::TestCase
 
     assert_equal "1", calculator.event_categories.first.results.first.place
   end
+
+  def test_place_many
+    category = Calculations::V3::Models::Category.new("Masters Men")
+    rules = Calculations::V3::Rules.new(categories: [category])
+    calculator = Calculations::V3::Calculator.new(rules: rules, source_results: [])
+    event_category = calculator.event_categories.first
+
+    participant = Calculations::V3::Models::Participant.new(0)
+    source_result = Calculations::V3::Models::SourceResult.new(id: 0)
+    result = Calculations::V3::Models::CalculatedResult.new(participant, [source_result])
+    result.points = 10
+    event_category.results << result
+
+    participant = Calculations::V3::Models::Participant.new(1)
+    source_result = Calculations::V3::Models::SourceResult.new(id: 1)
+    result = Calculations::V3::Models::CalculatedResult.new(participant, [source_result])
+    result.points = 3
+    event_category.results << result
+
+    participant = Calculations::V3::Models::Participant.new(1)
+    source_result = Calculations::V3::Models::SourceResult.new(id: 2)
+    result = Calculations::V3::Models::CalculatedResult.new(participant, [source_result])
+    result.points = 7
+    event_category.results << result
+
+    Calculations::V3::Steps::Place.calculate!(calculator)
+
+    assert_equal "1", calculator.event_categories.first.results.first.place
+    assert_equal "2", calculator.event_categories.first.results.second.place
+    assert_equal "3", calculator.event_categories.first.results.third.place
+  end
 end
