e00ba8cc4cec9c0195d41f59c544682588a6c531
Scott Willson <scott.willson@gmail.com>
2019-02-26 06:46:52 -0800

    Force table creation

--- db/migrate/20190120021103_create_calculations_categories.rb
+++ db/migrate/20190120021103_create_calculations_categories.rb
@@ -2,7 +2,7 @@
 
 class CreateCalculationsCategories < ActiveRecord::Migration[5.2]
   def change
-    create_table :calculations_categories, id: false do |t| # rubocop:disable Rails/CreateTableWithTimestamps
+    create_table :calculations_categories, id: false, force: true do |t| # rubocop:disable Rails/CreateTableWithTimestamps
       t.belongs_to :calculation, index: true
       t.belongs_to :category, index: true
     end
