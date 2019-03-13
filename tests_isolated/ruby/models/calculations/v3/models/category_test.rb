e22bbc5bb3aa27aeec11115cd724805076bd5d3f
Scott Willson <scott.willson@gmail.com>
2019-03-13 14:14:22 -0700

    DRY up equality tests

--- tests_isolated/ruby/models/calculations/v3/models/category_test.rb
+++ tests_isolated/ruby/models/calculations/v3/models/category_test.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 require_relative "../../v3"
+require_relative "./equality_assertion"
 
 module Calculations
   module V3
     module Models
       # :stopdoc:
       class CategoryTest < Ruby::TestCase
+        include EqualityAssertion
+
         def test_initialize
           category = Category.new("Women")
           assert_equal "Women", category.name
@@ -18,45 +21,7 @@ module Calculations
           c = Category.new("Women")
           d = Category.new("Men")
 
-          assert_equal a, a
-          assert_equal a, b
-          assert_equal a, c
-          assert_equal b, a
-          assert_equal b, b
-          assert_equal b, c
-          assert_equal c, a
-          assert_equal c, b
-          assert_equal c, c
-          assert_equal d, d
-          refute_equal a, d
-          refute_equal b, d
-          refute_equal c, d
-          refute_equal d, a
-          refute_equal d, b
-          refute_equal d, c
-          refute_equal a, nil
-          refute_equal d, nil
-          refute_equal nil, a
-          refute_equal nil, d
-        end
-
-        def test_hash
-          a = Category.new("Women")
-          b = Category.new("Women")
-          c = Category.new("Women")
-          d = Category.new("Men")
-
-          assert_equal a.hash, a.hash
-          assert_equal a.hash, b.hash
-          assert_equal a.hash, c.hash
-          assert_equal d.hash, d.hash
-          refute_equal a.hash, d.hash
-          refute_equal b.hash, d.hash
-          refute_equal c.hash, d.hash
-          refute_equal a.hash, nil.hash
-          refute_equal d.hash, nil.hash
-          refute_equal nil.hash, a.hash
-          refute_equal nil.hash, d.hash
+          assert_equality a, b, c, d
         end
       end
     end
--- tests_isolated/ruby/models/calculations/v3/models/event_test.rb
+++ tests_isolated/ruby/models/calculations/v3/models/event_test.rb
@@ -7,6 +7,8 @@ module Calculations
     module Models
       # :stopdoc:
       class EventTest < Ruby::TestCase
+        include EqualityAssertion
+
         def test_initialize
           event = Event.new(id: 0, date: Date.new(2016))
           assert_equal Date.new(2016), event.date
@@ -30,45 +32,7 @@ module Calculations
           c = Event.new(id: 0)
           d = Event.new(id: 1)
 
-          assert_equal a, a
-          assert_equal a, b
-          assert_equal a, c
-          assert_equal b, a
-          assert_equal b, b
-          assert_equal b, c
-          assert_equal c, a
-          assert_equal c, b
-          assert_equal c, c
-          assert_equal d, d
-          refute_equal a, d
-          refute_equal b, d
-          refute_equal c, d
-          refute_equal d, a
-          refute_equal d, b
-          refute_equal d, c
-          refute_equal a, nil
-          refute_equal d, nil
-          refute_equal nil, a
-          refute_equal nil, d
-        end
-
-        def test_hash
-          a = Event.new(id: 0)
-          b = Event.new(id: 0)
-          c = Event.new(id: 0)
-          d = Event.new(id: 1)
-
-          assert_equal a.hash, a.hash
-          assert_equal a.hash, b.hash
-          assert_equal a.hash, c.hash
-          assert_equal d.hash, d.hash
-          refute_equal a.hash, d.hash
-          refute_equal b.hash, d.hash
-          refute_equal c.hash, d.hash
-          refute_equal a.hash, nil.hash
-          refute_equal d.hash, nil.hash
-          refute_equal nil.hash, a.hash
-          refute_equal nil.hash, d.hash
+          assert_equality a, b, c, d
         end
       end
     end
