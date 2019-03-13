c49ab64407cdea7b07a61e4a951198a490f557d2
Scott Willson <scott.willson@gmail.com>
2019-03-13 14:14:22 -0700

    DRY up equality tests

--- tests_isolated/ruby/models/calculations/v3/models/event_category_test.rb
+++ tests_isolated/ruby/models/calculations/v3/models/event_category_test.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 require_relative "../../v3"
+require_relative "./equality_assertion"
 
 module Calculations
   module V3
     module Models
       # :stopdoc:
       class EventCategoryTest < Ruby::TestCase
+        include EqualityAssertion
+
         def test_initialize
           participant = Participant.new(1)
           category = Category.new("Juniors")
@@ -26,45 +29,7 @@ module Calculations
           c = EventCategory.new(Category.new("Women"))
           d = EventCategory.new(Category.new("Men"))
 
-          assert_equal a, a
-          assert_equal a, b
-          assert_equal a, c
-          assert_equal b, a
-          assert_equal b, b
-          assert_equal b, c
-          assert_equal c, a
-          assert_equal c, b
-          assert_equal c, c
-          assert_equal d, d
-          refute_equal a, d
-          refute_equal b, d
-          refute_equal c, d
-          refute_equal d, a
-          refute_equal d, b
-          refute_equal d, c
-          refute_equal a, nil
-          refute_equal d, nil
-          refute_equal nil, a
-          refute_equal nil, d
-        end
-
-        def test_hash
-          a = EventCategory.new(Category.new("Women"))
-          b = EventCategory.new(Category.new("Women"))
-          c = EventCategory.new(Category.new("Women"))
-          d = EventCategory.new(Category.new("Men"))
-
-          assert_equal a.hash, a.hash
-          assert_equal a.hash, b.hash
-          assert_equal a.hash, c.hash
-          assert_equal d.hash, d.hash
-          refute_equal a.hash, d.hash
-          refute_equal b.hash, d.hash
-          refute_equal c.hash, d.hash
-          refute_equal a.hash, nil.hash
-          refute_equal d.hash, nil.hash
-          refute_equal nil.hash, a.hash
-          refute_equal nil.hash, d.hash
+          assert_equality a, b, c, d
         end
       end
     end
