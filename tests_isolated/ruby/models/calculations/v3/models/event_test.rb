254e00cedb6faa9d905a2abf6052d360e31148dc
Scott Willson <scott.willson@gmail.com>
2019-03-13 13:49:11 -0700

    Test Event#add_child

--- tests_isolated/ruby/models/calculations/v3/models/event_test.rb
+++ tests_isolated/ruby/models/calculations/v3/models/event_test.rb
@@ -15,6 +15,15 @@ module Calculations
           assert_equal Date.new(2016)..Date.new(2016), event.dates
         end
 
+        def test_add_child
+          series = Models::Event.new(id: 0, date: Date.new(2018, 5, 1), end_date: Date.new(2018, 5, 8))
+          event = Models::Event.new(id: 1, date: Date.new(2018, 5, 1))
+          series.add_child event
+          assert_equal 1, series.children.size
+          assert_equal 1, series.children[0].id
+          assert_equal series, event.parent
+        end
+
         def test_equality
           a = Event.new(id: 0)
           b = Event.new(id: 0)
