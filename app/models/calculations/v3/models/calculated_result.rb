66f53c12074312357edd7e24b1b9ff31c4437bfe
Scott Willson <scott.willson@gmail.com>
2019-03-14 14:53:11 -0700

    Calculate with tiebreaking rules

--- app/models/calculations/v3/models/calculated_result.rb
+++ app/models/calculations/v3/models/calculated_result.rb
@@ -21,6 +21,10 @@ module Calculations
           validate!
         end
 
+        def placed_source_results_with_points
+          source_results.select(&:placed?).select(&:points?)
+        end
+
         def reject(reason)
           @rejection_reason = reason
           @rejected = true
