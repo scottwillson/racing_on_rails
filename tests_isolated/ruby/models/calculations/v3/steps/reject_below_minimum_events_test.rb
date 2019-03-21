3f11ca835978c591475973ad20ff6fbcf4c4a1bb
Scott Willson <scott.willson@gmail.com>
2019-03-21 15:33:20 -0700

    Don't count DNFs (rejected results) towards minimums

--- tests_isolated/ruby/models/calculations/v3/steps/reject_below_minimum_events_test.rb
+++ tests_isolated/ruby/models/calculations/v3/steps/reject_below_minimum_events_test.rb
@@ -26,6 +26,28 @@ module Calculations
           assert event_categories.first.results.first.rejected?
           assert_equal :below_minimum_events, event_categories.first.results.first.rejection_reason
         end
+
+        def test_dnfs_dont_count
+          category = Models::Category.new("Women")
+          rules = Rules.new(categories: [category], minimum_events: 3)
+          calculator = Calculator.new(rules: rules, source_results: [])
+          event_category = calculator.event_categories.first
+
+          participant = Models::Participant.new(0)
+          rejected_result = Models::SourceResult.new(id: 35, event_category: Models::EventCategory.new(category), place: "DNF", points: 0)
+          rejected_result.reject(:dnf)
+          source_results = [
+            Models::SourceResult.new(id: 0, event_category: Models::EventCategory.new(category), place: 1, points: 100),
+            Models::SourceResult.new(id: 33, event_category: Models::EventCategory.new(category), place: 1, points: 100)
+          ]
+          result = Models::CalculatedResult.new(participant, source_results)
+          event_category.results << result
+
+          event_categories = RejectBelowMinimumEvents.calculate!(calculator)
+
+          assert event_categories.first.results.first.rejected?
+          assert_equal :below_minimum_events, event_categories.first.results.first.rejection_reason
+        end
       end
     end
   end
