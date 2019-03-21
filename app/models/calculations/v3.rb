9a53b5639970e6506feb6afe8c060439bdd54d13
Scott Willson <scott.willson@gmail.com>
2019-03-21 08:36:10 -0700

    Reject DNFs

--- app/models/calculations/v3.rb
+++ app/models/calculations/v3.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Calculations::V3
-  REJECTION_REASONS = %w[ below_minimum_events not_calculation_category worse_result].freeze
+  REJECTION_REASONS = %w[ below_minimum_events dnf not_calculation_category worse_result].freeze
 end
