bbfeadd2f6016c301e3c9145de11bb732070f0f4
Scott Willson <scott.willson@gmail.com>
2019-03-25 16:32:24 -0700

    Allow Calculations without source event

--- db/migrate/20190120003758_create_calculations.rb
+++ db/migrate/20190120003758_create_calculations.rb
@@ -3,6 +3,7 @@
 class CreateCalculations < ActiveRecord::Migration[5.2]
   def change
     create_table :calculations, force: true do |t|
+      t.references :discipline
       t.references :event
       t.references :source_event
       t.boolean :double_points_for_last_event, default: false, null: false
@@ -10,6 +11,7 @@ class CreateCalculations < ActiveRecord::Migration[5.2]
       t.integer :maximum_events, default: 0, null: false
       t.string :name, default: "New Calculation", required: true, unique: true
       t.string :points_for_place, array: true, default: [].to_yaml
+      t.integer :year, default: nil, null: false
       t.timestamps
     end
   end
