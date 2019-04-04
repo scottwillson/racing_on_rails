dc6aa9a5a79fd8e9fa35640823a47cfb5f7a3e68
Scott Willson <scott.willson@gmail.com>
2019-04-04 07:02:45 -0700

    Rearrange methods to make them more readable

--- app/models/calculations/v3/calculation_concerns/calculated_results.rb
+++ app/models/calculations/v3/calculation_concerns/calculated_results.rb
@@ -3,11 +3,13 @@
 module Calculations::V3::CalculationConcerns::CalculatedResults
   extend ActiveSupport::Concern
 
-  # Change to generic persist
-  # Destroy obsolete races
+  # Take new model results from calculate (graph of EventCategories,
+  # CalculatedResults, and SourceResults) and save it to the DB.
+  # Inspect existing results. Create, update, or delete. Faster (but more
+  # complicated) than wiping out all results and recreating.
   def save_results(event_categories)
     ActiveSupport::Notifications.instrument "save_results.calculations.#{name}.racing_on_rails" do
-      delete_obsolete_races
+      delete_obsolete_races event_categories
       event_categories.each do |event_category|
         race = create_race(event_category)
         calculated_results = event_category.results
@@ -34,12 +36,45 @@ module Calculations::V3::CalculationConcerns::CalculatedResults
     obsolete_results = race.results.select { |r| r.person_id.in? obsolete_participant_ids }
 
     ActiveSupport::Notifications.instrument(
-      "partition_results.calculations.#{name}.racing_on_rails new_results: #{new_results.size} " \
+      "partition_results.calculations.#{name}.racing_on_rails" \
+      "new_results: #{new_results.size} " \
       "existing_results: #{existing_results.size} " \
       "obsolete_results: #{obsolete_results.size}"
     )
 
     [new_results, existing_results, obsolete_results]
+  end
+
+  def create_race(event_category)
+    category = Category.find_or_create_by_normalized_name(event_category.name)
+    race = event.races.find_or_create_by!(
+      category: category,
+      rejected: event_category.rejected?,
+      rejection_reason: event_category.rejection_reason
+    )
+    race.destroy_duplicate_results!
+    race.results.reload
+    race
+  end
+
+  def delete_obsolete_races(event_categories)
+    ActiveSupport::Notifications.instrument "delete_obsolete_races.calculations.#{name}.racing_on_rails" do
+      category_names = event_categories.map(&:name)
+      obsolete_races = event.races.reject { |race| race.name.in?(category_names) }
+
+      logger.debug(
+        "delete_obsolete_races.calculations.#{name}.racing_on_rails.obsolete_races " \
+        "race_ids: #{obsolete_races.size} " \
+        "race_names: #{obsolete_races.map(&:name)}"
+      )
+
+      if obsolete_races.any?
+        race_ids = obsolete_races.map(&:id)
+        ::ResultSource.where("calculated_result_id in (select id from results where race_id in (?))", race_ids).delete_all
+        ::Result.where("race_id in (?)", race_ids).delete_all
+      end
+      obsolete_races.each { |race| event.races.delete(race) }
+    end
   end
 
   def create_calculated_results_for(results, race)
@@ -68,6 +103,14 @@ module Calculations::V3::CalculationConcerns::CalculatedResults
     true
   end
 
+  def delete_calculated_results_for(results, race)
+    Rails.logger.debug "delete_calculated_results_for #{race.name}"
+    if results.present?
+      ::ResultSource.where(calculated_result_id: results).delete_all
+      ::Result.where(id: results).delete_all
+    end
+  end
+
   def update_calculated_results_for(results, race)
     Rails.logger.debug "update_calculation_results_for #{race.name}"
     return true if results.empty?
@@ -78,6 +121,16 @@ module Calculations::V3::CalculationConcerns::CalculatedResults
     results.each do |result|
       update_calculated_result_for result, existing_results, team_ids
     end
+  end
+
+  def create_result_source(calculated_result, source_result)
+    ::ResultSource.create!(
+      source_result_id: source_result.id,
+      calculated_result_id: calculated_result.id,
+      points: source_result.points,
+      rejected: source_result.rejected?,
+      rejection_reason: source_result.rejection_reason
+    )
   end
 
   def update_calculated_result_for(result, existing_results, team_ids)
@@ -95,10 +148,10 @@ module Calculations::V3::CalculationConcerns::CalculatedResults
       existing_result.save!
     end
 
-    update_sources_for result, existing_result
+    update_result_sources_for result, existing_result
   end
 
-  def update_sources_for(result, existing_result)
+  def update_result_sources_for(result, existing_result)
     # TODO change this to models, not arrays
     # existing_sources = existing_result.sources.map { |s| [s.id, s.points.to_f] }
     # new_sources = result.source_results.map { |s| [s.id || existing_result.id, s.points.to_f] }
@@ -112,50 +165,6 @@ module Calculations::V3::CalculationConcerns::CalculatedResults
     # sources_to_create.each do |source|
     #   create_result_source existing_result, source.first, source.second
     # end
-  end
-
-  def delete_calculated_results_for(results, race)
-    Rails.logger.debug "delete_calculated_results_for #{race.name}"
-    if results.present?
-      ::ResultSource.where(calculated_result_id: results).delete_all
-      ::Result.where(id: results).delete_all
-    end
-  end
-
-  def create_result_source(calculated_result, source_result)
-    ::ResultSource.create!(
-      source_result_id: source_result.id,
-      calculated_result_id: calculated_result.id,
-      points: source_result.points,
-      rejected: source_result.rejected?,
-      rejection_reason: source_result.rejection_reason
-    )
-  end
-
-  def create_race(event_category)
-    category = Category.find_or_create_by_normalized_name(event_category.name)
-    race = event.races.find_or_create_by!(
-      category: category,
-      rejected: event_category.rejected?,
-      rejection_reason: event_category.rejection_reason
-    )
-    race.destroy_duplicate_results!
-    race.results.reload
-    race
-  end
-
-  def delete_obsolete_races
-    ActiveSupport::Notifications.instrument "delete_obsolete_races.calculations.#{name}.racing_on_rails" do
-      # TODO consider rejected races, too. Inspect event_categories?
-      obsolete_races = event.races.reject { |race| race.name.in?(category_names) }
-      logger.debug "delete_obsolete_races.calculations.#{name}.racing_on_rails.obsolete_races race_ids: #{obsolete_races.size} race_names: #{obsolete_races.map(&:name)}"
-      if obsolete_races.any?
-        race_ids = obsolete_races.map(&:id)
-        ::ResultSource.where("calculated_result_id in (select id from results where race_id in (?))", race_ids).delete_all
-        ::Result.where("race_id in (?)", race_ids).delete_all
-      end
-      obsolete_races.each { |race| event.races.delete(race) }
-    end
   end
 
   def team_ids_by_participant_id_hash(results)
