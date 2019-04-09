1575b87647c22f8c72be64925c76ecaf18128c2e
Scott Willson <scott.willson@gmail.com>
2019-04-09 15:20:05 -0700

    Force table creation to fix index error

--- db/migrate/20190331045946_create_calculations_events.rb
+++ db/migrate/20190331045946_create_calculations_events.rb
@@ -1,6 +1,6 @@
 class CreateCalculationsEvents < ActiveRecord::Migration[5.2]
   def change
-    create_table :calculations_events do |t|
+    create_table :calculations_events, force: true do |t|
       t.belongs_to :calculation, index: true
       t.belongs_to :event, index: true
       t.float :multiplier, default: 1.0, null: false
