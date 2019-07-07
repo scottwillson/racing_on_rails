d130a64b3e87ba0746a51b27f54a30b8e66d2263
Scott Willson <s.willson@flyblade.com>
2019-07-07 15:33:11 -0700

    Reject worst 10% of results for calculations placed by place

---
+++ tests_isolated/ruby/models/calculations/v3/steps/reject_category_worst_results_test.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require_relative "../../v3"
+
+module Calculations
+  module V3
+    module Steps
+      # :stopdoc:
+      class RejectWorstResultsTest < Ruby::TestCase
+        def test_calculate
+          category = Models::Category.new("Women")
+          rules = Rules.new(place_by: "place")
+          calculator = Calculator.new(rules: rules, source_results: [])
+
+          participant = Models::Participant.new(0)
+          event = Models::Event.new(id: 0, calculated: true)
+
+          3.times do |ability|
+            category = Models::Category.new("Women #{ability + 1}")
+            20.times do |index|
+              source_result = Models::SourceResult.new(
+                id: ability * 20 + index,
+                event_category: Models::EventCategory.new(category, event),
+                participant: participant,
+                place: index + 1
+              )
+              result = Models::CalculatedResult.new(participant, [source_result])
+              calculator.event_categories.first.results << result
+            end
+          end
+
+          event_categories = RejectWorstResults.calculate!(calculator)
+
+          rejected_results = event_categories.flat_map(&:results).select(&:rejected?)
+          assert_equal 4, rejected_results.size
+          assert_equal [19, 19, 20, 20], rejected_results.map(&:source_result_numeric_place).sort
+        end
+      end
+    end
+  end
+end
