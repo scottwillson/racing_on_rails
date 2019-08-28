50df4c319dbb9a81e2173f956e6c17402c751885
Scott Willson <s.willson@flyblade.com>
2019-08-28 16:38:02 -0700

    Add more calculation rules to show page

--- test/controllers/calculations_controller_test.rb
+++ test/controllers/calculations_controller_test.rb
@@ -3,8 +3,15 @@
 require "test_helper"
 
 class CalculationsControllerTest < ActionController::TestCase
-  test "should get index" do
+  test "get index" do
     get :index
+    assert_response :success
+  end
+
+  test "get show" do
+    FactoryBot.create(:discipline)
+    calculation = Calculations::V3::Calculation.create!
+    get :show, params: { id: calculation.to_param }
     assert_response :success
   end
 end
