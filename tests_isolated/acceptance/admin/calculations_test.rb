6366da3bc9630ee1b6079cb0fdc8954cd491fc22
Scott Willson <s.willson@flyblade.com>
2019-08-28 15:41:09 -0700

    Add calculations controller

---
+++ tests_isolated/acceptance/admin/calculations_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require_relative "../acceptance_test"
+
+# :stopdoc:
+class CategoriesTest < AcceptanceTest
+  test "edit" do
+    FactoryBot.create(:discipline)
+
+    Calculations::V3::Calculation.create!(
+      members_only: true,
+      name: "Ironman",
+      points_for_place: 1
+    )
+
+    login_as FactoryBot.create(:administrator)
+
+    visit "/calculations"
+  end
+end
