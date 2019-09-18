74587a4ed988aa65f949e83985aee1cbb6782068
Scott Willson <scott.willson@gmail.com>
2019-09-17 19:51:17 -0700

    Deploy branch

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,7 @@ set :deploy_to, "/var/www/rails/#{fetch(:application)}"
 
 set :repo_url, "git://github.com/scottwillson/racing_on_rails.git"
 set :site_local_repo_url, "git@github.com:scottwillson/#{fetch(:application)}-local.git"
+set :branch, "competitions-3"
 
 set :puma_conf, "#{shared_path}/config/puma.rb"
 
@@ -43,7 +44,7 @@ namespace :deploy do
 
         execute :rm, "-rf \"#{release_path}/lib/registration_engine\"" if test("[ -L \"#{release_path}/lib/registration_engine\" ]")
 
-        execute :git, "clone git@github.com:scottwillson/registration_engine.git #{release_path}/lib/registration_engine"
+        execute :git, "clone git@github.com:scottwillson/registration_engine.git -b competitions-3 #{release_path}/lib/registration_engine"
       end
     end
   end
