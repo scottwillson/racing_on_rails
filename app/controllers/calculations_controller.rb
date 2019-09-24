35d6eb4838cbd69f0d8a9948dbe58b8c94316c92
Scott Willson <scott.willson@gmail.com>
2019-09-23 20:32:28 -0700

    Add calculation edit view

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 class CalculationsController < ApplicationController
+  before_action :require_administrator, only: :edit
+
+  def edit
+    @calculation = Calculations::V3::Calculation.find(params[:id])
+  end
+
   def index
     @calculations = Calculations::V3::Calculation.all
   end
