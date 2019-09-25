6372b8010aaef5f9505df4489a9e29a4e534851b
Scott Willson <scott.willson@gmail.com>
2019-09-24 20:05:56 -0700

    Update OJCS cats

--- app/models/competitions/oregon_junior_cyclocross_series/team.rb
+++ app/models/competitions/oregon_junior_cyclocross_series/team.rb
@@ -26,11 +26,11 @@ module Competitions
       def source_result_category_names
         [
           "Junior Men 1/2/3",
-          "Junior Men 1/2/3",
           "Junior Men 13-14 3/4/5",
           "Junior Men 15-16 3/4/5",
           "Junior Men 17-18 3/4/5",
           "Junior Men 9-12 3/4/5",
+          "Junior Women 1/2/3",
           "Junior Women 13-14 3/4/5",
           "Junior Women 15-16 3/4/5",
           "Junior Women 17-18 3/4/5",
