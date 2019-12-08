85d3d9a389948d34fdd11f41e36f98abb8208f5a
Scott Willson <scott.willson@gmail.com>
2019-12-08 08:05:27 -0800

    Rev schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_205842) do
-
+ActiveRecord::Schema.define(version: 2019_12_02_001959) do
   create_table "adjustments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "order_id"
     t.integer "person_id"
