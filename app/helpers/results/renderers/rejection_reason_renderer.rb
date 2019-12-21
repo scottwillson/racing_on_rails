375cc13787bfb3aa5b9962a1b93bf5f0597a84aa
Scott Willson <s.willson@flyblade.com>
2019-12-21 10:55:40 -0500

    Don't render nil rejection reason

--- app/helpers/results/renderers/rejection_reason_renderer.rb
+++ app/helpers/results/renderers/rejection_reason_renderer.rb
@@ -7,6 +7,8 @@ module Results
     class RejectionReasonRenderer < Results::Renderers::DefaultResultRenderer
       def self.render(_column, row)
         result = row.source
+        return "" if result.rejection_reason.blank?
+
         I18n.t result.rejection_reason, category: result.category, minimum_events: 3
       end
     end
