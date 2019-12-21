ef1a5c88a352dbeaa91df89d2d1ef23abf81a7c8
Scott Willson <s.willson@flyblade.com>
2019-12-21 17:28:54 -0500

    Fix numeric places

--- db/migrate/20191221220447_set_null_numeric_places_to_max.rb
+++ db/migrate/20191221220447_set_null_numeric_places_to_max.rb
@@ -1,9 +1,11 @@
 class SetNullNumericPlacesToMax < ActiveRecord::Migration[5.2]
   def change
+    execute "update results set numeric_place=999999 where numeric_place is null"
     Result.find_each do |result|
       result.set_numeric_place(result.place)
-      result.save!
+      if result.numeric_place_changed?
+        result.update_column :numeric_place, result.numeric_place
+      end
     end
-    execute "update results set numeric_place=999999 where numeric_place is null"
   end
 end
