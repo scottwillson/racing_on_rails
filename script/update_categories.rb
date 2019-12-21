60984456da090b604b0ba86f4b119fda8c142c17
Scott Willson <s.willson@flyblade.com>
2019-12-21 16:25:08 -0500

    Add update categories script

---
+++ script/update_categories.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+::Category.order(:name).find_each do |category|
+  category.set_abilities_from_name
+  category.set_ages_from_name
+  category.set_equipment_from_name
+  category.set_gender_from_name
+  category.set_weight_from_name
+
+  if category.changed?
+    puts "#{category.name} #{category.changes}"
+  end
+
+  category.save!
+end
