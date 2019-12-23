d03279b8d835a82a4bb2cb7404c70fbb9ee7a412
Scott Willson <scott.willson@gmail.com>
2019-12-22 16:29:53 -0800

    One more tweak for singlespeed matching

--- test/models/categories/best_match_in_test.rb
+++ test/models/categories/best_match_in_test.rb
@@ -265,6 +265,8 @@ module Competitions
       eddy_senior_men = Category.find_or_create_by_normalized_name("Eddy Senior Men")
       eddy_women = Category.find_or_create_by_normalized_name("Eddy Women")
       eddy_senior_women = Category.find_or_create_by_normalized_name("Eddy Senior Women")
+      singlespeed_40_plus = Category.find_or_create_by_normalized_name("Singlespeed Men 40+")
+      singlespeed_women_40_plus = Category.find_or_create_by_normalized_name("Singlespeed Women 40+")
 
       event = FactoryBot.create(:event)
       event.races.create!(category: Category.find_or_create_by_normalized_name("Athena"))
@@ -283,7 +285,7 @@ module Competitions
       event.races.create!(category: Category.find_or_create_by_normalized_name("Masters Women 4"))
       event.races.create!(category: pro_1_2_men)
       event.races.create!(category: pro_1_2_women)
-      event.races.create!(category: Category.find_or_create_by_normalized_name("Singlespeed/Fixed"))
+      event.races.create!(category: @singlespeed)
       event.races.create!(category: Category.find_or_create_by_normalized_name("Tandem"))
 
       assert_best_match_in [pro_1_2_men, men_15_24, men_19_29], pro_1_2_men, event
@@ -291,6 +293,7 @@ module Competitions
       assert_best_match_in [@junior_women, junior_women_10_12], @junior_women, event
       assert_best_match_in [eddy_men, eddy_senior_men, pro_1_2_men], pro_1_2_men, event
       assert_best_match_in [eddy_women, eddy_senior_women, pro_1_2_women], pro_1_2_women, event
+      assert_best_match_in [@singlespeed, singlespeed_40_plus, singlespeed_women_40_plus], @singlespeed, event
     end
 
     def assert_best_match_in(categories, race_category, event, result_age = nil)
