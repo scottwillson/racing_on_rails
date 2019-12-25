e0dd8b00e1cfd6f089609b517ccc9d38d8cc285a
Scott Willson <scott.willson@gmail.com>
2019-12-25 11:10:37 -0800

    Fix mocha deprecation warning

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,7 +7,7 @@ ENV["RAILS_ENV"] ||= "test"
 
 require_relative "../config/environment"
 require "rails/test_help"
-require "mocha/setup"
+require "mocha/minitest"
 require "authlogic/test_case"
 require "parallel_tests/test/runtime_logger" if ENV["RECORD_RUNTIME"]
 require "test/enumerable_assertions"
--- tests_isolated/ruby/test_case.rb
+++ tests_isolated/ruby/test_case.rb
@@ -5,7 +5,7 @@ require File.expand_path("../../../lib/test/enumerable_assertions", __FILE__)
 require "active_support/core_ext/object/blank"
 require "active_support/core_ext/object/try"
 require "active_support/concern"
-require "mocha/setup"
+require "mocha/minitest"
 
 module Ruby
   class TestCase < Minitest::Test
