bb1d54ee535ca1fdae79a360d7bffb2a47eb29aa
Scott Willson <s.willson@flyblade.com>
2020-01-06 15:28:58 -0800

    Spec class name for calculation_category_mappings

--- app/models/discipline.rb
+++ app/models/discipline.rb
@@ -3,7 +3,7 @@
 # Road, track, criterium, time trial ...
 # Cached. Call +reset+ to clear cache.
 class Discipline < ApplicationRecord
-  has_many :calculation_category_mappings, dependent: :destroy
+  has_many :calculation_category_mappings, dependent: :destroy, class_name: "::Calculations::V3::CategoryMapping"
   has_many :discipline_aliases
   has_and_belongs_to_many :bar_categories, class_name: "Category", join_table: "discipline_bar_categories"
 
