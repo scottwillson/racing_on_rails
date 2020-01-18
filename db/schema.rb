e6338d62c4db5e992d195a52cfe789b76792fd34
Scott Willson <scott.willson@gmail.com>
2020-01-17 16:40:25 -0800

    Update scghema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_07_150098) do
+ActiveRecord::Schema.define(version: 2020_01_17_183910) do
   create_table "adjustments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci", force: :cascade do |t|
     t.integer "order_id"
     t.integer "person_id"
