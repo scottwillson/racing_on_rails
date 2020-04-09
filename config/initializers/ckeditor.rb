2c8fc9f35dc0ca60bfeac896b825111b1532dec5
Scott Willson <scott.willson@gmail.com>
2020-04-09 09:52:00 -0700

    Disable more things to avoid autoloading

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 # Use this hook to configure ckeditor
-Ckeditor.setup do |_|
-  require "ckeditor/orm/active_record"
-end
+# Ckeditor.setup do |_|
+#   require "ckeditor/orm/active_record"
+# end
