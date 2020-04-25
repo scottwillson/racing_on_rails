b4fb6ad273806064cbedae562b7a92e0b692a680
Scott Willson <scott.willson@gmail.com>
2020-04-25 08:44:57 -0700

    Add styles back

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,16 +2,3 @@
 
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 Rails.application.config.assets.version = "6.0"
-
-Rails.application.configure do
-  config.assets.precompile += %w[
-    admin.js
-    apple-touch-icon.png
-    ckeditor/config.js
-    favicon.ico
-    racing_association.css
-    racing_association.js
-    registration_engine/application.js
-  ]
-  config.assets.precompile << ["*.png", "*.jpg"]
-end
