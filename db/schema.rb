3eba684c0448d09c536b608cd67d3d1098eab6bb
Scott Willson <scott.willson@gmail.com>
2020-05-01 08:24:11 -0700

    Replace Team#name unique validation with index

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_25_160436) do
+ActiveRecord::Schema.define(version: 2020_05_01_151835) do
 
   create_table "#Tableau_01_sid_00026E8B_4_Group", id: false, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "Age (group)", limit: 21
@@ -1145,7 +1145,7 @@ ActiveRecord::Schema.define(version: 2020_04_25_160436) do
     t.string "created_by_name"
     t.string "updated_by_name"
     t.index ["created_by_id"], name: "index_teams_on_created_by_id"
-    t.index ["name"], name: "index_teams_on_name"
+    t.index ["name"], name: "index_teams_on_name", unique: true
     t.index ["updated_at"], name: "index_teams_on_updated_at"
     t.index ["updated_by_id"], name: "index_teams_on_updated_by_id"
   end
@@ -1174,8 +1174,8 @@ ActiveRecord::Schema.define(version: 2020_04_25_160436) do
     t.integer "item_id", null: false
     t.string "event", null: false
     t.string "whodunnit"
-    t.text "object", limit: 4294967295
-    t.text "object_changes", limit: 4294967295
+    t.text "object", size: :long
+    t.text "object_changes", size: :long
     t.datetime "created_at"
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
