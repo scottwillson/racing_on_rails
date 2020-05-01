f2b5bc6202f83be42a89d0bce60fb4ea5e757792
Scott Willson <scott.willson@gmail.com>
2020-05-01 16:51:19 -0700

    Use update! not update_attributes!

--- db/migrate/20141125165135_remove_mbrabar_notes.rb
+++ db/migrate/20141125165135_remove_mbrabar_notes.rb
@@ -4,7 +4,7 @@ class RemoveMbrabarNotes < ActiveRecord::Migration
   def change
     bar = Competitions::MbraBar.current_year.first
     bar&.races&.map(&:results)&.flatten&.each do |result|
-      result.update_attributes! notes: nil if result.notes.present?
+      result.update! notes: nil if result.notes.present?
     end
   end
 end
