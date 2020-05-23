7dbcb6d5bb1f0306c4848bca312e911ce2fcfaab
Scott Willson <scott.willson@gmail.com>
2020-05-23 08:36:39 -0700

    Add JS error assertion

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -21,7 +21,21 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   Capybara.server = :puma, { Silent: true }
 
   setup :stub_elasticsearch
+  teardown :assert_no_java_script_errors
 
   include ::Actions
   include ::Assertions
+
+  def assert_no_java_script_errors
+    logs = page.driver.browser.manage.logs.get(:browser)
+    errors = logs.select { |log| log.level == "SEVERE" }
+    return if errors.none?
+
+    formatted_errors = errors
+                       .map(&:message)
+                       .map { |message| message.gsub('\\n', "\n") }
+                       .join("\n\n")
+
+    raise "Browser JavaScript errors\n\n#{formatted_errors}"
+  end
 end
