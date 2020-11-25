9e6761ff3894e484f04eff01d9f6b55501ffbe8f
Scott Willson <scott.willson@gmail.com>
2020-11-25 08:48:07 -0800

    Add back registration email check

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -133,7 +133,7 @@ class PeopleController < ApplicationController
 
     @person.errors.add :email, "can't be blank" if person_params[:email].blank?
 
-    @person.errors.add :email, "must been email address" if person_params[:email].blank?
+    @person.errors.add :email, "must be an email address" if person_params[:email].blank? || !person_params[:email][URI::MailTo::EMAIL_REGEXP]
 
     @person.errors.add :login, "can't be blank" if person_params[:login].blank?
 
