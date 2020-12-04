3327556ec691827edd9fa4e99f4f32dde5460859
Scott Willson <scott.willson@gmail.com>
2020-12-03 19:06:04 -0800

    Profile memory in prod

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -41,12 +41,4 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
-  config.middleware.use(
-    StackProf::Middleware,
-    enabled: true,
-    mode: :object,
-    interval: 1000,
-    save_every: 5
-  )
 end
