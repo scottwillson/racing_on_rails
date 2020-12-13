8ec271d7650d2652b64ed7a62076c3b150930373
Scott Wilson <scott.willson@gmail.com=>
2020-12-13 09:53:14 -0800

    Enable debugging tools

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module RacingOnRails
     config.exceptions_app = routes
 
     Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemcacheStore
+    Rack::MiniProfiler.config.enable_advanced_debugging_tools = true
     Rack::MiniProfiler.config.snapshot_every_n_requests = 10_000
 
     def exception_notifier
