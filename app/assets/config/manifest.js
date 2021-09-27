d8356ff07e96444edfb9537bcb2d0ca8a072a315
Scott Willson <scott.willson@gmail.com>
2021-09-26 20:17:58 -0700

    Start to build out WP menus

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,8 @@
 //= link_tree ../fonts
 //= link_directory ../javascripts .js
 
+//= link_tree ../../../local/app/assets/images
+
 // Don't precompile partials
 //= link application.css
 //= link registration_engine/application.css
