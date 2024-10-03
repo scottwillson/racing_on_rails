8a5870288c2f0ce1de3f375e6d61942bc11043b1
Scott Hillson <scott.hillson@gmail.com>
2024-10-02 18:05:41 -0700

    Update stats_controller.rb

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -5,7 +5,8 @@ module Admin
       Discipline.pluck(:name).each do |discipline|
         @chart_data.push({ name: discipline, data: [] })
       end
-      %w[2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023 2024].each do |year|
+      @years = %w[2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023 2024]
+      @years.each do |year|
         res = Result.joins(:event).where(events: { year: year, type: "SingleDayEvent" })
                     .where(competition_result: false, team_competition_result: false)
                     .where.not(person_id: nil).group(:discipline).count
