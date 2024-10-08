d8a86b55b2978d08611893fab55a1fa2fef0155e
Scott Hillson <scott.hillson@gmail.com>
2024-10-08 10:27:52 -0700

    Update stats_controller.rb

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -4,6 +4,9 @@ module Admin
       @years = Stats.years
       @racer_days_by_discipline = Stats.racer_days_by_discipline
       @total_racer_days = Stats.total_racer_days
+      @racers_by_discipline = Stats.racers_by_discipline
+      @memberships = Stats.memberships
+      @total_juniors = Stats.total_juniors
     end
   end
 end
