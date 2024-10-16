eace8d0229056600bdbc7c3deb6306ee2e488ea8
Scott Hillson <scott.hillson@gmail.com>
2024-10-15 21:59:14 -0700

    Update stats_controller.rb

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -1,10 +1,11 @@
 module Admin
   class StatsController < Admin::AdminController
     def index
-      @years = %w[2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023 2024]
-      if params[:nopandemic] == "true"
-        @years = %w[2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2021 2022 2023 2024]
-      end
+      @years = if params[:nopandemic] == "true"
+                 %w[2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2021 2022 2023 2024]
+               else
+                 %w[2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022 2023 2024]
+               end
       @racer_days_gender_ratio = Stats.racer_days_gender_ratio(@years)
       @racer_days_by_discipline = Stats.racer_days_by_discipline(@years)
       @racers_by_discipline = Stats.racers_by_discipline(@years)
