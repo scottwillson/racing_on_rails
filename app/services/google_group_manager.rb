568d002c416fb5bb123975348280b30b01d13309
Scott Hillson <scott.hillson@gmail.com>
2025-03-21 23:35:46 -0700

    Update google_group_manager.rb

--- app/services/google_group_manager.rb
+++ app/services/google_group_manager.rb
@@ -1,3 +1,6 @@
+require "google/apis/admin_directory_v1"
+require "json"
+
 class GoogleGroupManager
   def initialize(key_path, admin_email)
     @service = Google::Apis::AdminDirectoryV1::DirectoryService.new
