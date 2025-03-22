e822af9c1f60dfc779912b6ea1b7f23a7148d70c
Scott Hillson <scott.hillson@gmail.com>
2025-03-22 10:33:43 -0700

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.eager_load                                         = true
   config.i18n.fallbacks                                     = true
   config.log_formatter                                      = ::Logger::Formatter.new
-  config.log_level                                          = :info
+  config.log_level                                          = :debug
   config.log_tags                                           = [:request_id]
   config.logger                                             = Syslog::Logger.new("racing_on_rails", Syslog::LOG_LOCAL4)
   config.public_file_server.enabled                         = ENV["RAILS_SERVE_STATIC_FILES"].present?
